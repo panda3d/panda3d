06f55154bee40649c6e6e072c8996f4c871cb7f4
Cary Sandvig <frang@users.sourceforge.net>
2000-10-12 21:35:09 +0000

    woo!  mp3

--- panda/src/audiotraits/Sources.pp
+++ panda/src/audiotraits/Sources.pp
@@ -35,3 +35,15 @@
     audio_load_st.cxx
 
 #end lib_target
+
+#begin lib_target
+  #define TARGET audio_load_mp3
+  #define BUILDING_DLL BUILDING_MISC
+  #define LOCAL_LIBS \
+    audio mpg123
+  #define CFLAGS -DGENERIC -DNOXFERMEM
+
+  #define SOURCES \
+    audio_load_mp3.cxx
+
+#end lib_target
