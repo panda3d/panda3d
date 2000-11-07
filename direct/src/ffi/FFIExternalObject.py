c6fc1c9ad2d2b7f20fa3d7fb7a23934cde832851
Joe Shochet <shochet@gmail.com>
2000-11-07 21:50:08 +0000

    *** empty log message ***

--- direct/src/ffi/FFIExternalObject.py
+++ direct/src/ffi/FFIExternalObject.py
@@ -5,8 +5,8 @@ import TypedObject
 WrapperClassMap = {}
 
 # For testing, you can turn verbose and debug on
-FFIConstants.notify.setVerbose(1)
-FFIConstants.notify.setDebug(1)
+# FFIConstants.notify.setVerbose(1)
+# FFIConstants.notify.setDebug(1)
 
 
 # Register a python class in the type map if it is a typed object
