dca70e17414b75924393069ec5700229f3cb1ee7
Cary Sandvig <frang@users.sourceforge.net>
2000-11-15 22:14:57 +0000

    new file

---
+++ panda/src/testbed/deadrec_rec.cxx
@@ -0,0 +1,14 @@
+// Filename: deadrec_rec.cxx
+// Created by:  cary (15Nov00)
+// 
+////////////////////////////////////////////////////////////////////
+
+#include "framework.h"
+
+void deadrec_keys(EventHandler& eh) {
+}
+
+int main(int argc, char* argv[]) {
+  define_keys = &deadrec_keys;
+  return framework_main(argc, argv);
+}
---
+++ panda/src/testbed/deadrec_send.cxx
@@ -0,0 +1,15 @@
+// Filename: deadrec_send.cxx
+// Created by:  cary (15Nov00)
+// 
+////////////////////////////////////////////////////////////////////
+
+#include "framework.h"
+
+void deadrec_keys(EventHandler& eh) {
+  
+}
+
+int main(int argc, char* argv[]) {
+  define_keys = &deadrec_keys;
+  return framework_main(argc, argv);
+}
