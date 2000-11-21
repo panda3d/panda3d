8553c342bf70186b173ea770e94b1fff9cc346fd
Mike Goslin <mikegoslin@users.sourceforge.net>
2000-11-21 19:06:16 +0000

    *** empty log message ***

---
+++ dtool/src/parser-inc/hex.h
@@ -0,0 +1,14 @@
+// Filename: hex.h
+// Created by:  drose (17Nov00)
+// 
+////////////////////////////////////////////////////////////////////
+
+// This file, and all the other hex in this directory, aren't
+// intended to be compiled--they're just parsed by CPPParser (and
+// interrogate) in lieu of the actual system headers, to generate the
+// interrogate database.
+
+#ifndef HEX_H
+#define HEX_H
+
+#endif
