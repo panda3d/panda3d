67cb365f99cacd01545891133d952d9ffbfdec3d
Cary Sandvig <frang@users.sourceforge.net>
2000-12-05 01:45:44 +0000

    comment cleanup and junk

--- panda/src/testbed/deadrec_send.cxx
+++ panda/src/testbed/deadrec_send.cxx
@@ -520,24 +520,6 @@ static void deadrec_setup(EventHandler& eh) {
   my_arc = new RenderRelation(render, smiley);
 
   writer = new ConnectionWriter(&cm, 0);
-  /*
-  // open a connection to the receiver
-  NetAddress host;
-  if (!host.set_host(hostname, hostport)) {
-    deadrec_cat->fatal() << "Unknown host: " << hostname << endl;
-    exit(0);
-  }
-  conn = cm.open_TCP_client_connection(host, 5000);
-  if (conn.is_null()) {
-    deadrec_cat->fatal() << "no connection." << endl;
-    exit(0);
-  }
-  if (deadrec_cat->is_debug())
-    deadrec_cat->debug() << "opened TCP connection to " << hostname
-			 << " on port " << conn->get_address().get_port()
-			 << " and IP " << conn->get_address() << endl;
-  writer = new ConnectionWriter(&cm, 0);
-  */
 
   // create an interface
   GuiManager* mgr = GuiManager::get_ptr(main_win, mak);
