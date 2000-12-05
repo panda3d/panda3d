b3f2abc0b56563bdf9a8eddacbc035cec2c59469
Mike Goslin <mikegoslin@users.sourceforge.net>
2000-12-05 23:12:26 +0000

    *** empty log message ***

---
+++ direct/src/fsm/StateData.py
@@ -0,0 +1,42 @@
+
+"""StateData module: contains StateData class"""
+
+from DirectObject import *
+
+class StateData(DirectObject):
+
+    """StateData class: """
+
+    def __init__(self, doneEvent)
+        """__init__(self, Event)
+        """
+	self.doneEvent = doneEvent
+
+    def enter(self):
+        """enter(self)"""
+	print "Called abstract enter function"
+
+    def exit(self):
+	"""exit(self)"""
+	print "Called abstract exit function"
+
+    def load(self):
+	"""load(self)"""
+	print "Called abstract load function"
+	
+    def unload(self):
+	"""unload(self)"""
+	print "Called abstract unload function"
+
+
+            
+        
+
+    
+
+
+
+
+
+
+
