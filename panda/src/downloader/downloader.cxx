2e54f4bc764c1d607ceb7eca3e7d2fb161d2a2d4
Mike Goslin <mikegoslin@users.sourceforge.net>
2000-12-15 01:31:05 +0000

    *** empty log message ***

--- panda/src/downloader/downloader.cxx
+++ panda/src/downloader/downloader.cxx
@@ -426,6 +426,9 @@ run(void) {
           return DS_error_write;
       }
       _dest_stream.close();
+      // The "Connection: close" line tells the server to close the 
+      // connection when the download is complete
+      _connected == false;
       return DS_success;
     } else {
       if (downloader_cat.is_debug())
