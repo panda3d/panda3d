147a42a6e30e0bf2555bdf733c73cef2d158364f
Mike Goslin <mikegoslin@users.sourceforge.net>
2000-12-19 00:15:05 +0000

    *** empty log message ***

--- panda/src/downloadertools/pdecompress.cxx
+++ panda/src/downloadertools/pdecompress.cxx
@@ -11,8 +11,12 @@ main(int argc, char *argv[]) {
 
   Filename source_file = argv[1];
 
+#ifdef TESTING_CODE
+  Decompressor decompressor;
+#else
   PT(Buffer) buffer = new Buffer(1000000);
   Decompressor decompressor(buffer);
+#endif
   if (decompressor.decompress(source_file) == false)
     cerr << "Decompress failed" << endl;
 
