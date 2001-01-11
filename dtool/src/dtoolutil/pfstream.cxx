820f77f19af336decd9c81d4179e1ade4b74e1f4
Cary Sandvig <frang@users.sourceforge.net>
2001-01-11 01:58:04 +0000

    *** empty log message ***

---
+++ dtool/src/dtoolutil/pfstream.cxx
@@ -0,0 +1,8 @@
+// Filename: pfstream.h
+// Created by:  cary (09Jan01)
+//
+////////////////////////////////////////////////////////////////////
+
+#include "pfstream.h"
+
+// dummy C++ file to make some compilers happy
