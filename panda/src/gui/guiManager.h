ae22114628d914131fe53862a5b3e7d4144b0a13
David Rose <david@ddrose.com>
2001-01-11 00:26:39 +0000

    *** empty log message ***

--- panda/src/gui/guiManager.h
+++ panda/src/gui/guiManager.h
@@ -32,8 +32,7 @@ private:
   INLINE GuiManager(MouseWatcher*, Node*);
 
 PUBLISHED:
-  static GuiManager* get_ptr(GraphicsWindow*, MouseAndKeyboard*,
-			     Node *root2d = (Node *)NULL);
+  static GuiManager* get_ptr(GraphicsWindow*, MouseAndKeyboard*, Node *root2d);
 
   void add_region(GuiRegion*);
   void add_label(GuiLabel*);
