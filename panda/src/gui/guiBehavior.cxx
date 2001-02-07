79dc9d7d6c3bee83c773ea9d0e1044efdb79534a
Cary Sandvig <frang@users.sourceforge.net>
2001-02-07 22:56:09 +0000

    avuncular

---
+++ panda/src/gui/guiBehavior.cxx
@@ -0,0 +1,46 @@
+// Filename: guiBehavior.cxx
+// Created by:  cary (07Feb01)
+// 
+////////////////////////////////////////////////////////////////////
+
+#include "guiBehavior.h"
+
+TypeHandle GuiBehavior::_type_handle;
+
+GuiBehavior::GuiBehavior(const string& name) : GuiItem(name),
+					       _eh((EventHandler*)0L) {
+}
+
+GuiBehavior::~GuiBehavior(void) {
+}
+
+void GuiBehavior::manage(GuiManager* mgr, EventHandler& eh) {
+  _eh = &eh;
+  GuiItem::manage(mgr, eh);
+}
+
+void GuiBehavior::unmanage(void) {
+  if (_behavior_running)
+    this->stop_behavior();
+  _eh = (EventHandler)0L;
+  GuiItem::unmanage();
+}
+
+void GuiBehavior::start_behavior(void) {
+  _behavior_running = true;
+}
+
+void GuiBehavior::stop_behavior(void) {
+  _behavior_running = false;
+}
+
+void GuiBehavior::reset_behavior(void) {
+}
+
+void GuiBehavior::output(ostream& os) const {
+  GuiItem::output(os);
+  os << "  Behavior Data:" << endl;
+  os << "    behavior is " << (_behavior_running?"":"not ") << "running"
+     << endl;
+  os << "    eh - 0x" << (void*)_eh << endl;
+}
