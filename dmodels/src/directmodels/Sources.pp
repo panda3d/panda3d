3358d2716f9e8fee1b07ae05bce193317dd0afcf
Mark Mine <mark.mine@disney.com>
2001-02-07 00:59:07 +0000

    *** empty log message ***

--- dmodels/src/directmodels/Sources.pp
+++ dmodels/src/directmodels/Sources.pp
@@ -1,7 +1,11 @@
 #define DIR_TYPE models
 #define INSTALL_TO models/directmodels
 
+#begin flt_egg
+  #define SOURCES $[wildcard *.flt]
+#end flt_egg
+
 #begin install_egg
   #define SOURCES \
-    smiley.egg
+    sphere.egg smiley.egg
 #end install_egg
---
+++ dmodels/src/icons/Sources.pp
@@ -0,0 +1,10 @@
+#define DIR_TYPE models
+#define INSTALL_TO icons
+
+
+#begin install_icons
+  #define SOURCES \
+      folder.gif minusnode.gif openfolder.gif plusnode.gif python.gif \
+      sphere2.gif tk.gif
+#end install_dna
+
