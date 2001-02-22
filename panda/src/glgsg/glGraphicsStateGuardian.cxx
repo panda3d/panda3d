51e071252516e3a5fefec7412f0e33c88f2ff15f
georges <>
2001-02-22 02:07:08 +0000

    undo that last change

--- panda/src/glgsg/glGraphicsStateGuardian.cxx
+++ panda/src/glgsg/glGraphicsStateGuardian.cxx
@@ -1983,9 +1983,9 @@ copy_pixel_buffer(PixelBuffer *pb, const DisplayRegion *dr) {
     << (void *)pb->_image.p() << ")" << endl;
 #endif
 
-
-//glReadPixels( pb->get_xorg() + xo, pb->get_yorg() + yo,  // pb->origin is probably a meaningless vestigial field thats always zero.  shouldnt be added here
-  glReadPixels(xo, yo, 
+  // pixelbuffer "origin" represents upper left screen point at which
+  // pixelbuffer should be drawn using draw_pixel_buffer
+  glReadPixels(pb->get_xorg() + xo, pb->get_yorg() + yo,  
 			   pb->get_xsize(), pb->get_ysize(), 
 			   get_external_image_format(pb->get_format()),
 			   get_image_type(pb->get_image_type()),
