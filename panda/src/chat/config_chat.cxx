c01e5cb2ad92642a3d7568414b692133fda61e55
cxgeorge <>
2001-06-04 01:50:24 +0000

    update precomp hdrs

--- panda/src/chat/config_chat.cxx
+++ panda/src/chat/config_chat.cxx
@@ -16,13 +16,13 @@
 //
 ////////////////////////////////////////////////////////////////////
 
-#ifdef WIN32_VC
+#if defined(WIN32_VC) && !defined(NO_PCH)
 #include "chat_headers.h"
 #endif
 
 #pragma hdrstop
 
-#ifndef WIN32_VC
+#if !defined(WIN32_VC) || defined(NO_PCH)
 #include "chatInput.h"
 #endif
 
