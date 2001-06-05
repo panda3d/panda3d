a0f8d9a0b997aa1850d27a311d20325a80d9d2f6
David Rose <david@ddrose.com>
2001-06-05 21:54:44 +0000

    add memory to parser-inc list

--- dtool/src/parser-inc/Sources.pp
+++ dtool/src/parser-inc/Sources.pp
@@ -1,4 +1,5 @@
 #define INSTALL_PARSER_INC \
-    algorithm deque iostream list map pair queue set stack stdtypedefs.h \
+    algorithm deque iostream list map memory \
+    pair queue set stack stdtypedefs.h \
     string vector windows.h zlib.h md5.h files.h hex.h \
     nurbs.hh
