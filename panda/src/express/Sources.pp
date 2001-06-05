6eac15baf79888409d5ad74addad6e6d8da6699e
cxgeorge <>
2001-06-05 21:22:25 +0000

    use combined sources

--- panda/src/express/Sources.pp
+++ panda/src/express/Sources.pp
@@ -7,39 +7,40 @@
   #define USE_NSPR yes
   #define USE_CRYPTO yes
   #define USE_NET yes
+  
+  #define COMBINED_SOURCES $[TARGET]_composite1.cxx $[TARGET]_composite2.cxx
 
-  #define SOURCES                           \
-    bigEndian.h buffer.I buffer.cxx buffer.h    \
-    checksumHashGenerator.I checksumHashGenerator.cxx \
-    checksumHashGenerator.h \
-    circBuffer.I circBuffer.h clockObject.I clockObject.cxx     \
-    clockObject.h config_express.cxx config_express.h datagram.I    \
-    datagram.cxx datagram.h datagramGenerator.I datagramGenerator.cxx   \
-    datagramGenerator.h datagramInputFile.I datagramInputFile.h     \
-    datagramInputFile.cxx datagramIterator.I \
-    datagramIterator.cxx datagramIterator.h \
-    datagramOutputFile.I datagramOutputFile.h datagramOutputFile.cxx    \
-    datagramSink.I datagramSink.cxx datagramSink.h          \
-    get_config_path.cxx get_config_path.h               \
-    hashGeneratorBase.I hashGeneratorBase.cxx hashGeneratorBase.h \
-    hashVal.I hashVal.cxx hashVal.h \
-    indent.I indent.cxx indent.h \
-    littleEndian.h memoryUsage.I memoryUsage.cxx    \
-    memoryUsage.h memoryUsagePointers.I memoryUsagePointers.cxx     \
-    memoryUsagePointers.h multifile.I multifile.cxx multifile.h \
-    namable.I namable.cxx namable.h \
-    nativeNumericData.I nativeNumericData.cxx nativeNumericData.h \
-    numeric_types.h             \
-    pointerTo.I pointerTo.h referenceCount.I    \
-    profileTimer.I profileTimer.cxx profileTimer.h  \
-    referenceCount.cxx referenceCount.h \
-    reversedNumericData.I reversedNumericData.cxx reversedNumericData.h \
-    tokenBoard.I tokenBoard.h   \
-    trueClock.I trueClock.cxx trueClock.h typeHandle.I typeHandle.cxx   \
-    typeHandle.h \
-    typedObject.I typedObject.cxx typedObject.h \
-    typedReferenceCount.I typedReferenceCount.cxx       \
-    typedReferenceCount.h typedef.h error_utils.cxx error_utils.h
+  #define SOURCES \
+     bigEndian.h buffer.I buffer.h \
+     checksumHashGenerator.I checksumHashGenerator.h circBuffer.I \
+     circBuffer.h clockObject.I clockObject.h config_express.h \
+     datagram.I datagram.h datagramGenerator.I \
+     datagramGenerator.h datagramInputFile.I datagramInputFile.h \
+     datagramIterator.I datagramIterator.h datagramOutputFile.I \
+     datagramOutputFile.h datagramSink.I datagramSink.h \
+     get_config_path.h hashGeneratorBase.I hashGeneratorBase.h \
+     hashVal.I hashVal.h indent.I indent.h littleEndian.h \
+     memoryUsage.I memoryUsage.h memoryUsagePointers.I \
+     memoryUsagePointers.h multifile.I multifile.h namable.I \
+     namable.h nativeNumericData.I nativeNumericData.h \
+     numeric_types.h pointerTo.I pointerTo.h referenceCount.I \
+     profileTimer.I profileTimer.h referenceCount.h \
+     reversedNumericData.I reversedNumericData.h tokenBoard.I \
+     tokenBoard.h trueClock.I trueClock.h typeHandle.I \
+     typeHandle.h typedObject.I typedObject.h \
+     typedReferenceCount.I typedReferenceCount.h typedef.h \
+     error_utils.h 
+    
+  #define INCLUDED_SOURCES  \
+     buffer.cxx checksumHashGenerator.cxx clockObject.cxx \
+     config_express.cxx datagram.cxx datagramGenerator.cxx \
+     datagramInputFile.cxx datagramIterator.cxx \
+     datagramOutputFile.cxx datagramSink.cxx get_config_path.cxx \
+     hashGeneratorBase.cxx hashVal.cxx indent.cxx memoryUsage.cxx \
+     memoryUsagePointers.cxx multifile.cxx namable.cxx \
+     nativeNumericData.cxx profileTimer.cxx referenceCount.cxx \
+     reversedNumericData.cxx trueClock.cxx typeHandle.cxx \
+     typedObject.cxx typedReferenceCount.cxx error_utils.cxx
 
   #define IF_CRYPTO_SOURCES                         \
     crypto_utils.cxx crypto_utils.h \
@@ -71,7 +72,7 @@
     namable.I namable.h tokenBoard.I patchfile.h patchfile.I        \
     error_utils.h
 
-  #define PRECOMPILED_HEADER express_headers.h 
+//  #define PRECOMPILED_HEADER express_headers.h 
 
   #define IGATESCAN all
 
---
+++ panda/src/express/express_composite2.cxx
@@ -0,0 +1,12 @@
+
+#include "multifile.cxx"
+#include "namable.cxx"
+#include "nativeNumericData.cxx"
+#include "profileTimer.cxx"
+#include "referenceCount.cxx"
+#include "reversedNumericData.cxx"
+#include "trueClock.cxx"
+#include "typeHandle.cxx"
+#include "typedObject.cxx "
+#include "typedReferenceCount.cxx "
+
