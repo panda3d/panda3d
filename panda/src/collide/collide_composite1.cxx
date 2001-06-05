8f3f084a99a913a6977775d72824348aaf572bfc
cxgeorge <>
2001-06-05 21:45:48 +0000

    oops

---
+++ panda/src/collide/collide_composite1.cxx
@@ -0,0 +1,12 @@
+
+#include "config_collide.cxx"
+#include "collisionEntry.cxx"
+#include "collisionHandler.cxx"
+#include "collisionHandlerEvent.cxx"
+#include "collisionHandlerFloor.cxx"
+#include "collisionHandlerPhysical.cxx"
+#include "collisionHandlerPusher.cxx"
+#include "collisionHandlerQueue.cxx"
+#include "collisionLevelState.cxx"
+#include "collisionNode.cxx"
+
---
+++ panda/src/collide/collide_composite2.cxx
@@ -0,0 +1,10 @@
+
+#include "collisionPlane.cxx"
+#include "collisionPolygon.cxx"
+#include "collisionRay.cxx"
+#include "collisionSegment.cxx"
+#include "collisionSolid.cxx"
+#include "collisionSphere.cxx"
+#include "collisionTraverser.cxx"
+
+
