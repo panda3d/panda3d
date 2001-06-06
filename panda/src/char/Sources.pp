b78b500300067f600cd83468c5717223d7526082
cxgeorge <>
2001-06-06 01:16:57 +0000

    use combined srcs

--- panda/src/char/Sources.pp
+++ panda/src/char/Sources.pp
@@ -6,15 +6,20 @@
   #define LOCAL_LIBS \
     chan graph sgraph linmath putil event sgattrib mathutil gsgbase \
     pstatclient    
+    
+  #define COMBINED_SOURCES $[TARGET]_composite1.cxx $[TARGET]_composite2.cxx    
 
   #define SOURCES \
-    character.I character.cxx character.h characterJoint.cxx \
-    characterJoint.h characterJointBundle.I characterJointBundle.cxx \
-    characterJointBundle.h characterSlider.cxx characterSlider.h \
-    computedVertices.I computedVertices.cxx computedVertices.h \
-    computedVerticesMorph.I computedVerticesMorph.cxx \
-    computedVerticesMorph.h config_char.cxx config_char.h \
-    dynamicVertices.cxx dynamicVertices.h
+     character.I character.h characterJoint.h characterJointBundle.I  \
+     characterJointBundle.h characterSlider.h computedVertices.I  \
+     computedVertices.h computedVerticesMorph.I  \
+     computedVerticesMorph.h config_char.h dynamicVertices.h
+    
+  #define INCLUDED_SOURCES \
+     character.cxx characterJoint.cxx characterJointBundle.cxx  \
+     characterSlider.cxx computedVertices.cxx  \
+     computedVerticesMorph.cxx config_char.cxx  \
+     dynamicVertices.cxx
 
   #define INSTALL_HEADERS \
     character.I character.h characterJoint.h characterJointBundle.I \
@@ -22,7 +27,7 @@
     computedVertices.h computedVerticesMorph.I computedVerticesMorph.h \
     config_char.h dynamicVertices.h
     
-  #define PRECOMPILED_HEADER char_headers.h
+//  #define PRECOMPILED_HEADER char_headers.h
 
   #define IGATESCAN all
 
---
+++ panda/src/char/char_composite2.cxx
@@ -0,0 +1,6 @@
+
+#include "characterSlider.cxx"
+#include "computedVertices.cxx"
+#include "computedVerticesMorph.cxx"
+#include "dynamicVertices.cxx"
+
