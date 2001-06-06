63ce60221a76fceb5d118c1a5383295000768427
cxgeorge <>
2001-06-06 20:26:32 +0000

    use combined srcs

--- dtool/src/interrogate/Sources.pp
+++ dtool/src/interrogate/Sources.pp
@@ -2,30 +2,36 @@
 
 #begin bin_target
   #define TARGET interrogate
+  
+  #define COMBINED_SOURCES $[TARGET]_composite1.cxx $[TARGET]_composite2.cxx   
 
   #define SOURCES \
-    interrogate.cxx interrogate.h                                       \
-    interrogateBuilder.cxx interrogateBuilder.h                         \
-    parameterRemap.I parameterRemap.cxx parameterRemap.h                        \
-    parameterRemapBasicStringRefToString.cxx                            \
-    parameterRemapBasicStringRefToString.h                              \
-    parameterRemapBasicStringToString.cxx                               \
-    parameterRemapBasicStringToString.h                                 \
-    parameterRemapCharStarToString.cxx parameterRemapCharStarToString.h \
-    parameterRemapConcreteToPointer.cxx                                 \
-    parameterRemapConcreteToPointer.h parameterRemapConstToNonConst.cxx \
-    parameterRemapConstToNonConst.h parameterRemapEnumToInt.cxx         \
-    parameterRemapEnumToInt.h parameterRemapPTToPointer.cxx             \
-    parameterRemapPTToPointer.h parameterRemapReferenceToConcrete.cxx   \
-    parameterRemapReferenceToConcrete.h                                 \
-    parameterRemapReferenceToPointer.cxx                                        \
-    parameterRemapReferenceToPointer.h parameterRemapThis.cxx           \
-    parameterRemapThis.h parameterRemapToString.cxx                     \
-    parameterRemapToString.h parameterRemapUnchanged.cxx                        \
-    parameterRemapUnchanged.h typeManager.cxx                           \
-    typeManager.h wrapperBuilder.cxx wrapperBuilder.h                   \
-    wrapperBuilderC.cxx wrapperBuilderC.h wrapperBuilderPython.cxx      \
-    wrapperBuilderPython.h
+     interrogate.h interrogateBuilder.h parameterRemap.I  \
+     parameterRemap.h parameterRemapBasicStringRefToString.h  \
+     parameterRemapBasicStringToString.h  \
+     parameterRemapCharStarToString.h  \
+     parameterRemapConcreteToPointer.h  \
+     parameterRemapConstToNonConst.h parameterRemapEnumToInt.h  \
+     parameterRemapPTToPointer.h  \
+     parameterRemapReferenceToConcrete.h  \
+     parameterRemapReferenceToPointer.h parameterRemapThis.h  \
+     parameterRemapToString.h parameterRemapUnchanged.h  \
+     typeManager.h wrapperBuilder.h wrapperBuilderC.h  \
+     wrapperBuilderPython.h      
+
+ #define INCLUDED_SOURCES  \
+     interrogate.cxx interrogateBuilder.cxx parameterRemap.cxx  \
+     parameterRemapBasicStringRefToString.cxx  \
+     parameterRemapBasicStringToString.cxx  \
+     parameterRemapCharStarToString.cxx  \
+     parameterRemapConcreteToPointer.cxx  \
+     parameterRemapConstToNonConst.cxx  \
+     parameterRemapEnumToInt.cxx parameterRemapPTToPointer.cxx  \
+     parameterRemapReferenceToConcrete.cxx  \
+     parameterRemapReferenceToPointer.cxx parameterRemapThis.cxx  \
+     parameterRemapToString.cxx parameterRemapUnchanged.cxx  \
+     typeManager.cxx wrapperBuilder.cxx wrapperBuilderC.cxx  \
+     wrapperBuilderPython.cxx  
 
 #end bin_target
 
