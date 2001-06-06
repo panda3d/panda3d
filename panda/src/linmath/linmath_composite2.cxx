ac1378d156c8bb747020a691a0684bc89eff1a04
cxgeorge <>
2001-06-06 03:08:33 +0000

    use combined srcs

---
+++ panda/src/linmath/linmath_composite2.cxx
@@ -0,0 +1,12 @@
+
+#include "compose_matrix.cxx"
+#include "config_linmath.cxx"
+#include "coordinateSystem.cxx"
+#include "ioPtaDatagramLinMath.cxx"
+#include "lmatrix3.cxx"
+#include "lmatrix4.cxx"
+#include "lmatrix.cxx"
+#include "lorientation.cxx"
+#include "lpoint2.cxx"
+#include "lpoint3.cxx"
+#include "lpoint4.cxx"
