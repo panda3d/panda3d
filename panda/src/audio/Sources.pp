dfc6b42a17c055e1fa18f2fb2e6146b51e8a2c9d
Dave Schuyler <nthparameter@gmail.com>
2001-07-12 20:51:14 +0000

    moved platform stuff to audiotraits

--- panda/src/audio/Sources.pp
+++ panda/src/audio/Sources.pp
@@ -1,27 +1,14 @@
 #define OTHER_LIBS dtoolconfig dtool
 #define DIRECTORY_IF_AUDIO yes
-#define DIRECTORY_IF_IPC yes
-// #define USE_MIKMOD yes
 
 #begin lib_target
-  #define USE_AUDIO yes
-  #define USE_MIKMOD yes
-
   #define TARGET audio
-  #define LOCAL_LIBS putil ipc gui
+  #define LOCAL_LIBS putil gui
 
   #define SOURCES \
     config_audio.cxx config_audio.h \
     audioManager.I audioManager.cxx audioManager.h \
     audioSound.cxx audioSound.h \
-    $[if $[HAVE_SYS_SOUNDCARD_H], \
-      linuxAudioManager.cxx linuxAudioManager.h \
-      linuxAudioSound.cxx linuxAudioSound.h \
-    ,] \
-    $[if $[HAVE_RAD_MSS], \
-      milesAudioManager.cxx milesAudioManager.h \
-      milesAudioSound.I milesAudioSound.cxx milesAudioSound.h \
-    ,] \
     nullAudioManager.cxx nullAudioManager.h \
     nullAudioSound.cxx nullAudioSound.h \
     audio_gui_functor.h audio_gui_functor.cxx
@@ -31,14 +18,6 @@
     audio.h \
     audioManager.h audioManager.I \
     audioSound.h \
-    $[if $[HAVE_SYS_SOUNDCARD_H], \
-      linuxAudioManager.h \
-      linuxAudioSound.h \
-    ,] \
-    $[if $[HAVE_RAD_MSS], \
-      milesAudioManager.h \
-      milesAudioSound.h milesAudioSound.I \
-    ,] \
     nullAudioManager.h \
     nullAudioSound.h \
     audio_gui_functor.h
