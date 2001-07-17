d37e9a2f7df7bd09602ab2ebe6bc4431e86b633c
David Rose <david@ddrose.com>
2001-07-17 19:30:38 +0000

    *** empty log message ***

---
+++ pandaapp/src/pandaappbase/Sources.pp
@@ -0,0 +1,10 @@
+#begin ss_lib_target
+  #define TARGET pandaappbase
+  
+  #define SOURCES \
+    pandaappbase.cxx pandaappbase.h pandaappsymbols.h
+
+  #define INSTALL_HEADERS \
+    pandaappbase.h pandaappsymbols.h
+
+#end ss_lib_target
