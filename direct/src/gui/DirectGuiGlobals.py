42d393ad943f38a6727d42c8c7cc803fc55eaf34
David Rose <david@ddrose.com>
2001-10-24 21:02:43 +0000

    add within/without messages

--- direct/src/gui/DirectGuiGlobals.py
+++ direct/src/gui/DirectGuiGlobals.py
@@ -44,6 +44,11 @@ DESTROY = 'destroy-'
 PRINT = 'print-'
 ENTER = PGButton.getEnterPrefix()
 EXIT = PGButton.getExitPrefix()
+#WITHIN = PGButton.getWithinPrefix()
+#WITHOUT = PGButton.getWithoutPrefix()
+# Temporary for old pandas
+WITHIN = 'within-'
+WITHOUT = 'without-'
 B1CLICK = PGButton.getClickPrefix() + MouseButton.one().getName() + '-'
 B2CLICK = PGButton.getClickPrefix() + MouseButton.two().getName() + '-'
 B3CLICK = PGButton.getClickPrefix() + MouseButton.three().getName() + '-'
