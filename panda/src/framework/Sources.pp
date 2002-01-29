8990f3007e18d204eaf2f6aa7b9c471797b31a7f
cxgeorge <>
2002-01-29 20:42:54 +0000

    add multimon

--- panda/src/framework/Sources.pp
+++ panda/src/framework/Sources.pp
@@ -15,3 +15,18 @@
 
 #end static_lib_target
 
+
+#begin static_lib_target
+  #define TARGET framework_multimon
+  #define LOCAL_LIBS \
+    putil collide loader sgmanip chan text chancfg cull \
+    pnmimage pnmimagetypes event wdxdisplay
+
+  #define SOURCES \
+    config_framework.cxx config_framework.h framework_multimon.cxx framework.h
+
+  #define INSTALL_HEADERS \
+    framework.h
+
+#end static_lib_target
+
