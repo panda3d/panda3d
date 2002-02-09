fe1b0046961515479804b7db4c578a4023ae7f6d
cxgeorge <>
2002-02-09 02:50:56 +0000

    combined srcs

--- pandatool/src/egg-palettize/Sources.pp
+++ pandatool/src/egg-palettize/Sources.pp
@@ -6,29 +6,32 @@
     egg:c loader:c linmath:c putil:c express:c pnmimage:c pnmimagetypes:c \
     pandaegg:m panda:m pandaexpress:m \
     dtoolutil:c dtoolbase:c dconfig:c dtoolconfig:m dtool:m pystub
-  #define UNIX_SYS_LIBS \
-    m
+  #define UNIX_SYS_LIBS m
+  
+  #define COMBINED_SOURCES $[TARGET]_composite1.cxx     
 
   #define SOURCES \
-    config_egg_palettize.cxx config_egg_palettize.h \
-    destTextureImage.cxx destTextureImage.h \
-    eggFile.cxx eggFile.h eggPalettize.cxx eggPalettize.h \
-    filenameUnifier.cxx filenameUnifier.h \
-    imageFile.cxx imageFile.h omitReason.cxx omitReason.h \
-    pal_string_utils.cxx pal_string_utils.h \
-    paletteGroup.h paletteGroup.cxx \
-    paletteGroups.h paletteGroups.cxx paletteImage.h paletteImage.cxx \
-    palettePage.cxx palettePage.h \
-    palettizer.cxx palettizer.h \
-    sourceTextureImage.cxx sourceTextureImage.h \
-    textureImage.cxx textureImage.h \
-    textureMemoryCounter.cxx textureMemoryCounter.h \
-    texturePlacement.cxx texturePlacement.h \
-    texturePosition.cxx texturePosition.h \
-    textureProperties.cxx textureProperties.h textureReference.cxx \
-    textureReference.h textureRequest.h textureRequest.cxx \
-    txaFile.cxx txaFile.h \
-    txaLine.cxx txaLine.h
+     config_egg_palettize.h destTextureImage.h eggFile.h \
+     eggPalettize.h filenameUnifier.h imageFile.h omitReason.h \
+     pal_string_utils.h paletteGroup.h \
+     paletteGroups.h paletteImage.h \
+     palettePage.h palettizer.h sourceTextureImage.h \
+     textureImage.h textureMemoryCounter.h texturePlacement.h \
+     texturePosition.h textureProperties.h \
+     textureReference.h textureRequest.h \
+     txaFile.h txaLine.h 
+
+  #define INCLUDED_SOURCES \
+     config_egg_palettize.cxx destTextureImage.cxx eggFile.cxx \
+     eggPalettize.cxx filenameUnifier.cxx imageFile.cxx \
+     omitReason.cxx pal_string_utils.cxx paletteGroup.cxx \   
+     paletteGroups.cxx paletteImage.cxx palettePage.cxx \
+     palettizer.cxx sourceTextureImage.cxx textureImage.cxx \
+     textureMemoryCounter.cxx texturePlacement.cxx \
+     texturePosition.cxx textureProperties.cxx \
+     textureReference.cxx textureRequest.cxx txaFile.cxx \
+     txaLine.cxx 
+
 
 #end bin_target
 
