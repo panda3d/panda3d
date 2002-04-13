7eb27fc2ec7be3f9ea515dcf20862e277983962b
Dave Schuyler <nthparameter@gmail.com>
2002-04-13 02:45:38 +0000

    added directd

--- direct/metalibs/direct/Sources.pp
+++ direct/metalibs/direct/Sources.pp
@@ -8,7 +8,7 @@
 #define BUILDING_DLL BUILDING_DIRECT
 
 #define COMPONENT_LIBS \
-   directbase dcparser showbase deadrec
+   directbase dcparser showbase deadrec directd
 
 #define OTHER_LIBS panda pandaexpress dtoolconfig dtool
 
