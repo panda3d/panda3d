57ec16d5c1af43538994271814c5fd2be1f96f2e
David Rose <david@ddrose.com>
2002-06-03 22:17:58 +0000

    remove xfile

--- pandatool/src/ptloader/Sources.pp
+++ pandatool/src/ptloader/Sources.pp
@@ -2,7 +2,7 @@
   #define TARGET ptloader
   #define BUILDING_DLL BUILDING_PTLOADER
   #define LOCAL_LIBS \
-    xfile fltegg flt lwoegg lwo mayaegg converter pandatoolbase
+    fltegg flt lwoegg lwo mayaegg converter pandatoolbase
   #define OTHER_LIBS \
     egg2pg:c builder:c egg:c pandaegg:m \
     mathutil:c linmath:c putil:c panda:m \
@@ -12,13 +12,7 @@
     m
 
   // If we've got Maya, link in the Maya libraries.
-  // To link in xfile, we need to also link in the DX libraries.
-  #define USE_PACKAGES maya dx
-
-  #if $[HAVE_DX]
-    // This should have come in through USE_PACKAGES dx.  Fix this.
-    #define WIN_SYS_LIBS d3dxof.lib
-  #endif
+  #define USE_PACKAGES maya
 
   #define SOURCES \
     config_ptloader.cxx config_ptloader.h \
