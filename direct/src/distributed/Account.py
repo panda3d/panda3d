b58582dd892f3e4f827568d6e6c89887d5132249
Joe Shochet <shochet@gmail.com>
2003-01-04 18:46:43 +0000

    *** empty log message ***

---
+++ direct/src/distributed/Account.py
@@ -0,0 +1,12 @@
+"""
+Account module: stub to fulfill the Account toon.dc Distributed Class
+This is a class Roger needs for the server to be able to display these values
+appropriately in the db web interface.
+"""
+
+import DistributedObject
+
+class Account(DistributedObject.DistributedObject):
+    def __init__(self, cr):
+        pass
+
