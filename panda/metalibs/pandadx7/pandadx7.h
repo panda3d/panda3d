0de392dc875e62bbadfac5b7b404f3f86116561f
David Rose <david@ddrose.com>
2003-01-09 22:28:18 +0000

    new window code

---
+++ panda/metalibs/pandadx7/pandadx7.h
@@ -0,0 +1,13 @@
+// Filename: pandadx.h
+// Created by:  drose (2Jan01)
+// 
+////////////////////////////////////////////////////////////////////
+
+#ifndef PANDADX_H
+#define PANDADX_H
+
+#include <pandabase.h>
+
+EXPCL_PANDADX void init_libpandadx();
+
+#endif
