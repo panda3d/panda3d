e1508a193803e235f2618a15f7d776eeb9caf5cf
cxgeorge <>
2003-01-23 02:17:27 +0000

    new dlls

--- pandatool/src/scripts/basedlls.pl
+++ pandatool/src/scripts/basedlls.pl
@@ -12,7 +12,7 @@ $rebase_path="/msvc7/Common7/Tools/bin";
 $ENV{'PATH'}=$rebase_path.":".$ENV{'PATH'};
 
 # note: this is the order dlls will be laid out in memory
-@dll_names=("libpanda","libpandadx","libpandadx7","libpandagl","libpandaexpress","libpandaphysics","libpandafx","libdtool","libdtoolconfig","libpystub","libdirect","libtoontown","libmiles_audio","libpandaegg","libframework");
+@dll_names=("libpanda","libwindisplay","libpandadx8","libpandadx7","libpandagl","libpandaexpress","libpandaphysics","libpandafx","libdtool","libdtoolconfig","libpystub","libdirect","libtoontown","libmiles_audio","libpandaegg","libframework");
 
 # old audio dlls
 #            "libaudio_load_midi","libaudio_load_mp3","libaudio_load_st","libaudio_load_wav",
