df04b7d01e5b37ddf08cc10f4149947105b7cc65
David Rose <david@ddrose.com>
2003-02-07 14:14:25 +0000

    remove accidentally committed file

--- panda/src/audiotraits/fmod_audio_composite.cxx
+++
@@ -1,5 +0,0 @@
-/* Generated automatically by ppremake 1.11 from Sources.pp. */
-/* ################################# DO NOT EDIT ########################### */
-
-#include "fmod_audio_composite1.cxx"
-
