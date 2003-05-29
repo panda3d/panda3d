2eaf4b99dbc1799651459ff52776bf4eb0438a47
David Rose <david@ddrose.com>
2003-05-29 22:41:40 +0000

    document SOCKS error codes

--- panda/src/downloader/httpChannel.cxx
+++ panda/src/downloader/httpChannel.cxx
@@ -1038,6 +1038,20 @@ run_socks_proxy_connect_reply() {
   if (reply[1] != 0x00) {
     downloader_cat.info()
       << "Connection refused, SOCKS code " << (int)reply[1] << "\n";
+    /*
+      Socks error codes (from RFC1928):
+             o  X'00' succeeded
+             o  X'01' general SOCKS server failure
+             o  X'02' connection not allowed by ruleset
+             o  X'03' Network unreachable
+             o  X'04' Host unreachable
+             o  X'05' Connection refused
+             o  X'06' TTL expired
+             o  X'07' Command not supported
+             o  X'08' Address type not supported
+             o  X'09' to X'FF' unassigned
+    */
+
     close_connection();  // connection is now bad.
     _state = S_try_next_proxy;
     return false;
