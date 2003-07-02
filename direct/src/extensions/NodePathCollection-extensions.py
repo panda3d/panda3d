3d916a1ff3924a00c06bdf11325b04a8097ac741
David Rose <david@ddrose.com>
2003-07-02 02:37:28 +0000

    fix problems with newly divided house model

--- direct/src/extensions/NodePathCollection-extensions.py
+++ direct/src/extensions/NodePathCollection-extensions.py
@@ -14,3 +14,21 @@
             for nodePathIndex in range(self.getNumPaths()):
                 npList.append(self.getPath(nodePathIndex))
             return npList
+
+    def getTightBounds(self):
+        import Point3
+        
+        if self.getNumPaths() == 0:
+            return (Point3.Point3(0), Point3.Point3(0))
+
+        v1, v2 = self.getPath(0).getTightBounds()
+        for i in range(1, self.getNumPaths()):
+            v1x, v2x = self.getPath(i).getTightBounds()
+            v1 = Point3.Point3(min(v1[0], v1x[0]),
+                               min(v1[1], v1x[1]),
+                               min(v1[2], v1x[2]))
+            v2 = Point3.Point3(max(v2[0], v2x[0]),
+                               max(v2[1], v2x[1]),
+                               max(v2[2], v2x[2]))
+        
+        return v1, v2
