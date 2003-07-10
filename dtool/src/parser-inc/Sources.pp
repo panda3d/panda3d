67942bfee665d4f751d4376838e21c4c5a345043
David Rose <david@ddrose.com>
2003-07-10 22:26:27 +0000

    add krb5.h

--- dtool/src/parser-inc/Sources.pp
+++ dtool/src/parser-inc/Sources.pp
@@ -2,4 +2,4 @@
     algorithm deque ft2build.h iostream list map memory \
     pair queue set stack stdcompare.h stdtypedefs.h \
     string vector windows.h zlib.h md5.h files.h hex.h \
-    nurbs.hh stddef.h
+    nurbs.hh stddef.h krb5.h
---
+++ dtool/src/parser-inc/krb5.h
@@ -0,0 +1,34 @@
+// Filename: krb5.h
+// Created by:  drose (10Jul03)
+//
+////////////////////////////////////////////////////////////////////
+//
+// PANDA 3D SOFTWARE
+// Copyright (c) 2001, Disney Enterprises, Inc.  All rights reserved
+//
+// All use of this software is subject to the terms of the Panda 3d
+// Software license.  You should have received a copy of this license
+// along with this source code; you will also find a current copy of
+// the license at http://www.panda3d.org/license.txt .
+//
+// To contact the maintainers of this program write to
+// panda3d@yahoogroups.com .
+//
+////////////////////////////////////////////////////////////////////
+
+// This file, and all the other files in this directory, aren't
+// intended to be compiled--they're just parsed by CPPParser (and
+// interrogate) in lieu of the actual system headers, to generate the
+// interrogate database.
+
+#ifndef KRB5_H
+#define KRB5_H
+
+typedef int krb5_enctype;
+typedef int krb5_octet;
+typedef int krb5_error_code;
+
+#define FAR
+
+#endif
+
