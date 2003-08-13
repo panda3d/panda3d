6b908559da57455b2526d87029c9ef8c7673a2a6
David Rose <david@ddrose.com>
2003-08-13 23:57:36 +0000

    remove old Makefile stuff

--- dtool/src/build/Sources.pp
+++ dtool/src/build/Sources.pp
@@ -1,16 +1,4 @@
-#define INSTALL_LIBS \
-    Makefile.a.template Makefile.bin.template Makefile.foreign.template \
-    Makefile.install.template Makefile.meta.template Makefile.o.include \
-    Makefile.o.template Makefile.project.template Makefile.so.template \
-    Makefile.ss.template Makefile.uxb.template
-
 #define INSTALL_HEADERS \
-    Makefile.a.rules Makefile.bin.rules Makefile.bin.vars \
-    Makefile.foreign.rules Makefile.foreign.vars Makefile.install.rules \
-    Makefile.install.vars Makefile.meta.rules Makefile.o.rules \
-    Makefile.o.vars Makefile.project.vars Makefile.so.rules \
-    Makefile.ss.rules Makefile.ss.vars Makefile.uxb.rules \
-    Makefile.penv.vars \
     ctinstmake.pl ctproj.pl ctutils.pl
 
 #define INSTALL_SCRIPTS \
