a1312c6553bf22aea4c656d4836f087841e0b219
David Rose <david@ddrose.com>
2003-08-21 05:34:21 +0000

    remove egg-optchar-new

--- dtool/Config.pp
+++ dtool/Config.pp
@@ -505,9 +505,6 @@
   #define OBJ .o
 #endif
 
-// Temporary, while we migrate egg-optchar.
-#define USE_EGG_OPTCHAR_NEW 1
-
 
 ///////////////////////////////////////////////////////////////////////
 // The following variables are only meaningful when BUILD_TYPE is
