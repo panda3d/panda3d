f196aaf0fe030e8d94a74ae61c5f347eff60ca59
Darren Ranalli <github@dranalli.com>
2003-09-15 21:06:14 +0000

    added LevelMgr

---
+++ direct/src/level/LevelMgr.py
@@ -0,0 +1,13 @@
+"""LevelMgr module: contains the LevelMgr class"""
+
+import Entity
+
+class LevelMgr(Entity.Entity):
+    """This class manages editable client-side level attributes"""
+    def __init__(self, level, entId):
+        Entity.Entity.__init__(self, level, entId)
+        self.initializeEntity()
+        self.callSetters('modelFilename')
+
+    def setModelFilename(self, modelFilename):
+        self.level.modelFilename = modelFilename
---
+++ direct/src/level/LevelMgrAI.py
@@ -0,0 +1,9 @@
+"""LevelMgrAI module: contains the LevelMgrAI class"""
+
+import Entity
+
+class LevelMgrAI(Entity.Entity):
+    """This class manages editable AI level attributes"""
+    def __init__(self, level, entId):
+        Entity.Entity.__init__(self, level, entId)
+        self.initializeEntity()
