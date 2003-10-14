e936606b8da776f64d6c15a4586b0a9575095b47
David Rose <david@ddrose.com>
2003-10-14 21:42:21 +0000

    use composite build

---
+++ pandatool/src/egg-qtess/egg-qtess_composite1.cxx
@@ -0,0 +1,8 @@
+#include "config_egg_qtess.cxx"
+#include "eggQtess.cxx"
+#include "isoPlacer.cxx"
+#include "qtessGlobals.cxx"
+#include "qtessInputEntry.cxx"
+#include "qtessInputFile.cxx"
+#include "qtessSurface.cxx"
+#include "subdivSegment.cxx"
