b4c9aa5cab260b5b1b4702f91d20f3d776d8e8a6
Samir Naik <samir.naik@gmail.com>
2003-12-16 21:23:09 +0000

    moved crate wall collisions up a little

--- direct/src/level/ModelEntity.py
+++ direct/src/level/ModelEntity.py
@@ -27,13 +27,13 @@ class ModelEntity(BasicEntities.NodePathEntity):
         if self.modelPath == "phase_9/models/cogHQ/woodCrateB.bam":
             # move walls down
             cNode = self.find("**/wall")
-            cNode.setZ(-1.25)
+            cNode.setZ(-.75)
             # duplicate the floor and move it down to crate a
             # catch effect for low-hopped toons
             colNode = self.find("**/collision")
             floor = colNode.find("**/floor")
             floor2 = floor.copyTo(colNode)
-            floor2.setZ(-1.25)
+            floor2.setZ(-.75)
 
     def setModelPath(self, path):
         self.modelPath = path
