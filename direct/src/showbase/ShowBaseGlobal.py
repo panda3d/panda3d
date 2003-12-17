9b0379e0162a5c7cdca2c7b9aa2cdad8d0897aea
Darren Ranalli <github@dranalli.com>
2003-12-17 00:21:12 +0000

    only allow __dev__ in __debug__

--- direct/src/showbase/ShowBaseGlobal.py
+++ direct/src/showbase/ShowBaseGlobal.py
@@ -14,3 +14,7 @@ def inspect(anObject):
 
 __builtins__["inspect"] = inspect
 __builtins__["__dev__"] = base.config.GetBool('want-dev', 0)
+# this also appears in AIBaseGlobal
+if (not __debug__) and __dev__:
+    notify = directNotify.newCategory('ShowBaseGlobal')
+    notify.error("You must set 'want-dev' to false in non-debug mode.")
