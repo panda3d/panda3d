b5f8664df220e64fd7dac9d25079da4200dae4c9
Dave Schuyler <nthparameter@gmail.com>
2004-03-23 01:16:39 +0000

    added ifndef __APPLE__

--- dtool/src/parser-inc/stdtypedefs.h
+++ dtool/src/parser-inc/stdtypedefs.h
@@ -23,12 +23,18 @@
 
 #ifndef STDTYPEDEFS_H
 #define STDTYPEDEFS_H
-
+#ifndef __APPLE__
 typedef unsigned int size_t;
 typedef int off_t;
 typedef unsigned int time_t;
-typedef int ptrdiff_t;
 typedef int clock_t;
+
+struct FILE;
+
+namespace std {
+}
+#endif
+typedef int ptrdiff_t;
 typedef int wchar_t;
 
 typedef unsigned int uint;
@@ -37,11 +43,6 @@ typedef unsigned short ushort;
 typedef unsigned char uchar;
 
 #define NULL ((void *)0)
-
-struct FILE;
-
-namespace std {
-}
 
 #endif
 
