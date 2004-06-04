d11dd3ccd4ea1cdb0dc5ff5c44d2ccac1f4f656a
David Rose <david@ddrose.com>
2004-06-04 21:27:02 +0000

    remove hardcoded references from script

--- direct/src/directscripts/Sources.pp
+++ direct/src/directscripts/Sources.pp
@@ -1,3 +1,8 @@
-// For now, since these scripts are not installed anywhere, this file can
-// remain empty.
-#define INSTALL_SCRIPTS runPythonEmacs
+#if $[CTPROJS]
+  // This script is only useful if you're using the ctattach scripts;
+  // therefore, we only bother to install it if you're using the
+  // cattach scripts.
+  #define INSTALL_SCRIPTS runPythonEmacs
+
+#endif
+
