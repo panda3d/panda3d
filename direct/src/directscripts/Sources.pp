679b7ec444f0c15a60d6ecc79efc0a0cd6f9cb4b
David Rose <david@ddrose.com>
2004-06-12 01:26:38 +0000

    don't install runPythonEmacs on Linux

--- direct/src/directscripts/Sources.pp
+++ direct/src/directscripts/Sources.pp
@@ -1,7 +1,7 @@
-#if $[CTPROJS]
-  // This script is only useful if you're using the ctattach scripts;
-  // therefore, we only bother to install it if you're using the
-  // cattach scripts.
+#if $[and $[CTPROJS],$[WINDOWS_PLATFORM]]
+  // This script is only useful if you're using the ctattach script on
+  // Windows; therefore, we only bother to install it if you're using
+  // the cattach scripts.
   #define INSTALL_SCRIPTS runPythonEmacs
 
 #endif
