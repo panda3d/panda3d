9f63d9cbeb4fe2ff986a63584f33038a9ccc4ec4
Darren Ranalli <github@dranalli.com>
2004-06-12 04:09:53 +0000

    render/parentMgr getters

--- direct/src/distributed/DistributedNodeAI.py
+++ direct/src/distributed/DistributedNodeAI.py
@@ -14,6 +14,13 @@ class DistributedNodeAI(DistributedObjectAI.DistributedObjectAI, NodePath):
             self.removeNode()
         DistributedObjectAI.DistributedObjectAI.delete(self)
 
+    def getRender(self):
+        # note that this will return a different node if we change zones
+        return self.air.getRender(self.zoneId)
+
+    def getParentMgr(self):
+        return self.air.getParentMgr(self.zoneId)
+
     ### setParent ###
 
     def b_setParent(self, parentToken):
@@ -38,7 +45,7 @@ class DistributedNodeAI(DistributedObjectAI.DistributedObjectAI, NodePath):
         self.do_setParent(parentToken)
 
     def do_setParent(self, parentToken):
-        self.air.parentMgr.requestReparent(self, parentToken)
+        self.getParentMgr().requestReparent(self, parentToken)
 
     ###### set pos and hpr functions #######
 
