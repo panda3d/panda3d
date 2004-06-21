c7922f53ca42f8ea6c07d544b3f490a774fc27eb
David Rose <david@ddrose.com>
2004-06-21 16:32:43 +0000

    don't build outside vr studio

--- direct/src/leveleditor/Sources.pp
+++ direct/src/leveleditor/Sources.pp
@@ -1,3 +1,15 @@
+// Arguably, this directory doesn't really belong in direct at all,
+// since it's so very Toontown-specific.  The scripts here certainly
+// won't be useful to people outside the VR Studio.
+
+// In the short term, we'll just disable the installation of this
+// directory unless you have the ctattach tools in effect (which
+// generally indicates that you're a member of the VR Studio).
+
+#define BUILD_DIRECTORY $[CTPROJS]
 
 // Install scripts for building zipfiles (leveleditor and RobotToonManager)
-#define INSTALL_SCRIPTS printdir printlib copyfiles zipfiles
+#if $[CTPROJS]
+  #define INSTALL_SCRIPTS printdir printlib copyfiles zipfiles
+#endif
+
