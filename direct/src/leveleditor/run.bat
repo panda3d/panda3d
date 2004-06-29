0eee6f63b6bf338da5f7e0ccd23e241710f29f7a
Mark Mine <mark.mine@disney.com>
2004-06-29 23:20:26 +0000

    match new world order

--- direct/src/leveleditor/run.bat
+++ direct/src/leveleditor/run.bat
@@ -2,4 +2,6 @@ set PATH=lib
 set PYTHONPATH=.;lib-tk
 set TTMODELS=/i/alpha/player/install/ttmodels
 set DMODELS=/i/alpha/player/install/dmodels
-python.exe LevelEditor.py
+set CFG_PATH=.
+set CONFIG_CONFIG=:configpath=CFG_PATH
+python.exe direct\leveleditor\LevelEditor.py
