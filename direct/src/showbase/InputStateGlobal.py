98a25419fa6690dd2d18a2242cd80a3c94fa25e9
Dave Schuyler <nthparameter@gmail.com>
2004-08-07 01:10:49 +0000

    moved avatar controls to direct.controls

--- direct/src/showbase/InputStateGlobal.py
+++ direct/src/showbase/InputStateGlobal.py
@@ -3,6 +3,6 @@
 # This file had to be separated from MessengerGlobal to resolve a
 # circular include dependency with DirectObject.
 
-import InputState
+from direct.controls import InputState
 
 inputState = InputState.InputState()
