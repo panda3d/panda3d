0434436a24451b819295f2db3fe7f05e722f866d
Dave Schuyler <nthparameter@gmail.com>
2004-10-16 00:29:31 +0000

    draft version

---
+++ direct/src/directbase/ThreeUpStart.py
@@ -0,0 +1,35 @@
+
+print 'ThreeUpStart: Starting up environment.'
+
+from pandac.PandaModules import *
+
+from direct.showbase.PythonUtil import *
+from direct.showbase import ThreeUpShow
+ThreeUpShow.ThreeUpShow()
+
+# Put an axis in the world:
+loader.loadModelCopy("models/misc/xyzAxis").reparentTo(render)
+
+if 0:
+    # Hack:
+    # Enable drive mode but turn it off, and reset the camera
+    # This is here because ShowBase sets up a drive interface, this
+    # can be removed if ShowBase is changed to not set that up.
+    base.useDrive()
+    base.disableMouse()
+    if base.mouseInterface:
+        base.mouseInterface.reparentTo(base.dataUnused)
+    if base.mouse2cam:
+        base.mouse2cam.reparentTo(base.dataUnused)
+    # end of hack.
+
+camera.setPosHpr(0, -10.0, 0, 0, 0, 0)
+base.camLens.setFov(52.0)
+base.camLens.setNearFar(1.0, 10000.0)
+
+globalClock.setMaxDt(0.2)
+base.enableParticles()
+base.addAngularIntegrator()
+
+# Force the screen to update:
+base.graphicsEngine.renderFrame()
