28e0aaddeca99ed2611989df1193aece04924797
Darren Ranalli <github@dranalli.com>
2004-12-04 02:03:05 +0000

    moved to /usr/darren/player/otp/src/level

--- direct/src/level/EditMgr.py
+++
@@ -1,7 +0,0 @@
-"""EditMgr module: contains the EditMgr class"""
-
-import EditMgrBase
-
-class EditMgr(EditMgrBase.EditMgrBase):
-    """This class handles client-side editor-specific functionality"""
-    pass
