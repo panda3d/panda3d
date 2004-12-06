4ff080d0d125fc192ad100ae6fad153a79615846
David Rose <david@ddrose.com>
2004-12-06 19:33:57 +0000

    remove old stale code

--- panda/src/shader/shader_composite1.cxx
+++
@@ -1,7 +0,0 @@
-
-#include "casterShader.cxx"
-#include "config_shader.cxx"
-#include "planarReflector.cxx"
-#include "projtexShader.cxx"
-#include "projtexShadower.cxx"
-#include "spotlightShader.cxx"
--- panda/src/shader/shader_composite2.cxx
+++
@@ -1,8 +0,0 @@
-
-#include "shader.cxx"
-#include "outlineShader.cxx"
-#include "shaderTransition.cxx"
-#include "spheretexHighlighter.cxx"
-#include "spheretexReflector.cxx"
-#include "spheretexShader.cxx"
-
