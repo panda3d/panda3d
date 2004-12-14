6495232f82d8debdb8132243723752475e092a32
Darren Ranalli <github@dranalli.com>
2004-12-14 01:08:55 +0000

    large-blob-path defaults to i:\beta\largeblob

--- direct/src/directutil/LargeBlobSenderConsts.py
+++ direct/src/directutil/LargeBlobSenderConsts.py
@@ -7,9 +7,4 @@ ChunkSize = 100
 FilePattern = 'largeBlob.%s'
 
 def getLargeBlobPath():
-    path = config.GetString('large-blob-path', '')
-    if len(path) == 0:
-        assert 0, (
-            'you must config large-blob-path to beta/largeblob, i.e.\n'
-            'large-blob-path i:\\beta\\largeblob')
-    return path
+    return config.GetString('large-blob-path', 'i:\beta\largeblob')
