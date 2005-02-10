1f23a24db3e3439f62fb6fd1ab93bb909f4b9b20
David Rose <david@ddrose.com>
2005-02-10 22:22:24 +0000

    protect DirectSelection stuff from shipping

--- direct/src/particles/Particles.py
+++ direct/src/particles/Particles.py
@@ -126,12 +126,13 @@ class Particles(ParticleSystem.ParticleSystem):
             self.renderer = LineParticleRenderer.LineParticleRenderer()
         elif (type == "GeomParticleRenderer"):
             self.renderer = GeomParticleRenderer.GeomParticleRenderer()
-            npath = NodePath('default-geom')
             # This was moved here because we do not want to download
             # the direct tools with toontown.
-            from direct.directtools import DirectSelection
-            bbox = DirectSelection.DirectBoundingBox(npath)
-            self.renderer.setGeomNode(bbox.lines.node())
+            if __dev__:
+                from direct.directtools import DirectSelection
+                npath = NodePath('default-geom')
+                bbox = DirectSelection.DirectBoundingBox(npath)
+                self.renderer.setGeomNode(bbox.lines.node())
         elif (type == "SparkleParticleRenderer"):
             self.renderer = SparkleParticleRenderer.SparkleParticleRenderer()
         elif (type == "SpriteParticleRenderer"):
