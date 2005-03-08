2828866d5ecdc5996e5f83b4ba98da0c5ae2aab8
Asad M. Zaman <masaduzz@users.sourceforge.net>
2005-03-08 22:21:07 +0000

    missed this new file

---
+++ direct/src/distributed/AccountAI.py
@@ -0,0 +1,12 @@
+"""
+Account module: stub to fulfill the Account toon.dc Distributed Class
+This is a class Roger needs for the server to be able to display these values
+appropriately in the db web interface.
+"""
+
+import DistributedObjectAI
+
+class AccountAI(DistributedObjectAI.DistributedObjectAI):
+    def __init__(self, cr):
+        pass
+
