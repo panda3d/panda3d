0cca4fbe02c0bfe2b27e9c3927ae1e34005b9cb5
David Rose <david@ddrose.com>
2005-05-19 18:36:39 +0000

    add PYTHON_NATIVE

--- dtool/pptempl/Global.pp
+++ dtool/pptempl/Global.pp
@@ -640,13 +640,13 @@ Warning: Variable $[upcase $[tree]]_INSTALL is not set!
     $[CDEFINES_OPT$[OPTIMIZE]:%=-D%] \
     $[filter -D%,$[C++FLAGS]] \
     $[INTERROGATE_OPTIONS] \
-    $[if $[INTERROGATE_PYTHON_INTERFACE],-python] \
+    $[if $[INTERROGATE_PYTHON_INTERFACE],$[if $[PYTHON_NATIVE],-python-native,-python]] \
     $[if $[INTERROGATE_C_INTERFACE],-c] \
     $[if $[TRACK_IN_INTERPRETER],-track-interpreter] \
     $[if $[<= $[OPTIMIZE], 1],-spam]
 
 #defer interrogate_module_options \
-    $[if $[INTERROGATE_PYTHON_INTERFACE],-python] \
+    $[if $[INTERROGATE_PYTHON_INTERFACE],$[if $[PYTHON_NATIVE],-python-native,-python]] \
     $[if $[INTERROGATE_C_INTERFACE],-c] \
     $[if $[TRACK_IN_INTERPRETER],-track-interpreter]
 
