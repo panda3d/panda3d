50a66c57c5b3ecf24b65a02fcf9f5d1be8152b85
Zachary Pavlov <zachary.pavlov@gmail.com>
2005-06-16 22:15:56 +0000

    there we go. getTightBounds should work now for nodepath collections

--- direct/src/extensions_native/NodePathCollection_extensions.py
+++ direct/src/extensions_native/NodePathCollection_extensions.py
@@ -20,7 +20,7 @@ del asList
 #####################################################################3333      
 
 def getTightBounds(self):
-    from pandac import Point3
+    from pandac.PandaModules import Point3
     
     if self.getNumPaths() == 0:
         return (Point3.Point3(0), Point3.Point3(0))
