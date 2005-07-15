565cce77e554b847d2dccd6cb0f98e127d6a16a4
Zachary Pavlov <zachary.pavlov@gmail.com>
2005-07-15 21:28:54 +0000

    Added a function for creating a non repeating random list of number.

--- direct/src/showbase/PythonUtil.py
+++ direct/src/showbase/PythonUtil.py
@@ -50,7 +50,19 @@ def indent(stream, numIndents, str):
     stream.write('    ' * numIndents + str)
 
 
-
+def nonRepeatingRandomList(vals,max):
+    random.seed(time.time())
+    #first generate a set of random values
+    valueList=range(max)
+    finalVals=[]
+    for i in range(vals):
+        index=int(random.random()*len(valueList))
+        finalVals.append(valueList[index])
+        valueList.remove(valueList[index])
+    return finalVals
+
+
+
 def writeFsmTree(instance, indent = 0):
     if hasattr(instance, 'parentFSM'):
         writeFsmTree(instance.parentFSM, indent-2)
@@ -62,7 +74,7 @@ def writeFsmTree(instance, indent = 0):
 
 
 
-
+
 if __debug__:
     class StackTrace:
         def __init__(self, label="", start=0, limit=None):
