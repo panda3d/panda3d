047e9525ff2c9db147a42ef1b8872429ae6e394a
Josh Yelon <jyelon@gmail.com>
2005-09-04 23:25:39 +0000

    More shader stuff

---
+++ panda/src/glstuff/glShaderContext_src.h
@@ -0,0 +1,55 @@
+// Filename: glShaderContext_src.h
+// Created by: jyelon (01Sep05)
+//
+////////////////////////////////////////////////////////////////////
+//
+// PANDA 3D SOFTWARE
+// Copyright (c) 2001 - 2004, Disney Enterprises, Inc.  All rights reserved
+//
+// All use of this software is subject to the terms of the Panda 3d
+// Software license.  You should have received a copy of this license
+// along with this source code; you will also find a current copy of
+// the license at http://etc.cmu.edu/panda3d/docs/license/ .
+//
+// To contact the maintainers of this program write to
+// panda3d-general@lists.sourceforge.net .
+//
+////////////////////////////////////////////////////////////////////
+
+#include "pandabase.h"
+#include "shaderContext.h"
+
+////////////////////////////////////////////////////////////////////
+//       Class : GLShaderContext
+// Description : xyz
+////////////////////////////////////////////////////////////////////
+
+class EXPCL_GL CLP(ShaderContext): public ShaderContext {
+public:
+  INLINE CLP(ShaderContext)(Shader *shader);
+  INLINE ~CLP(ShaderContext)(void);
+
+  void bind(ShaderMode *mode);
+  void unbind(void);
+  void rebind(ShaderMode *oldmode, ShaderMode *newmode);
+
+public:
+  static TypeHandle get_class_type() {
+    return _type_handle;
+  }
+  static void init_type() {
+    TypedObject::init_type();
+    register_type(_type_handle, CLASSPREFIX_QUOTED "ShaderContext",
+                  TypedObject::get_class_type());
+  }
+  virtual TypeHandle get_type() const {
+    return get_class_type();
+  }
+  virtual TypeHandle force_init_type() {init_type(); return get_class_type();}
+
+private:
+  static TypeHandle _type_handle;
+};
+
+#include "glShaderContext_src.I"
+
