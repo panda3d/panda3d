d2e116ce7b49196c83ae74f7e5d485675f1b9add
Dave Schuyler <nthparameter@gmail.com>
2005-11-02 20:43:53 +0000

    commenting old server messages

--- direct/src/distributed/MsgTypes.py
+++ direct/src/distributed/MsgTypes.py
@@ -10,12 +10,12 @@ CLIENT_GO_GET_LOST =                          4
 CLIENT_GET_AVATARS_RESP =                     5
 CLIENT_CREATE_AVATAR =                        6
 CLIENT_CREATE_AVATAR_RESP =                   7
-CLIENT_GET_SHARD_LIST =                       8
-CLIENT_GET_SHARD_LIST_RESP =                  9
+#Roger wants to remove this CLIENT_GET_SHARD_LIST =                       8
+#Roger wants to remove this CLIENT_GET_SHARD_LIST_RESP =                  9
 CLIENT_GET_FRIEND_LIST =                     10
 CLIENT_GET_FRIEND_LIST_RESP =                11
-CLIENT_GET_FRIEND_DETAILS =                  12
-CLIENT_GET_FRIEND_DETAILS_RESP =             13
+#Roger wants to remove this CLIENT_GET_FRIEND_DETAILS =                  12
+#Roger wants to remove this CLIENT_GET_FRIEND_DETAILS_RESP =             13
 CLIENT_GET_AVATAR_DETAILS =                  14
 CLIENT_GET_AVATAR_DETAILS_RESP =             15
 CLIENT_LOGIN_2 =                             16
@@ -42,8 +42,8 @@ CLIENT_REQUEST_GENERATES =                   36
 
 CLIENT_DISCONNECT =                          37
 
-CLIENT_CHANGE_IP_ADDRESS_RESP =              45
-CLIENT_GET_STATE =                           46
+#Roger wants to remove this CLIENT_CHANGE_IP_ADDRESS_RESP =              45
+#Roger wants to remove this CLIENT_GET_STATE =                           46
 CLIENT_GET_STATE_RESP =                      47
 CLIENT_DONE_INTEREST_RESP =                  48
     
@@ -58,8 +58,8 @@ CLIENT_FRIEND_ONLINE =                       53
 CLIENT_FRIEND_OFFLINE =                      54
 CLIENT_REMOVE_FRIEND =                       56
 
-CLIENT_SERVER_UP =                           57
-CLIENT_SERVER_DOWN =                         58
+#Roger wants to remove this CLIENT_SERVER_UP =                           57
+#Roger wants to remove this CLIENT_SERVER_DOWN =                         58
 
 CLIENT_CHANGE_PASSWORD =                     65
 
