00482ececda1d3dc3c7ff7a2c3f3fed994af050f
aignacio_sf <>
2006-01-10 21:48:33 +0000

    Whoops! Needed to use EXPCL_PANDA  instead of EXPCL_PANDADX.

--- panda/src/display/lru.h
+++ panda/src/display/lru.h
@@ -69,7 +69,7 @@ PageTypeStatistics;
 
 typedef bool (*LruPageTypeFunction) (LruPage *lru_page);
 
-class EXPCL_PANDADX LruPage
+class EXPCL_PANDA LruPage
 {
 
 protected:
@@ -150,7 +150,7 @@ public:
 // is not enough memory available, then the lowest priority pages
 // will be paged out first until there is enough memory available.
 ////////////////////////////////////////////////////////////////////
-class EXPCL_PANDADX Lru
+class EXPCL_PANDA Lru
 {
 public:
 
