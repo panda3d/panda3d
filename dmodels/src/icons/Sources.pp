6236e6d2a226ae6a6c441d147a8e85522c93428f
Jason Yeung <jyeung@users.sourceforge.net>
2006-01-26 22:48:33 +0000

    *** empty log message ***

--- dmodels/src/icons/Sources.pp
+++ dmodels/src/icons/Sources.pp
@@ -1,6 +1,15 @@
 #define DIR_TYPE models
 #define INSTALL_TO icons
 
+#define fltfiles $[wildcard *.flt]
+#begin flt_egg
+  #define SOURCES $[fltfiles]
+#end flt_egg
+
+#define mayafiles $[wildcard *.mb]
+#begin maya_egg
+  #define SOURCES $[mayafiles]
+#end maya_egg
 
 #begin install_icons
   #define SOURCES \
@@ -8,3 +17,8 @@
       sphere2.gif tk.gif
 #end install_icons
 
+#begin install_egg
+  #define SOURCES \ 
+    $[fltfiles:%.flt=%.egg] $[mayafiles:%.mb=%.egg] \
+    icon_lightbulb.egg 
+#end install_egg
