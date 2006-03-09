04879e382efe59040e5d5208bbc300267ad1bf16
David Rose <david@ddrose.com>
2006-03-09 20:40:35 +0000

    extra bracket

--- panda/src/osxdisplay/Sources.pp
+++ panda/src/osxdisplay/Sources.pp
@@ -1,4 +1,4 @@
-#define BUILD_DIRECTORY $[IS_OSX]]
+#define BUILD_DIRECTORY $[IS_OSX]
 
 #define OTHER_LIBS interrogatedb:c dconfig:c dtoolconfig:m \
                    dtoolutil:c dtoolbase:c dtool:m prc:c 
