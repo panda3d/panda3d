2de65f7d85de727fb1dc6d9d8ddd4309f0ec9b13
Darren Ranalli <github@dranalli.com>
2006-05-15 20:10:27 +0000

    turn on gc.DEBUG_SAVEALL by default

--- direct/src/distributed/ConnectionRepository.py
+++ direct/src/distributed/ConnectionRepository.py
@@ -65,6 +65,10 @@ class ConnectionRepository(
         # DC file.  The AIRepository will redefine this to 'AI'.
         self.dcSuffix = ''
 
+        if self.config.GetBool('want-debug-leak', 1):
+            import gc
+            gc.set_debug(gc.DEBUG_SAVEALL)
+
     def generateGlobalObject(self, doId, dcname, values=None):
         def applyFieldValues(distObj, dclass, values):
             for i in range(dclass.getNumInheritedFields()):
