a8f09b9631c8a5677be4f412a63a0e343b609104
Josh Yelon <jyelon@gmail.com>
2006-05-18 18:21:06 +0000

    Fixed BufferViewer bug

--- direct/src/showbase/ShowBase.py
+++ direct/src/showbase/ShowBase.py
@@ -280,8 +280,8 @@ class ShowBase(DirectObject.DirectObject):
             self.multiClientSleep = 0
 
         # Offscreen buffer viewing utility.
-        if self.config.GetBool('show-buffers', 0):
-            self.bufferViewer = BufferViewer()
+        # This needs to be allocated even if the viewer is off.
+        self.bufferViewer = BufferViewer()
 
         # Start Tk and DIRECT if specified by Config.prc
         fTk = self.config.GetBool('want-tk', 0)
