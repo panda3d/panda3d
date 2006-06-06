2e005bf840efe3b47bb9b17ae14d5346e6c8b25b
Darren Ranalli <github@dranalli.com>
2006-06-06 02:07:21 +0000

    typo crash fix

--- direct/src/distributed/DistributedNodeAI.py
+++ direct/src/distributed/DistributedNodeAI.py
@@ -60,7 +60,7 @@ class DistributedNodeAI(DistributedObjectAI.DistributedObjectAI, NodePath):
 
     def setParentStr(self, parentToken):
         self.notify.debug('setParentStr(%s): %s' % (self.doId, parentToken))
-        if len(parentTokenStr) > 0:
+        if len(parentToken) > 0:
             self.do_setParent(parentToken)
 
     def setParent(self, parentToken):
