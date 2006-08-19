20688bbae95c021db16b87781b4442bae8c40e5a
Joseph Lee <jtlee@users.sourceforge.net>
2006-08-19 00:40:58 +0000

    do not assume _WIN32_WINNT hasn't been already defined

--- dtool/src/dtoolbase/dtoolbase.h
+++ dtool/src/dtoolbase/dtoolbase.h
@@ -81,6 +81,9 @@
   I'm smart enough to use GetProcAddress for backward compat on
   w95/w98 for newer fns
 */
+#ifdef _WIN32_WINNT
+#undef _WIN32_WINNT
+#endif
 #define _WIN32_WINNT 0x0502
 
 #include "dtoolsymbols.h"
