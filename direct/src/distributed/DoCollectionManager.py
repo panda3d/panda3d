1cf90faf20a3aff0faf415b559cd2589ed13e6e6
Darren Ranalli <github@dranalli.com>
2006-11-08 19:49:17 +0000

    exceptionLogged storeObjectLocation

--- direct/src/distributed/DoCollectionManager.py
+++ direct/src/distributed/DoCollectionManager.py
@@ -191,6 +191,7 @@ class DoCollectionManager:
         else:
             self.notify.warning('handleSetLocation: object %s not present' % self.getMsgChannel())
 
+    @exceptionLogged
     def storeObjectLocation(self, object, parentId, zoneId):
         oldParentId = object.parentId
         oldZoneId = object.zoneId
