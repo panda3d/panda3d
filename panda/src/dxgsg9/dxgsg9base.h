6bb18ad0991bcb909bf974f824ab5db5a2156169
aignacio_sf <>
2006-12-08 22:41:29 +0000

    Add:   bool _supports_rgba16f_texture_format;   bool _supports_rgba32f_texture_format;

--- panda/src/dxgsg9/dxgsg9base.h
+++ panda/src/dxgsg9/dxgsg9base.h
@@ -197,6 +197,8 @@ struct DXScreenData {
   bool _is_dx9_1;
   UINT _supported_screen_depths_mask;
   UINT _supported_tex_formats_mask;
+  bool _supports_rgba16f_texture_format;
+  bool _supports_rgba32f_texture_format;
   D3DCAPS9 _d3dcaps;
   D3DDISPLAYMODE _display_mode;
   D3DPRESENT_PARAMETERS _presentation_params;  // not redundant with _display_mode since width/height must be 0 for windowed mode
