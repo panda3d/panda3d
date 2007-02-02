07776f816bb556abd6de38b8ba2842f3af774af2
aignacio_sf <>
2007-02-02 05:41:55 +0000

    Prevent free twice.

--- direct/src/gui/DirectSlider.py
+++ direct/src/gui/DirectSlider.py
@@ -114,8 +114,9 @@ class DirectSlider(DirectFrame):
             raise ValueError, 'Invalid value for orientation: %s' % (self['orientation'])
 
     def destroy(self):
-        self.thumb.destroy() # ow!
-        del self.thumb
+        if (hasattr(self, 'thumb')):
+            self.thumb.destroy() # ow!
+            del self.thumb
         DirectFrame.destroy(self)
 
     def commandFunc(self):
