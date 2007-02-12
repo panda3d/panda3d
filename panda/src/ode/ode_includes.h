70d5e7c549125eff007ff39bab62a6b702b76f1f
Josh Wilson <hatjosh@users.sourceforge.net>
2007-02-12 20:58:32 +0000

    undef after temporary redef

--- panda/src/ode/ode_includes.h
+++ panda/src/ode/ode_includes.h
@@ -21,16 +21,18 @@
  
 #ifdef int8
   #define temp_ode_int8 int8
+  #undef int8
 #endif
 
 #ifdef int32
   #define temp_ode_int32 int32
+  #undef int32
 #endif
 
 #ifdef uint32
   #define temp_ode_uint32 uint32
+  #undef uint32
 #endif
-
 
 #define int8 ode_int8
 #define int32 ode_int32
@@ -38,6 +40,8 @@
 
 #include "ode/ode.h"
 
+// These are the ones that conflict with other defines in Panda.
+// It may be necessary to add to this list at a later time.
 #undef int8
 #undef int32
 #undef uint32
