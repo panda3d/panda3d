02183867c08634e0ce291bff49d4b7914a003f9a
Darren Ranalli <github@dranalli.com>
2007-02-17 07:34:57 +0000

    don't include recordCreationStack in recorded creation stack

--- direct/src/showbase/PythonUtil.py
+++ direct/src/showbase/PythonUtil.py
@@ -2584,7 +2584,7 @@ def recordCreationStack(cls):
         raise 'recordCreationStack: class \'%s\' must define __init__' % cls.__name__
     cls.__moved_init__ = cls.__init__
     def __recordCreationStack_init__(self, *args, **kArgs):
-        self._creationStackTrace = StackTrace()
+        self._creationStackTrace = StackTrace(start=1)
         return self.__moved_init__(*args, **kArgs)
     def getCreationStackTrace(self):
         return self._creationStackTrace
