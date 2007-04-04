d94f65c29ee084abd2e2968f07b6e859f44cccc4
David Rose <david@ddrose.com>
2007-04-04 01:59:28 +0000

    put WIN32_LEAN_AND_MEAN on command line, for now

--- pandatool/src/win-stats/Sources.pp
+++ pandatool/src/win-stats/Sources.pp
@@ -25,6 +25,7 @@
   #if $[DEVELOP_WINSTATS]
     #define EXTRA_CDEFS $[EXTRA_CDEFS] DEVELOP_WINSTATS
   #endif
+  #define EXTRA_CDEFS $[EXTRA_CDEFS] WIN32_LEAN_AND_MEAN
 
   #define WIN_SYS_LIBS Imm32.lib winmm.lib kernel32.lib oldnames.lib user32.lib gdi32.lib
 
