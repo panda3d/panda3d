ea3d4eea3f9f2d6f5752fa56c7c6d62c099a47be
David Rose <david@ddrose.com>
2007-04-16 22:17:49 +0000

    stub thread functions

--- dtool/src/pystub/pystub.cxx
+++ dtool/src/pystub/pystub.cxx
@@ -19,14 +19,31 @@
 #include "pystub.h"
 
 extern "C" {
+  EXPCL_DTOOLCONFIG int PyArg_Parse(...);
   EXPCL_DTOOLCONFIG int PyArg_ParseTuple(...);
+  EXPCL_DTOOLCONFIG int PyArg_ParseTupleAndKeywords(...);
+  EXPCL_DTOOLCONFIG int PyCFunction_New(...);
+  EXPCL_DTOOLCONFIG int PyCFunction_NewEx(...);
+  EXPCL_DTOOLCONFIG int PyCallable_Check(...);
+  EXPCL_DTOOLCONFIG int PyDict_GetItem(...);
+  EXPCL_DTOOLCONFIG int PyDict_New(...);
+  EXPCL_DTOOLCONFIG int PyDict_SetItem(...);
+  EXPCL_DTOOLCONFIG int PyDict_SetItemString(...);
+  EXPCL_DTOOLCONFIG int PyDict_Size(...);
+  EXPCL_DTOOLCONFIG int PyDict_Type(...);
+  EXPCL_DTOOLCONFIG int PyErr_Clear(...);
+  EXPCL_DTOOLCONFIG int PyErr_Fetch(...);
+  EXPCL_DTOOLCONFIG int PyErr_Format(...);
   EXPCL_DTOOLCONFIG int PyErr_Occurred(...);
   EXPCL_DTOOLCONFIG int PyErr_SetString(...);
+  EXPCL_DTOOLCONFIG int PyEval_RestoreThread(...);
+  EXPCL_DTOOLCONFIG int PyEval_SaveThread(...);
   EXPCL_DTOOLCONFIG int PyExc_TypeError(...);
-  EXPCL_DTOOLCONFIG int PyDict_GetItem(...);
   EXPCL_DTOOLCONFIG int PyFloat_AsDouble(...);
   EXPCL_DTOOLCONFIG int PyFloat_FromDouble(...);
   EXPCL_DTOOLCONFIG int PyFloat_Type(...);
+  EXPCL_DTOOLCONFIG int PyGILState_Ensure(...);
+  EXPCL_DTOOLCONFIG int PyGILState_Release(...);
   EXPCL_DTOOLCONFIG int PyInt_AsLong(...);
   EXPCL_DTOOLCONFIG int PyInt_FromLong(...);
   EXPCL_DTOOLCONFIG int PyInt_Type(...);
@@ -41,12 +58,16 @@ extern "C" {
   EXPCL_DTOOLCONFIG int PyLong_FromUnsignedLong(...);
   EXPCL_DTOOLCONFIG int PyLong_FromUnsignedLongLong(...);
   EXPCL_DTOOLCONFIG int PyLong_Type(...);
+  EXPCL_DTOOLCONFIG int PyModule_AddIntConstant(...);
+  EXPCL_DTOOLCONFIG int PyModule_AddObject(...);
   EXPCL_DTOOLCONFIG int PyNumber_Long(...);
-  EXPCL_DTOOLCONFIG int PyObject_CallObject(...);
-  EXPCL_DTOOLCONFIG int PyObject_CallMethodObjArgs(...);
   EXPCL_DTOOLCONFIG int PyObject_Call(...);
+  EXPCL_DTOOLCONFIG int PyObject_CallMethodObjArgs(...);
+  EXPCL_DTOOLCONFIG int PyObject_CallObject(...);
   EXPCL_DTOOLCONFIG int PyObject_Cmp(...);
   EXPCL_DTOOLCONFIG int PyObject_Free(...);
+  EXPCL_DTOOLCONFIG int PyObject_GenericGetAttr(...);
+  EXPCL_DTOOLCONFIG int PyObject_GenericSetAttr(...);
   EXPCL_DTOOLCONFIG int PyObject_GetAttrString(...);
   EXPCL_DTOOLCONFIG int PyObject_HasAttrString(...);
   EXPCL_DTOOLCONFIG int PyObject_IsInstance(...);
@@ -64,37 +85,22 @@ extern "C" {
   EXPCL_DTOOLCONFIG int PyString_Type(...);
   EXPCL_DTOOLCONFIG int PyTuple_GetItem(...);
   EXPCL_DTOOLCONFIG int PyTuple_New(...);
+  EXPCL_DTOOLCONFIG int PyTuple_Size(...);
+  EXPCL_DTOOLCONFIG int PyTuple_Type(...);
+  EXPCL_DTOOLCONFIG int PyType_GenericAlloc(...);
   EXPCL_DTOOLCONFIG int PyType_IsSubtype(...);
-  EXPCL_DTOOLCONFIG int PyUnicode_Type(...);
+  EXPCL_DTOOLCONFIG int PyType_Ready(...);
   EXPCL_DTOOLCONFIG int PyUnicodeUCS2_FromWideChar(...);
   EXPCL_DTOOLCONFIG int PyUnicodeUCS4_FromWideChar(...);
+  EXPCL_DTOOLCONFIG int PyUnicode_Type(...);
   EXPCL_DTOOLCONFIG int Py_BuildValue(...);
   EXPCL_DTOOLCONFIG int Py_InitModule4(...);
-  EXPCL_DTOOLCONFIG int PyCFunction_New(...);
-  EXPCL_DTOOLCONFIG int PyCFunction_NewEx(...);
-  EXPCL_DTOOLCONFIG int PyDict_SetItemString(...);
-  EXPCL_DTOOLCONFIG int PyDict_SetItem(...);
-  EXPCL_DTOOLCONFIG int PyObject_GenericSetAttr(...);
-  EXPCL_DTOOLCONFIG int PyType_GenericAlloc(...);
-  EXPCL_DTOOLCONFIG int PyModule_AddObject(...);
-  EXPCL_DTOOLCONFIG int _PyObject_Del(...);
-  EXPCL_DTOOLCONFIG int PyDict_New(...);
-  EXPCL_DTOOLCONFIG int PyErr_Clear(...);
-  EXPCL_DTOOLCONFIG int PyType_Ready(...);
-  EXPCL_DTOOLCONFIG int PyTuple_Type(...);
-  EXPCL_DTOOLCONFIG int PyErr_Format(...);
-  EXPCL_DTOOLCONFIG int PyTuple_Size(...);
-  EXPCL_DTOOLCONFIG int PyModule_AddIntConstant(...);
-  EXPCL_DTOOLCONFIG int PyObject_GenericGetAttr(...);
-  EXPCL_DTOOLCONFIG int PyArg_Parse(...);
-  EXPCL_DTOOLCONFIG int PyDict_Type(...);
-  EXPCL_DTOOLCONFIG int PyArg_ParseTupleAndKeywords(...);
-  EXPCL_DTOOLCONFIG int PyDict_Size(...);
   EXPCL_DTOOLCONFIG int Py_InitModule4TraceRefs(...);
   EXPCL_DTOOLCONFIG int _PyObject_DebugFree(...);
+  EXPCL_DTOOLCONFIG int _PyObject_Del(...);
   EXPCL_DTOOLCONFIG int _Py_Dealloc(...);
-  EXPCL_DTOOLCONFIG int _Py_RefTotal(...);
   EXPCL_DTOOLCONFIG int _Py_NegativeRefcount(...);
+  EXPCL_DTOOLCONFIG int _Py_RefTotal(...);
 
   EXPCL_DTOOLCONFIG extern void *PyExc_AssertionError;
   EXPCL_DTOOLCONFIG extern void *_Py_NoneStruct;
@@ -102,16 +108,31 @@ extern "C" {
 };
 
 
-int PyArg_ParseTupleAndKeywords(...) { return 0; }
-int PyDict_Size(...){ return 0; }
+int PyArg_Parse(...) { return 0; };
 int PyArg_ParseTuple(...) { return 0; }
+int PyArg_ParseTupleAndKeywords(...) { return 0; }
+int PyCFunction_New(...) { return 0; };
+int PyCFunction_NewEx(...) { return 0; };
+int PyCallable_Check(...) { return 0; }
+int PyDict_GetItem(...) { return 0; }
+int PyDict_New(...) { return 0; };
+int PyDict_SetItem(...) { return 0; };
+int PyDict_SetItemString(...) { return 0; };
+int PyDict_Size(...){ return 0; }
+int PyDict_Type(...) { return 0; };
+int PyErr_Clear(...) { return 0; };
+int PyErr_Fetch(...) { return 0; }
+int PyErr_Format(...) { return 0; };
 int PyErr_Occurred(...) { return 0; }
 int PyErr_SetString(...) { return 0; }
+int PyEval_RestoreThread(...) { return 0; }
+int PyEval_SaveThread(...) { return 0; }
 int PyExc_TypeError(...) { return 0; }
-int PyDict_GetItem(...) { return 0; }
 int PyFloat_AsDouble(...) { return 0; }
 int PyFloat_FromDouble(...) { return 0; }
 int PyFloat_Type(...) { return 0; }
+int PyGILState_Ensure(...) { return 0; }
+int PyGILState_Release(...) { return 0; }
 int PyInt_AsLong(...) { return 0; }
 int PyInt_FromLong(...) { return 0; }
 int PyInt_Type(...) { return 0; }
@@ -126,12 +147,16 @@ int PyLong_FromLongLong(...) { return 0; }
 int PyLong_FromUnsignedLong(...) { return 0; }
 int PyLong_FromUnsignedLongLong(...) { return 0; }
 int PyLong_Type(...) { return 0; }
+int PyModule_AddIntConstant(...) { return 0; };
+int PyModule_AddObject(...) { return 0; };
 int PyNumber_Long(...) { return 0; }
-int PyObject_CallObject(...) { return 0; }
-int PyObject_CallMethodObjArgs(...) { return 0; }
 int PyObject_Call(...) { return 0; }
+int PyObject_CallMethodObjArgs(...) { return 0; }
+int PyObject_CallObject(...) { return 0; }
 int PyObject_Cmp(...) { return 0; }
 int PyObject_Free(...) { return 0; }
+int PyObject_GenericGetAttr(...) { return 0; };
+int PyObject_GenericSetAttr(...) { return 0; };
 int PyObject_GetAttrString(...) { return 0; }
 int PyObject_HasAttrString(...) { return 0; }
 int PyObject_IsInstance(...) { return 0; }
@@ -149,35 +174,22 @@ int PyString_FromStringAndSize(...) { return 0; }
 int PyString_Type(...) { return 0; }
 int PyTuple_GetItem(...) { return 0; }
 int PyTuple_New(...) { return 0; }
+int PyTuple_Size(...) { return 0; };
+int PyTuple_Type(...) { return 0; };
+int PyType_GenericAlloc(...) { return 0; };
 int PyType_IsSubtype(...) { return 0; }
-int PyUnicode_Type(...) { return 0; }
+int PyType_Ready(...) { return 0; };
 int PyUnicodeUCS2_FromWideChar(...) { return 0; }
 int PyUnicodeUCS4_FromWideChar(...) { return 0; }
+int PyUnicode_Type(...) { return 0; }
 int Py_BuildValue(...) { return 0; }
 int Py_InitModule4(...) { return 0; }
-int PyCFunction_New(...) { return 0; };
-int PyCFunction_NewEx(...) { return 0; };
-int PyDict_SetItemString(...) { return 0; };
-int PyDict_SetItem(...) { return 0; };
-int PyObject_GenericSetAttr(...) { return 0; };
-int PyType_GenericAlloc(...) { return 0; };
-int PyModule_AddObject(...) { return 0; };
-int _PyObject_Del(...) { return 0; };
-int PyDict_New(...) { return 0; };
-int PyErr_Clear(...) { return 0; };
-int PyType_Ready(...) { return 0; };
-int PyTuple_Type(...) { return 0; };
-int PyErr_Format(...) { return 0; };
-int PyTuple_Size(...) { return 0; };
-int PyModule_AddIntConstant(...) { return 0; };
-int PyObject_GenericGetAttr(...) { return 0; };
-int PyArg_Parse(...) { return 0; };
-int PyDict_Type(...) { return 0; };
 int Py_InitModule4TraceRefs(...) { return 0; };
 int _PyObject_DebugFree(...) { return 0; };
+int _PyObject_Del(...) { return 0; };
 int _Py_Dealloc(...) { return 0; };
-int _Py_RefTotal(...) { return 0; };
 int _Py_NegativeRefcount(...) { return 0; };
+int _Py_RefTotal(...) { return 0; };
 
 
 
