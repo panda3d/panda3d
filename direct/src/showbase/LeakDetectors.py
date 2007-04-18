657792498ace1bad9195cad958af7d77ae49e75b
Darren Ranalli <github@dranalli.com>
2007-04-18 09:40:15 +0000

    added GarbageLeakDetector

--- direct/src/showbase/LeakDetectors.py
+++ direct/src/showbase/LeakDetectors.py
@@ -1,6 +1,7 @@
 # objects that report different types of leaks to the ContainerLeakDetector
 
-import __builtin__
+from direct.showbase.PythonUtil import gcDebugOn
+import __builtin__, gc
 
 class LeakDetector:
     def __init__(self):
@@ -11,6 +12,21 @@ class LeakDetector:
         leakDetectors[id(self)] = self
     def destroy(self):
         del leakDetectors[id(self)]
+
+class GarbageLeakDetector(LeakDetector):
+    # are we accumulating Python garbage?
+    def __len__(self):
+        # do a garbage collection
+        wasOn = gcDebugOn()
+        oldFlags = gc.get_debug()
+        if not wasOn:
+            gc.set_debug(gc.DEBUG_SAVEALL)
+        gc.collect()
+        numGarbage = len(gc.garbage)
+        del gc.garbage[:]
+        if not wasOn:
+            gc.set_debug(oldFlags)
+        return numGarbage
 
 class SceneGraphLeakDetector(LeakDetector):
     # is a scene graph leaking nodes?
