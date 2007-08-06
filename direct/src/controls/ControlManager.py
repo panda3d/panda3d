eb08ce9e17688aeea62ce329a7cb47b35aa60ef4
Joe Shochet <shochet@gmail.com>
2007-08-06 09:53:56 +0000

    default inputStateTokens in case controlManager is deleted before ever enabled

--- direct/src/controls/ControlManager.py
+++ direct/src/controls/ControlManager.py
@@ -23,6 +23,7 @@ class ControlManager:
 
     def __init__(self, enable=True):
         assert self.notify.debugCall(id(self))
+        self.inputStateTokens = []
         self.controls = {}
         self.currentControls = None
         self.currentControlsName = None
