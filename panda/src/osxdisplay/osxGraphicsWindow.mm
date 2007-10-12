344a48f43d67d9b463b36a68662e81333c2ab0be
gephilipp <>
2007-10-12 19:33:11 +0000

    *** empty log message ***

--- panda/src/osxdisplay/osxGraphicsWindow.mm
+++ panda/src/osxdisplay/osxGraphicsWindow.mm
@@ -1049,6 +1049,7 @@ bool osxGraphicsWindow::OSOpenWindow(WindowProperties &req_properties)
 			[childWindow orderFront:nil];
 			
 			_properties.set_fixed_size(true);
+			osxdisplay_cat.info() << "Child window created\n";
 		}
 		else
 		{
@@ -1101,6 +1102,7 @@ bool osxGraphicsWindow::OSOpenWindow(WindowProperties &req_properties)
 			InstallWindowEventHandler(_osx_window, gWinEvtHandler, GetEventTypeCount(list), list, (void*)this, NULL); // add event handler
 			ShowWindow (_osx_window);
       
+			osxdisplay_cat.info() << "Event handler installed, now buildGL\n";
 			if(buildGL(false) != noErr)
 			{
 				osxdisplay_cat.error() << " Error In Generate GL \n";
@@ -1112,6 +1114,7 @@ bool osxGraphicsWindow::OSOpenWindow(WindowProperties &req_properties)
 				return false;
 			}
 
+			osxdisplay_cat.info() << "BuildGL complete, set properties\n";
 			//
 			// atach the holder context to the window..
 			//
