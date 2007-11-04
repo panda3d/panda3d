41f81676b186a63c83b0e97797299a440f2a11a2
Josh Yelon <jyelon@gmail.com>
2007-11-04 07:49:00 +0000

    Fixed export class

--- panda/src/movies/webcamVideo.h
+++ panda/src/movies/webcamVideo.h
@@ -26,7 +26,7 @@
 // Description : Allows you to open a webcam or other video capture
 //               device as a video stream.
 ////////////////////////////////////////////////////////////////////
-class EXPCL_PANDASKEL WebcamVideo : public MovieVideo {
+class EXPCL_PANDA_MOVIES WebcamVideo : public MovieVideo {
 
 PUBLISHED:
   virtual ~WebcamVideo();
