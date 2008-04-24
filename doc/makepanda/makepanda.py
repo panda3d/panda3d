fba37e607bfed490b063fff07dc015ca4179410b
Josh Yelon <jyelon@gmail.com>
2008-04-24 13:32:25 +0000

    Fixed a missing sys.exit

--- doc/makepanda/makepanda.py
+++ doc/makepanda/makepanda.py
@@ -915,7 +915,7 @@ srcdir1 = os.path.join(__path__[0], 'src')
 srcdir2 = os.path.join(__path__[0], '..', '..', 'direct', 'src')
 if    (os.path.isdir(srcdir1)): __path__[0] = srcdir1
 elif  (os.path.isdir(srcdir2)): __path__[0] = srcdir2
-else: exit("Cannot find the 'direct' tree")
+else: sys.exit("Cannot find the 'direct' tree")
 """
 
 if (PkgSkip("PYTHON")==0):
