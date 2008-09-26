0830c2e565470326c077e5baa925d683943fe4e7
rdb <git@rdb.name>
2008-09-26 05:51:23 +0000

    Fixed packpanda bug

--- direct/src/directscripts/packpanda.py
+++ direct/src/directscripts/packpanda.py
@@ -247,11 +247,11 @@ def py2pyc(file):
 
 def CompileFiles(file):
     if (os.path.isfile(file)):
-        if (string.endswith(".egg")):
+        if (file.endswith(".egg")):
             egg2bam(file, file[:-4]+'.bam')
-        elif (string.endswith(".egg.pz")):
+        elif (file.endswith(".egg.pz")):
             egg2bam(file, file[:-7]+'.bam')
-        elif (string.endswith(".py")):
+        elif (file.endswith(".py")):
             py2pyc(file)
         else: pass
     elif (os.path.isdir(file)):
