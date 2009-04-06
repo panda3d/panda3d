53a65f3a869474cb08a05a6f0ef3a5d98c54f341
David Rose <david@ddrose.com>
2009-04-06 23:43:28 +0000

    minor typo fix

--- panda/src/egg2pg/config_egg2pg.cxx
+++ panda/src/egg2pg/config_egg2pg.cxx
@@ -59,7 +59,8 @@ ConfigVariableDouble egg_flatten_radius
 ConfigVariableBool egg_unify
 ("egg-unify", true,
  PRC_DESC("When this is true, then in addition to flattening the scene graph "
-          "nodes, the egg loader will also as many Geoms as possible within "
+          "nodes, the egg loader will also combine as many Geoms as "
+          "possible within "
           "a given node into a single Geom.  This has theoretical performance "
           "benefits, especially on higher-end graphics cards, but it also "
           "slightly slows down egg loading."));
