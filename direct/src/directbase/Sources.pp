a944e44c6065b55585b2a09745b1d1d8e5c77029
David Rose <david@ddrose.com>
2009-04-28 01:01:33 +0000

    support for LINK_ALL_STATIC

--- direct/src/directbase/Sources.pp
+++ direct/src/directbase/Sources.pp
@@ -9,4 +9,12 @@
   #define INSTALL_HEADERS \
     directbase.h directsymbols.h
 
+  // These libraries and frameworks are used by dtoolutil; we redefine
+  // them here so they get into the panda build system.
+  #if $[ne $[PLATFORM], FreeBSD]
+    #define UNIX_SYS_LIBS dl
+  #endif
+  #define WIN_SYS_LIBS shell32.lib
+  #define OSX_SYS_FRAMEWORKS Foundation $[if $[not $[BUILD_IPHONE]],AppKit]
+
 #end lib_target
