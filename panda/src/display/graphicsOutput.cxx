a687dd8f964772168732ca051c0db49411a779c7
rdb <git@rdb.name>
2009-05-02 05:46:22 +0000

    nassertv(is_valid());

--- panda/src/display/graphicsOutput.cxx
+++ panda/src/display/graphicsOutput.cxx
@@ -267,6 +267,7 @@ clear_render_textures() {
 void GraphicsOutput::
 add_render_texture(Texture *tex, RenderTextureMode mode,
                    RenderTexturePlane plane) {
+  nassertv(is_valid());
   
   if (mode == RTM_none) {
     return;
