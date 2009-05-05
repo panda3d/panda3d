57afa6826bca1f71adc6bf7bd36f237dac3266fa
Gyedo Jeon <gyedojeon@gmail.com>
2009-05-05 18:50:16 +0000

    Moved toontown specific codes

--- direct/src/leveleditor/Sources.pp
+++ direct/src/leveleditor/Sources.pp
@@ -1,15 +1,3 @@
-// Arguably, this directory doesn't really belong in direct at all,
-// since it's so very Toontown-specific.  The scripts here certainly
-// won't be useful to people outside the VR Studio.
+// Toontown-specific codes have been moved into $TOONTOWN source tree
 
-// In the short term, we'll just disable the installation of this
-// directory unless you have the ctattach tools in effect (which
-// generally indicates that you're a member of the VR Studio).
-
-#define BUILD_DIRECTORY $[CTPROJS]
-
-// Install scripts for building zipfiles (leveleditor and RobotToonManager)
-#if $[CTPROJS]
-  #define INSTALL_SCRIPTS printdir printlib copyfiles copyfiles.pl zipfiles
-#endif
 
