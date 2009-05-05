b13c21055055e0bcddfa851f81ddb82e7cd05373
Gyedo Jeon <gyedojeon@gmail.com>
2009-05-05 18:54:46 +0000

    Removed toontown specific files

--- direct/src/leveleditor/run.bat
+++
@@ -1,7 +0,0 @@
-set PATH=lib
-set PYTHONPATH=.;lib-tk
-set TTMODELS=/i/alpha/player/install/ttmodels
-set DMODELS=/i/alpha/player/install/dmodels
-set CFG_PATH=.
-set CONFIG_CONFIG=:configpath=CFG_PATH
-python.exe direct\leveleditor\LevelEditor.py
