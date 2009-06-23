a65ac4fc0cd0c5f4c4c7e2f0263172493e9cf6e0
David Rose <david@ddrose.com>
2009-06-23 23:32:12 +0000

    missing files

---
+++ direct/src/plugin_npapi/ppDownloadRequest.h
@@ -0,0 +1,44 @@
+// Filename: ppDownloadRequest.h
+// Created by:  drose (23Jun09)
+//
+////////////////////////////////////////////////////////////////////
+//
+// PANDA 3D SOFTWARE
+// Copyright (c) Carnegie Mellon University.  All rights reserved.
+//
+// All use of this software is subject to the terms of the revised BSD
+// license.  You should have received a copy of this license along
+// with this source code in a file named "LICENSE."
+//
+////////////////////////////////////////////////////////////////////
+
+#ifndef PPDOWNLOADREQUEST_H
+#define PPDOWNLOADREQUEST_H
+
+#include "nppanda3d_common.h"
+
+////////////////////////////////////////////////////////////////////
+//       Class : PPDownloadRequest
+// Description : An instance of this object is assigned as the
+//               notifyData for URL requests, to help the plugin
+//               associate streams with requests.
+////////////////////////////////////////////////////////////////////
+class PPDownloadRequest {
+public:
+  enum RequestType {
+    RT_instance_data,
+    RT_core_dll,
+    RT_user
+  };
+
+  inline PPDownloadRequest(RequestType rtype, int user_id = 0);
+
+public:
+  RequestType _rtype;
+  int _user_id;
+};
+
+#include "ppDownloadRequest.I"
+
+#endif
+
