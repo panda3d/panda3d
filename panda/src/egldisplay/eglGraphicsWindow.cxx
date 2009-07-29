ee47d35695c544b473c7a76b403d17624d57e7cf
rdb <git@rdb.name>
2009-07-29 18:49:34 +0000

    Window fix

--- panda/src/egldisplay/eglGraphicsWindow.cxx
+++ panda/src/egldisplay/eglGraphicsWindow.cxx
@@ -669,7 +669,7 @@ open_window() {
   if (!_properties.has_parent_window()) {
     root_window = egl_pipe->get_root();
   } else {
-    root_window = (Window) (int) _properties.get_parent_window();
+    root_window = (Window) _properties.get_parent_window();
   }
 
   setup_colormap(visual_info);
