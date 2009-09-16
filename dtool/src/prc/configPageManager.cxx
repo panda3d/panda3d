63960eaaa80254db313c81268ee425f1cb4ddb86
David Rose <david@ddrose.com>
2009-09-16 20:40:13 +0000

    show-dll-error-dialog

--- dtool/src/prc/configPageManager.cxx
+++ dtool/src/prc/configPageManager.cxx
@@ -394,7 +394,15 @@ reload_implicit_pages() {
 #ifdef WIN32
   // We don't necessarily want an error dialog when we fail to load a
   // .dll file.  But sometimes it is useful for debugging.
-  if (ConfigVariableBool("show-dll-error-dialog", false)) {
+  ConfigVariableBool show_dll_error_dialog
+    ("show-dll-error-dialog", false,
+     PRC_DESC("Set this true to enable the Windows system dialog that pops "
+              "up when a DLL fails to load, or false to disable it.  It is "
+              "normally false, but it may be useful to set it true to debug "
+              "why a DLL is not loading.  (Note that this actually disables "
+              "*all* critical error messages, and that it's a global setting "
+              "that some other libraries might un-set.)"));
+  if (show_dll_error_dialog) {
     SetErrorMode(0);
   } else {
     SetErrorMode(SEM_FAILCRITICALERRORS);
