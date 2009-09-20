cdfc31c4fa2e8bb8a183d1851054969695b0f76d
rdb <git@rdb.name>
2009-09-20 15:18:50 +0000

    Interrogate can do perfectly without those

--- panda/src/ode/odeHelperStructs.h
+++ panda/src/ode/odeHelperStructs.h
@@ -1,18 +1,18 @@
 #ifndef odeHelperStructs_h
 #define odeHelperStructs_h
 
+#ifndef CPPPARSER
 struct sSurfaceParams
     {
         dSurfaceParameters colparams;
         dReal dampen;
     };
-    
+
 struct sBodyParams
     {
 //        int surfaceType;
         dReal dampen;
     };
-    
 #endif
 
-    
+#endif
