1e88d83febeece669d99416a7ee571ff800c5861
rdb <git@rdb.name>
2009-09-24 12:55:21 +0000

    ar.h to Sources.pp

--- dtool/src/parser-inc/Sources.pp
+++ dtool/src/parser-inc/Sources.pp
@@ -19,5 +19,5 @@
     rfftw.h libtar.h malloc.h ucontext.h ctype.h \
     libavutil/avutil.h libavformat/avformat.h \
     libavcodec/avcodec.h libswscale/swscale.h \
-    libavformat/avio.h
+    libavformat/avio.h AR/ar.h
 
