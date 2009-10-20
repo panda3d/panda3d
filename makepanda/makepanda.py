ac0992bc87aa15799b019693bfb1462b9b5978dd
rdb <git@rdb.name>
2009-10-20 08:39:55 +0000

    I don't trust linking against the Python framework for the runtime build.

--- makepanda/makepanda.py
+++ makepanda/makepanda.py
@@ -418,7 +418,11 @@ if (COMPILER=="LINUX"):
     PkgEnable("TINYXML",   "",          ("tinyxml"), "tinyxml.h")
     PkgEnable("VRPN",      "",          ("vrpn", "quat"), ("vrpn", "quat.h", "vrpn/vrpn_Types.h"))
     PkgEnable("ZLIB",      "",          ("z"), "zlib.h")
-    PkgEnable("PYTHON",    "", SDK["PYTHONVERSION"], (SDK["PYTHONVERSION"], SDK["PYTHONVERSION"] + "/Python.h"), tool = SDK["PYTHONVERSION"] + "-config", framework = "Python")
+    if (RUNTIME and sys.platform == "darwin"):
+        # Don't use the framework for the OSX runtime build. I'm afraid it gives problems somewhere.
+        PkgEnable("PYTHON",    "", SDK["PYTHONVERSION"], (SDK["PYTHONVERSION"], SDK["PYTHONVERSION"] + "/Python.h"), tool = SDK["PYTHONVERSION"] + "-config")
+    else:
+        PkgEnable("PYTHON",    "", SDK["PYTHONVERSION"], (SDK["PYTHONVERSION"], SDK["PYTHONVERSION"] + "/Python.h"), tool = SDK["PYTHONVERSION"] + "-config", framework = "Python")
     if (RUNTIME):
         PkgEnable("NPAPI", "",          (), ("xulrunner-*/stable", "xulrunner-*/stable/npapi.h", "nspr/prtypes.h", "nspr"))
         PkgEnable("WX",    tool = "wx-config")
