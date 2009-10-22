df9edbe1ddbc12e7083475c55598f4573b65016b
rdb <git@rdb.name>
2009-10-22 14:01:43 +0000

    Print a somewhat more useful message when the -i option is omitted

--- direct/src/p3d/PackageMerger.py
+++ direct/src/p3d/PackageMerger.py
@@ -79,6 +79,7 @@ class PackageMerger:
         """ Reads the contents.xml file from the indicated sourceDir,
         and updates the internal set of packages appropriately. """
 
+        assert sourceDir != None, "No source directory was specified!"
         contentsFilename = Filename(sourceDir, 'contents.xml')
         doc = TiXmlDocument(contentsFilename.toOsSpecific())
         if not doc.LoadFile():
