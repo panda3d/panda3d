252ab80b3e8fb03529296e08a62f6cc266440724
David Rose <david@ddrose.com>
2010-01-05 00:57:55 +0000

    missing file

---
+++ direct/src/plugin_standalone/p3dEmbed.h
@@ -0,0 +1,38 @@
+// Filename: p3dEmbed.h
+// Created by:  drose (04Jan10)
+//
+////////////////////////////////////////////////////////////////////
+//
+// PANDA 3D SOFTWARE
+// Copyright (c) Carnegie Mellon University.  All rights reserved.
+//
+// All use of this software is subject to the terms of the revised BSD
+// license.  You should have received a copy of this license along
+// with this source code in a file named "LICENSE."
+//
+////////////////////////////////////////////////////////////////////
+
+#ifndef P3DEMBED_H
+#define P3DEMBED_H
+
+#include "panda3dBase.h"
+#include "p3d_plugin.h"
+#include "httpChannel.h"
+#include "ramfile.h"
+#include "fileSpec.h"
+#include "pset.h"
+#include "vector_string.h"
+
+////////////////////////////////////////////////////////////////////
+//       Class : P3DEmbed
+// Description : This program is constructed to self-embed a p3d file
+//               and execute it directly.
+////////////////////////////////////////////////////////////////////
+class P3DEmbed : public Panda3DBase {
+public:
+  P3DEmbed(bool console_environment);
+
+  int run_embedded(streampos read_offset, int argc, char *argv[]);
+};
+
+#endif
