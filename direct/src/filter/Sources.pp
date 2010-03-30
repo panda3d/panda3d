ebb725cbf44d23082d207afc36692626c3cad5e7
rdb <git@rdb.name>
2010-03-30 09:41:55 +0000

    Install *.sha files

---
+++ direct/src/filter/Sources.pp
@@ -0,0 +1,4 @@
+
+#defer install_data_dir $[install_lib_dir]/$[PACKAGE]/$[DIRNAME]
+#define INSTALL_DATA filter-bloomi.sha filter-bloomx.sha filter-bloomy.sha filter-blurx.sha filter-blury.sha filter-copy.sha filter-down4.sha filter-ssao.sha
+
