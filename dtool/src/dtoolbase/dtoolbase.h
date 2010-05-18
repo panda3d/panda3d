2ec0617d32123770e2f705beba3e19cbff36de8d
rdb <git@rdb.name>
2010-05-18 12:57:58 +0000

    Define __STDC_CONSTANT_MACROS in case C libraries like ffmpeg don't

--- dtool/src/dtoolbase/dtoolbase.h
+++ dtool/src/dtoolbase/dtoolbase.h
@@ -59,16 +59,16 @@
 #if _MSC_VER >= 1300
  #if _MSC_VER >= 1310
    #define USING_MSVC7_1
-//#pragma message("VC 7.1")    
+//#pragma message("VC 7.1")
  #else
-//#pragma message("VC 7.0") 
+//#pragma message("VC 7.0")
  #endif
 #define USING_MSVC7
-#else 
+#else
 // #pragma message("VC 6.0")
 #endif
 
-// Use NODEFAULT to optimize a switch() stmt to tell MSVC to automatically go to the final untested case 
+// Use NODEFAULT to optimize a switch() stmt to tell MSVC to automatically go to the final untested case
 // after it has failed all the other cases (i.e. 'assume at least one of the cases is always true')
 #ifdef _DEBUG
 # define NODEFAULT  default: assert(0);
@@ -165,6 +165,9 @@
 #endif
 
 #ifdef PHAVE_STDINT_H
+#if defined(__cplusplus) && !defined(__STDC_LIMIT_MACROS)
+#define __STDC_LIMIT_MACROS 1
+#endif
 #include <stdint.h>
 #endif
 
@@ -181,19 +184,19 @@
 /* Otherwise, if we're not building with the Tau instrumentor, turn
    off all the TAU macros.  We could include the Tau header file to do
    this, but it's better not to assume that Tau is installed. */
-#define TAU_TYPE_STRING(profileString, str) 
-#define TAU_PROFILE(name, type, group) 
+#define TAU_TYPE_STRING(profileString, str)
+#define TAU_PROFILE(name, type, group)
 #define TAU_PROFILE_TIMER(var, name, type, group)
 #define TAU_PROFILE_START(var)
 #define TAU_PROFILE_STOP(var)
-#define TAU_PROFILE_STMT(stmt) 
+#define TAU_PROFILE_STMT(stmt)
 #define TAU_PROFILE_EXIT(msg)
 #define TAU_PROFILE_INIT(argc, argv)
 #define TAU_PROFILE_SET_NODE(node)
 #define TAU_PROFILE_SET_CONTEXT(context)
 #define TAU_PROFILE_SET_GROUP_NAME(newname)
 #define TAU_PROFILE_TIMER_SET_GROUP_NAME(t, newname)
-#define TAU_PROFILE_CALLSTACK()    
+#define TAU_PROFILE_CALLSTACK()
 #define TAU_DB_DUMP()
 #define TAU_DB_PURGE()
 
@@ -211,9 +214,9 @@
 #define TAU_REPORT_STATISTICS()
 #define TAU_REPORT_THREAD_STATISTICS()
 #define TAU_REGISTER_THREAD()
-#define TAU_REGISTER_FORK(id, op) 
-#define TAU_ENABLE_INSTRUMENTATION() 
-#define TAU_DISABLE_INSTRUMENTATION() 
+#define TAU_REGISTER_FORK(id, op)
+#define TAU_ENABLE_INSTRUMENTATION()
+#define TAU_DISABLE_INSTRUMENTATION()
 #define TAU_ENABLE_GROUP(group)
 #define TAU_DISABLE_GROUP(group)
 #define TAU_ENABLE_GROUP_NAME(group)
@@ -231,26 +234,26 @@
 #define TAU_TRACK_MUSE_EVENTS()
 #define TAU_SET_INTERRUPT_INTERVAL(value)
 
-#define TAU_TRACE_SENDMSG(type, destination, length) 
+#define TAU_TRACE_SENDMSG(type, destination, length)
 #define TAU_TRACE_RECVMSG(type, source, length)
 
 #define TAU_MAPPING(stmt, group) stmt
-#define TAU_MAPPING_OBJECT(FuncInfoVar) 
-#define TAU_MAPPING_LINK(FuncInfoVar, Group) 
-#define TAU_MAPPING_PROFILE(FuncInfoVar) 
-#define TAU_MAPPING_CREATE(name, type, key, groupname, tid) 
+#define TAU_MAPPING_OBJECT(FuncInfoVar)
+#define TAU_MAPPING_LINK(FuncInfoVar, Group)
+#define TAU_MAPPING_PROFILE(FuncInfoVar)
+#define TAU_MAPPING_CREATE(name, type, key, groupname, tid)
 #define TAU_MAPPING_PROFILE_TIMER(Timer, FuncInfoVar, tid)
 #define TAU_MAPPING_TIMER_CREATE(t, name, type, gr, group_name)
-#define TAU_MAPPING_PROFILE_START(Timer, tid) 
-#define TAU_MAPPING_PROFILE_STOP(tid) 
-#define TAU_MAPPING_PROFILE_EXIT(msg, tid)  
+#define TAU_MAPPING_PROFILE_START(Timer, tid)
+#define TAU_MAPPING_PROFILE_STOP(tid)
+#define TAU_MAPPING_PROFILE_EXIT(msg, tid)
 #define TAU_MAPPING_DB_DUMP(tid)
 #define TAU_MAPPING_DB_PURGE(tid)
-#define TAU_MAPPING_PROFILE_SET_NODE(node, tid)  
+#define TAU_MAPPING_PROFILE_SET_NODE(node, tid)
 #define TAU_MAPPING_PROFILE_SET_GROUP_NAME(timer, name)
 #define TAU_PROFILE_TIMER_SET_NAME(t, newname)
 #define TAU_PROFILE_TIMER_SET_TYPE(t, newname)
-#define TAU_PROFILE_TIMER_SET_GROUP(t, id) 
+#define TAU_PROFILE_TIMER_SET_GROUP(t, id)
 #define TAU_MAPPING_PROFILE_SET_NAME(timer, name)
 #define TAU_MAPPING_PROFILE_SET_TYPE(timer, name)
 #define TAU_MAPPING_PROFILE_SET_GROUP(timer, id)
@@ -259,15 +262,15 @@
 #define TAU_MAPPING_PROFILE_GET_NAME(timer)
 #define TAU_MAPPING_PROFILE_GET_TYPE(timer)
 
-#define TAU_PHASE(name, type, group) 
-#define TAU_PHASE_CREATE_STATIC(var, name, type, group) 
-#define TAU_PHASE_CREATE_DYNAMIC(var, name, type, group) 
-#define TAU_PHASE_START(var) 
-#define TAU_PHASE_STOP(var) 
-#define TAU_GLOBAL_PHASE(timer, name, type, group) 
-#define TAU_GLOBAL_PHASE_START(timer) 
-#define TAU_GLOBAL_PHASE_STOP(timer)  
-#define TAU_GLOBAL_PHASE_EXTERNAL(timer) 
+#define TAU_PHASE(name, type, group)
+#define TAU_PHASE_CREATE_STATIC(var, name, type, group)
+#define TAU_PHASE_CREATE_DYNAMIC(var, name, type, group)
+#define TAU_PHASE_START(var)
+#define TAU_PHASE_STOP(var)
+#define TAU_GLOBAL_PHASE(timer, name, type, group)
+#define TAU_GLOBAL_PHASE_START(timer)
+#define TAU_GLOBAL_PHASE_STOP(timer)
+#define TAU_GLOBAL_PHASE_EXTERNAL(timer)
 #define TAU_GLOBAL_TIMER(timer, name, type, group)
 #define TAU_GLOBAL_TIMER_EXTERNAL(timer)
 #define TAU_GLOBAL_TIMER_START(timer)
