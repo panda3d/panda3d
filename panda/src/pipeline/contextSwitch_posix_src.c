23208e494c3b17aa5dcbfd36b2114f1d882f3c01
David Rose <david@ddrose.com>
2010-05-29 18:49:48 +0000

    protect against undefined symbol

--- panda/src/pipeline/contextSwitch_posix_src.c
+++ panda/src/pipeline/contextSwitch_posix_src.c
@@ -160,9 +160,8 @@ alloc_thread_context() {
 
   pthread_mutexattr_t attr;
   pthread_mutexattr_init(&attr);
-#ifdef PTHREAD_MUTEX_DEFAULT
-  pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_DEFAULT);
-#endif
+  // The symbol PTHREAD_MUTEX_DEFAULT isn't always available?
+  //  pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_DEFAULT);
   int result = pthread_mutex_init(&context->_ready_mutex, &attr);
   pthread_mutexattr_destroy(&attr);
 
