50990675bdad3dfd51123ab46578c5bb693d507a
David Rose <david@ddrose.com>
2010-06-30 18:33:28 +0000

    repair comment blocks

--- pandaapp/src/indexify/default_font.h
+++ pandaapp/src/indexify/default_font.h
@@ -2,4 +2,21 @@
 // Created by:  drose (04Apr02)
 //
 ////////////////////////////////////////////////////////////////////
+///
+// PANDA 3D SOFTWARE
+// Copyright (c) Carnegie Mellon University.  All rights reserved.
 //
+// All use of this software is subject to the terms of the revised BSD
+// license.  You should have received a copy of this license along
+// with this source code in a file named "LICENSE."
+//
+////////////////////////////////////////////////////////////////////
+
+#ifndef DEFAULT_FONT_H
+#define DEFAULT_FONT_H
+
+extern const unsigned char default_font[];
+extern const int default_font_size;
+
+#endif
+
--- pandaapp/src/pandaappbase/pandaappsymbols.h
+++ pandaapp/src/pandaappbase/pandaappsymbols.h
@@ -3,3 +3,24 @@
  *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *
+ * PANDA 3D SOFTWARE
+ * Copyright (c) Carnegie Mellon University.  All rights reserved.
+ *
+ * All use of this software is subject to the terms of the revised BSD
+ * license.  You should have received a copy of this license along
+ * with this source code in a file named "LICENSE."
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef PANDAAPPSYMBOLS_H
+#define PANDAAPPSYMBOLS_H
+
+/* See dtoolsymbols.h for a rant on the purpose of this file.  */
+
+#if defined(WIN32_VC) && !defined(CPPPARSER) && !defined(LINK_ALL_STATIC)
+
+#else   /* !WIN32_VC */
+
+#endif  /* WIN32_VC */
+
+#endif
