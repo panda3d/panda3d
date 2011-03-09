716d7d0c4d7aeffe4475ad9cc6e633cbb8c647f5
rdb <git@rdb.name>
2011-03-09 12:30:38 +0000

    fltk requires comctl and winsock on windows

--- makepanda/makepanda.py
+++ makepanda/makepanda.py
@@ -3819,7 +3819,7 @@ if (RTDIST or RUNTIME):
       OPTS.append("FLTK")
       TargetAdd('plugin_p3dCert.obj', opts=OPTS, input='p3dCert.cxx')
       TargetAdd('p3dcert.exe', input='plugin_p3dCert.obj')
-      OPTS=['NOSTRIP', 'OPENSSL', 'FLTK']
+      OPTS=['NOSTRIP', 'OPENSSL', 'FLTK', 'WINCOMCTL', 'WINSOCK']
     else:
       OPTS.append("WX")
       TargetAdd('plugin_p3dCert.obj', opts=OPTS, input='p3dCert_wx.cxx')
