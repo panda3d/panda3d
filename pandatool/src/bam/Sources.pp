69738d7975858fcd7dd986ddd7464151a2723072
David Rose <david@ddrose.com>
2011-08-04 00:53:06 +0000

    let's not link with speedtree after all

--- pandatool/src/bam/Sources.pp
+++ pandatool/src/bam/Sources.pp
@@ -1,4 +1,4 @@
-#define USE_PACKAGES fftw cg speedtree
+#define USE_PACKAGES fftw cg
 #define OTHER_LIBS \
   egg2pg:c egg:c pandaegg:m \
   pipeline:c recorder:c parametrics:c collide:c chan:c char:c \
@@ -11,7 +11,6 @@
   $[if $[HAVE_NET],net:c] $[if $[WANT_NATIVE_NET],nativenet:c] \
   $[if $[HAVE_AUDIO],audio:c] \
   panda:m \
-  $[if $[HAVE_SPEEDTREE],pandaspeedtree] \
   pandabase:c express:c pandaexpress:m \
   interrogatedb:c dtoolutil:c dtoolbase:c prc:c dconfig:c \
   dtoolconfig:m dtool:m pystub
--- pandatool/src/bam/bamInfo.cxx
+++ pandatool/src/bam/bamInfo.cxx
@@ -27,10 +27,6 @@
 #include "bamCacheIndex.h"
 #include "pystub.h"
 
-#ifdef HAVE_SPEEDTREE
-#include "config_speedtree.h"
-#endif
-
 ////////////////////////////////////////////////////////////////////
 //     Function: BamInfo::Constructor
 //       Access: Public
@@ -343,10 +339,6 @@ list_hierarchy(PandaNode *node, int indent_level) {
 int main(int argc, char *argv[]) {
   // A call to pystub() to force libpystub.so to be linked in.
   pystub();
-
-#ifdef HAVE_SPEEDTREE
-  init_libspeedtree();
-#endif
 
   BamInfo prog;
   prog.parse_command_line(argc, argv);
