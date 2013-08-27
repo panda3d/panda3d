b3225609b23fcb38edc4d7a810ba72bcb39555ac
Chris Brunner <github@chrisweb.us>
2013-08-27 20:44:27 +0000

    Add a check for 32-bit vs 64-bit OS and then use the appropriate python

--- makepanda/makepanda.bat
+++ makepanda/makepanda.bat
@@ -1,17 +1,24 @@
 @echo off
 
 REM
-REM Verify that we can find the 'makepanda' python script
-REM and the python interpreter.  If we can find both, then
-REM run 'makepanda'.
+REM Check the Windows architecture and determine with Python
+REM to use; 64-bit or 32-bit. Verify that we can find the 
+REM 'makepanda' python script and the python interpreter.
+REM If we can find both, then run 'makepanda'.
 REM
+
+if %PROCESSOR_ARCHITECTURE% == AMD64 (
+  set pythondir=win-python-x64
+) else (
+  set pythondir=win-python
+)
 
 set thirdparty=thirdparty
 if defined MAKEPANDA_THIRDPARTY set thirdparty=%MAKEPANDA_THIRDPARTY%
 
 if not exist makepanda\makepanda.py goto :missing1
-if not exist %thirdparty%\win-python\python.exe goto :missing2
-%thirdparty%\win-python\python.exe makepanda\makepanda.py %*
+if not exist %thirdparty%\%pythondir%\python.exe goto :missing2
+%thirdparty%\%pythondir%\python.exe makepanda\makepanda.py %*
 if errorlevel 1 if x%1 == x--slavebuild exit 1
 goto done
 
@@ -22,6 +29,7 @@ goto done
   goto done
 
 :missing2
+  echo %thirdparty%
   echo You seem to be missing the 'thirdparty' directory.  You probably checked
   echo the source code out from sourceforge.  The sourceforge repository is
   echo missing the 'thirdparty' directory.  You will need to supplement the
