1ca02ae9e1cc0e764814bda733bf87cd9e45729e
Sam Edwards <CFSworks@gmail.com>
2013-10-14 05:29:05 -0600

    Update DOAI a bit: Simplify dependencies on AIR.

--- direct/src/distributed/DistributedObjectAI.py
+++ direct/src/distributed/DistributedObjectAI.py
@@ -144,8 +144,6 @@ class DistributedObjectAI(DistributedObjectBase):
                     barrier.cleanup()
                 self.__barriers = {}
 
-                self.air.stopTrackRequestDeletedDO(self)
-
                 # DCR: I've re-enabled this block of code so that Toontown's
                 # AI won't leak channels.
                 # Let me know if it causes trouble.
@@ -153,10 +151,9 @@ class DistributedObjectAI(DistributedObjectBase):
                 ### block until a solution is thought out of how to prevent
                 ### this delete message or to handle this message better
                 # TODO: do we still need this check?
-                if not hasattr(self, "doNotDeallocateChannel"):
-                    if self.air and not hasattr(self.air, "doNotDeallocateChannel"):
-                        if self.air.minChannel <= self.doId <= self.air.maxChannel:
-                            self.air.deallocateChannel(self.doId)
+                if not getattr(self, "doNotDeallocateChannel", False):
+                    if self.air:
+                        self.air.deallocateChannel(self.doId)
                 self.air = None
 
                 self.parentId = None
@@ -197,9 +194,6 @@ class DistributedObjectAI(DistributedObjectBase):
         of its required fields filled in. Overwrite when needed.
         """
         pass
-
-    def addInterest(self, zoneId, note="", event=None):
-        self.air.addInterest(self.doId, zoneId, note, event)
 
     def b_setLocation(self, parentId, zoneId):
         self.d_setLocation(parentId, zoneId)
@@ -271,9 +265,6 @@ class DistributedObjectAI(DistributedObjectBase):
         self.postGenerateMessage()
         
         dclass.receiveUpdateOther(self, di)
-
-    def sendSetZone(self, zoneId):
-        self.air.sendSetZone(self, zoneId)
 
     def startMessageBundle(self, name):
         self.air.startMessageBundle(name)
@@ -480,7 +471,6 @@ class DistributedObjectAI(DistributedObjectBase):
                 (self.__class__, doId))
             return
         self.air.requestDelete(self)
-        self.air.startTrackRequestDeletedDO(self)
         self._DOAI_requestedDelete = True
 
     def taskName(self, taskString):
