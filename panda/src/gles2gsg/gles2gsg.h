e665c2a908743cc7da381ec0ba7bbf908a235994
rdb <git@rdb.name>
2014-04-22 09:17:23 +0000

    Try using our own version of glext and gl2ext for OpenGL ES

--- panda/src/gles2gsg/gles2gsg.h
+++ panda/src/gles2gsg/gles2gsg.h
@@ -51,10 +51,10 @@
 
 #ifdef IS_OSX
   #include <OpenGLES/ES2/gl.h>
-  #include <OpenGLES/ES2/glext.h>
+//  #include <OpenGLES/ES2/glext.h>
 #else
   #include <GLES2/gl2.h>
-  #include <GLES2/gl2ext.h>
+//  #include <GLES2/gl2ext.h>
 #endif
 
 #include "panda_esgl2ext.h" 
