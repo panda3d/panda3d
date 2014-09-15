0a999d2b6bcb6e991ca72b7f610ceb2bbc6c9409
rdb <git@rdb.name>
2014-09-15 12:51:30 +0000

    Remove two includes that are not supported in 64-bit windows

--- direct/src/plugin_activex/stdafx.h
+++ direct/src/plugin_activex/stdafx.h
@@ -35,10 +35,6 @@
 #include <afxcmn.h>         // MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
-// Delete the two includes below if you do not wish to use the MFC
-//  database classes
-#include <afxdb.h>          // MFC database classes
-#include <afxdao.h>         // MFC DAO database classes
 #include <afxwin.h>
 
 #include <dispex.h>
