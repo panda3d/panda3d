52fec094bcc2fa4a2b38b44e0d48d74673bafdf2
rdb <git@rdb.name>
2015-02-25 16:24:56 +0100

    Automatically coerce Python function to CallbackObject

--- panda/src/putil/callbackObject.h
+++ panda/src/putil/callbackObject.h
@@ -38,6 +38,8 @@ public:
 PUBLISHED:
   virtual void output(ostream &out) const;
 
+  EXTENSION(static PT(CallbackObject) make(PyObject *function));
+
 public:
   virtual void do_callback(CallbackData *cbdata);
 
