411e597aeb466f296ce8f71c679fbd330aa8b65b
Bryan <bryanmuschter@hotmail.com>
2015-05-18 19:27:46 -0400

    we all have drive c

--- direct/src/directutil/LargeBlobSenderConsts.py
+++ direct/src/directutil/LargeBlobSenderConsts.py
@@ -9,4 +9,4 @@ FilePattern = 'largeBlob.%s'
 def getLargeBlobPath():
     # this folder needs to be accessible by everyone that is going to level edit
     # an area as a group
-    return config.GetString('large-blob-path', 'i:\\toontown_in_game_editor_temp')
+    return config.GetString('large-blob-path', 'c:\\toontown_in_game_editor_temp')
