b49289a4f6257e0c35e39c60595f13bc96bcee1c
rdb <git@rdb.name>
2015-05-21 11:16:46 +0200

    Allow setting min-lod, max-lod, lod-bias in egg file

--- panda/src/gobj/texture.h
+++ panda/src/gobj/texture.h
@@ -321,6 +321,7 @@ PUBLISHED:
   INLINE void set_render_to_texture(bool render_to_texture);
 
   INLINE const SamplerState &get_default_sampler() const;
+  INLINE void set_default_sampler(const SamplerState &sampler);
   INLINE SamplerState::WrapMode get_wrap_u() const;
   INLINE SamplerState::WrapMode get_wrap_v() const;
   INLINE SamplerState::WrapMode get_wrap_w() const;
