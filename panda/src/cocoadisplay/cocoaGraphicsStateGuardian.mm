f1df65034de12f8e75afe072964a1aada4c6ec79
rdb <git@rdb.name>
2015-08-07 17:29:03 +0200

    Fix Mac 10.11 compile error

--- panda/src/cocoadisplay/cocoaGraphicsStateGuardian.mm
+++ panda/src/cocoadisplay/cocoaGraphicsStateGuardian.mm
@@ -210,7 +210,7 @@ choose_pixel_format(const FrameBufferProperties &properties,
   attribs.push_back(CGDisplayIDToOpenGLDisplayMask(display));
 
   // End of the array
-  attribs.push_back((NSOpenGLPixelFormatAttribute) nil);
+  attribs.push_back((NSOpenGLPixelFormatAttribute)0);
 
   // Create the format.
   NSOpenGLPixelFormat* format = [[NSOpenGLPixelFormat alloc] initWithAttributes:&attribs[0]];
