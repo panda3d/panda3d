a6e6d4a03f77b6a56936f14c43bf0f30de960c35
Michael <mgracer48@yahoo.com>
2015-10-09 12:13:43 +0200

    delete extensions

--- direct/src/extensions/ConfigVariableFilename-extensions.py
+++
@@ -1,9 +0,0 @@
-
-    def __str__(self):
-        return self.cStr()
-
-    def __len__(self):
-        return self.length()
-    
-    def __getitem__(self, n):
-        return self.cStr().__getitem__(n)
