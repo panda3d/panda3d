6a2e1a438c6ac77e24f7dcd28d577ad30577d37f
rdb <git@rdb.name>
2016-01-02 21:48:25 +0100

    Clamp shininess to 0 to avoid a GL error when negative shininess was entered

--- panda/src/glstuff/glGraphicsStateGuardian_src.cxx
+++ panda/src/glstuff/glGraphicsStateGuardian_src.cxx
@@ -6428,7 +6428,7 @@ do_issue_material() {
 
   call_glMaterialfv(face, GL_SPECULAR, material->get_specular());
   call_glMaterialfv(face, GL_EMISSION, material->get_emission());
-  glMaterialf(face, GL_SHININESS, min(material->get_shininess(), (PN_stdfloat)128.0));
+  glMaterialf(face, GL_SHININESS, max(min(material->get_shininess(), (PN_stdfloat)128), (PN_stdfloat)0));
 
   if (material->has_ambient() && material->has_diffuse()) {
     // The material has both an ambient and diffuse specified.  This
