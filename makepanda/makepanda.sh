dacc0ab7dceff64539d6281d485a45223aa5ecd0
Michael <mgracer48@users.noreply.github.com>
2016-01-12 18:30:42 +0100

    add makepanda.sh
    there is a makepanda.bat time for a unix one

---
+++ makepanda/makepanda.sh
@@ -0,0 +1,3 @@
+echo "Please make sure you have python before running this script if you dont it will error out if you get python command not found install python"
+python makepanda/makepanda.py --everything --installer --no-eigen
+sleep 1
