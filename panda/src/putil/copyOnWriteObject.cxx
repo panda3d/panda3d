b21e8fdf3216b9a18d7f2edef9fc5ce5590f9969
rdb <git@rdb.name>
2016-12-11 15:22:40 +0100

    COW performance tweaks; also somehow fixes Bullet soft body issue

--- panda/src/putil/copyOnWriteObject.cxx
+++ panda/src/putil/copyOnWriteObject.cxx
@@ -35,4 +35,21 @@ unref() const {
   }
   return is_zero;
 }
+
+/**
+ * Explicitly increments the cache reference count only.  Don't use this.
+ *
+ * In the case of a CopyOnWriteObject, when the reference count decrements
+ * down to the cache reference count, the object is implicitly unlocked.
+ */
+void CopyOnWriteObject::
+cache_ref_only() const {
+  MutexHolder holder(_lock_mutex);
+  CachedTypedWritableReferenceCount::cache_ref_only();
+  if (get_ref_count() == get_cache_ref_count()) {
+    ((CopyOnWriteObject *)this)->_lock_status = LS_unlocked;
+    ((CopyOnWriteObject *)this)->_locking_thread = NULL;
+    ((CopyOnWriteObject *)this)->_lock_cvar.notify();
+  }
+}
 #endif  // COW_THREADED
