de598d966b662f71e317cb0be01966b32f9e844c
Mitchell Stokes <mogurijin@gmail.com>
2017-01-05 19:24:51 -0800

    asteroid sample: Add requirements.txt and wheels to setup.py exclude_paths

--- samples/asteroids/setup.py
+++ samples/asteroids/setup.py
@@ -3,7 +3,7 @@ from direct.showutil.dist import *
 setup(
     name="asteroids",
     directories=['.'],
-    exclude_paths=['build', 'setup.py'],
+    exclude_paths=['build', 'setup.py', 'requirements.txt', 'wheels'],
     applications=[Application('main.py', 'asteroids')],
     deploy_platforms=['linux_x86_64'],
 )
