2885a5a58fa99dbbde138a915a109364899ada3a
fireclawthefox <fireclawthefox@gmail.com>
2017-01-30 20:59:24 +0100

    some fixes/enhancements from the scene editor

--- direct/src/tkpanels/ParticlePanel.py
+++ direct/src/tkpanels/ParticlePanel.py
@@ -1886,7 +1886,7 @@ class ParticlePanel(AppShell):
         self.particles.renderer.setLifeScale(lScale)
     # Sprite #
     def setSpriteSourceType(self):
-        if self.rendererSpriteSourceType.get() == 0:
+        if self.rendererSpriteSourceType.get() == SpriteAnim.ST_texture:
             self.rendererSpriteTextureEntry['state'] = 'normal'
             self.rendererSpriteFileEntry['state'] = 'disabled'
             self.rendererSpriteNodeEntry['state'] = 'disabled'
@@ -1982,6 +1982,7 @@ class ParticlePanel(AppShell):
             bMethod = BaseParticleRenderer.PPBLENDCUBIC
         else:
             bMethod = BaseParticleRenderer.PPNOBLEND
+        self.particles.renderer.setAlphaBlendMethod(bMethod)
     def toggleRendererSpriteAlphaDisable(self):
         self.particles.renderer.setAlphaDisable(
             self.getVariable('Sprite Renderer', 'Alpha Disable').get())
