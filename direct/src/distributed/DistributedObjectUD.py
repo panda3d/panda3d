db913cae1187b4d9508a4014f86004780abeed79
PendulumDreams <PendulumDreams@users.noreply.github.com>
2017-03-08 12:10:51 -0700

    Is is faster than ==

--- direct/src/distributed/DistributedObjectUD.py
+++ direct/src/distributed/DistributedObjectUD.py
@@ -64,7 +64,7 @@ class DistributedObjectUD(DistributedObjectBase):
                 flags = []
                 if self.__generated:
                     flags.append("generated")
-                if self.air == None:
+                if self.air is None:
                     flags.append("deleted")
 
                 flagStr = ""
@@ -143,7 +143,7 @@ class DistributedObjectUD(DistributedObjectBase):
         Returns true if the object has been deleted,
         or if it is brand new and hasnt yet been generated.
         """
-        return self.air == None
+        return self.air is None
 
     def isGenerated(self):
         """
@@ -475,7 +475,7 @@ class DistributedObjectUD(DistributedObjectBase):
         avId = self.air.getAvatarIdFromSender()
         assert self.notify.debug('setBarrierReady(%s, %s)' % (context, avId))
         barrier = self.__barriers.get(context)
-        if barrier == None:
+        if barrier is None:
             # This may be None if a client was slow and missed an
             # earlier timeout.  Too bad.
             return
