6d250f2f1f797b106a86c8019814f100aca486cc
Sam Edwards <CFSworks@gmail.com>
2017-03-29 17:38:17 -0600

    fsm: FourState needs to initialize stateIndex BEFORE first debugPrint
    I honestly have no idea how this even worked before.

--- direct/src/fsm/FourState.py
+++ direct/src/fsm/FourState.py
@@ -90,6 +90,7 @@ class FourState:
         off (and so is state 2 which is oposite of 4 and therefore
         oposite of 'on').
         """
+        self.stateIndex = 0
         assert self.debugPrint("FourState(names=%s)"%(names))
         self.track = None
         self.stateTime = 0.0
@@ -120,7 +121,6 @@ class FourState:
                            self.exitState4,
                            [names[1]]),
             }
-        self.stateIndex = 0
         self.fsm = ClassicFSM.ClassicFSM('FourState',
                            list(self.states.values()),
                            # Initial State
