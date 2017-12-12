panda3d/panda3d

commit 6f8b379bda0f0a86f77001ff68b4e304a2b4a5e9

Big patch full of performance improvements This particularly benefits applications with a lot of nodes.

panda/src/pgraph/cullTraverser.cxx
@@ -113,10 +113,8 @@ traverse(const NodePath &root) {
 
     GeometricBoundingVolume *local_frustum = NULL;
     PT(BoundingVolume) bv = _scene_setup->get_lens()->make_bounds();
-    if (bv != (BoundingVolume *)NULL &&
-        bv->is_of_type(GeometricBoundingVolume::get_class_type())) {
-
-      local_frustum = DCAST(GeometricBoundingVolume, bv);
+    if (bv != nullptr) {
+      local_frustum = bv->as_geometric_bounding_volume();
     }
 
     // This local_frustum is in camera space
@@ -199,18 +197,17 @@ traverse_below(CullTraverserData &data) {
   PandaNode::Children children = node_reader->get_children();
   node_reader->release();
   int num_children = children.get_num_children();
-  if (node->has_selective_visibility()) {
+  if (!node->has_selective_visibility()) {
+    for (int i = 0; i < num_children; ++i) {
+      CullTraverserData next_data(data, children.get_child(i));
+      do_traverse(next_data);
+    }
+  } else {
     int i = node->get_first_visible_child();
     while (i < num_children) {
       CullTraverserData next_data(data, children.get_child(i));
       do_traverse(next_data);
       i = node->get_next_visible_child(i);
-    }
-
-  } else {
-    for (int i = 0; i < num_children; i++) {
-      CullTraverserData next_data(data, children.get_child(i));
-      do_traverse(next_data);
     }
   }
 }
@@ -235,12 +232,12 @@ draw_bounding_volume(const BoundingVolume *vol,
   if (bounds_viz != (Geom *)NULL) {
     _geoms_pcollector.add_level(2);
     CullableObject *outer_viz =
-      new CullableObject(bounds_viz, get_bounds_outer_viz_state(),
+      new CullableObject(move(bounds_viz), get_bounds_outer_viz_state(),
                          internal_transform);
     _cull_handler->record_object(outer_viz, this);
 
     CullableObject *inner_viz =
-      new CullableObject(bounds_viz, get_bounds_inner_viz_state(),
+      new CullableObject(move(bounds_viz), get_bounds_inner_viz_state(),
                          internal_transform);
     _cull_handler->record_object(inner_viz, this);
   }
@@ -270,7 +267,7 @@ show_bounds(CullTraverserData &data, bool tight) {
     if (bounds_viz != (Geom *)NULL) {
       _geoms_pcollector.add_level(1);
       CullableObject *outer_viz =
-        new CullableObject(bounds_viz, get_bounds_outer_viz_state(),
+        new CullableObject(move(bounds_viz), get_bounds_outer_viz_state(),
                            internal_transform);
       _cull_handler->record_object(outer_viz, this);
     }
@@ -281,7 +278,7 @@ show_bounds(CullTraverserData &data, bool tight) {
     if (node->is_geom_node()) {
       // Also show the bounding volumes of included Geoms.
       internal_transform = internal_transform->compose(node->get_transform());
-      GeomNode *gnode = DCAST(GeomNode, node);
+      GeomNode *gnode = (GeomNode *)node;
       int num_geoms = gnode->get_num_geoms();
       for (int i = 0; i < num_geoms; ++i) {
         draw_bounding_volume(gnode->get_geom(i)->get_bounds(),
@@ -334,29 +331,31 @@ make_bounds_viz(const BoundingVolume *vol) {
     const BoundingHexahedron *fvol = DCAST(BoundingHexahedron, vol);
 
     PT(GeomVertexData) vdata = new GeomVertexData
-      ("bounds", GeomVertexFormat::get_v3(),
-       Geom::UH_stream);
-    GeomVertexWriter vertex(vdata, InternalName::get_vertex());
+      ("bounds", GeomVertexFormat::get_v3(), Geom::UH_stream);
+    vdata->unclean_set_num_rows(8);
 
-    for (int i = 0; i < 8; ++i ) {
-      vertex.add_data3(fvol->get_point(i));
+    {
+      GeomVertexWriter vertex(vdata, InternalName::get_vertex());
+      for (int i = 0; i < 8; ++i) {
+        vertex.set_data3(fvol->get_point(i));
+      }
     }
 
     PT(GeomLines) lines = new GeomLines(Geom::UH_stream);
-    lines->add_vertices(0, 1); lines->close_primitive();
-    lines->add_vertices(1, 2); lines->close_primitive();
-    lines->add_vertices(2, 3); lines->close_primitive();
-    lines->add_vertices(3, 0); lines->close_primitive();
+    lines->add_vertices(0, 1);
+    lines->add_vertices(1, 2);
+    lines->add_vertices(2, 3);
+    lines->add_vertices(3, 0);
 
-    lines->add_vertices(4, 5); lines->close_primitive();
-    lines->add_vertices(5, 6); lines->close_primitive();
-    lines->add_vertices(6, 7); lines->close_primitive();
-    lines->add_vertices(7, 4); lines->close_primitive();
+    lines->add_vertices(4, 5);
+    lines->add_vertices(5, 6);
+    lines->add_vertices(6, 7);
+    lines->add_vertices(7, 4);
 
-    lines->add_vertices(0, 4); lines->close_primitive();
-    lines->add_vertices(1, 5); lines->close_primitive();
-    lines->add_vertices(2, 6); lines->close_primitive();
-    lines->add_vertices(3, 7); lines->close_primitive();
+    lines->add_vertices(0, 4);
+    lines->add_vertices(1, 5);
+    lines->add_vertices(2, 6);
+    lines->add_vertices(3, 7);
 
     geom = new Geom(vdata);
     geom->add_primitive(lines);
@@ -368,39 +367,29 @@ make_bounds_viz(const BoundingVolume *vol) {
     box.local_object();
 
     PT(GeomVertexData) vdata = new GeomVertexData
-      ("bounds", GeomVertexFormat::get_v3(),
-       Geom::UH_stream);
-    GeomVertexWriter vertex(vdata, InternalName::get_vertex());
+      ("bounds", GeomVertexFormat::get_v3(), Geom::UH_stream);
+    vdata->unclean_set_num_rows(8);
 
-    for (int i = 0; i < 8; ++i ) {
-      vertex.add_data3(box.get_point(i));
+    {
+      GeomVertexWriter vertex(vdata, InternalName::get_vertex());
+      for (int i = 0; i < 8; ++i) {
+        vertex.set_data3(box.get_point(i));
+      }
     }
 
     PT(GeomTriangles) tris = new GeomTriangles(Geom::UH_stream);
     tris->add_vertices(0, 4, 5);
-    tris->close_primitive();
     tris->add_vertices(0, 5, 1);
-    tris->close_primitive();
     tris->add_vertices(4, 6, 7);
-    tris->close_primitive();
     tris->add_vertices(4, 7, 5);
-    tris->close_primitive();
     tris->add_vertices(6, 2, 3);
-    tris->close_primitive();
     tris->add_vertices(6, 3, 7);
-    tris->close_primitive();
     tris->add_vertices(2, 0, 1);
-    tris->close_primitive();
     tris->add_vertices(2, 1, 3);
-    tris->close_primitive();
     tris->add_vertices(1, 5, 7);
-    tris->close_primitive();
     tris->add_vertices(1, 7, 3);
-    tris->close_primitive();
     tris->add_vertices(2, 6, 4);
-    tris->close_primitive();
     tris->add_vertices(2, 4, 0);
-    tris->close_primitive();
 
     geom = new Geom(vdata);
     geom->add_primitive(tris);
@@ -430,19 +419,21 @@ make_tight_bounds_viz(PandaNode *node) const {
                           _current_thread);
   if (found_any) {
     PT(GeomVertexData) vdata = new GeomVertexData
-      ("bounds", GeomVertexFormat::get_v3(),
-      Geom::UH_stream);
-    GeomVertexWriter vertex(vdata, InternalName::get_vertex(),
-                            _current_thread);
+      ("bounds", GeomVertexFormat::get_v3(), Geom::UH_stream);
+    vdata->unclean_set_num_rows(8);
 
-    vertex.add_data3(n[0], n[1], n[2]);
-    vertex.add_data3(n[0], n[1], x[2]);
-    vertex.add_data3(n[0], x[1], n[2]);
-    vertex.add_data3(n[0], x[1], x[2]);
-    vertex.add_data3(x[0], n[1], n[2]);
-    vertex.add_data3(x[0], n[1], x[2]);
-    vertex.add_data3(x[0], x[1], n[2]);
-    vertex.add_data3(x[0], x[1], x[2]);
+    {
+      GeomVertexWriter vertex(vdata, InternalName::get_vertex(),
+                              _current_thread);
+      vertex.set_data3(n[0], n[1], n[2]);
+      vertex.set_data3(n[0], n[1], x[2]);
+      vertex.set_data3(n[0], x[1], n[2]);
+      vertex.set_data3(n[0], x[1], x[2]);
+      vertex.set_data3(x[0], n[1], n[2]);
+      vertex.set_data3(x[0], n[1], x[2]);
+      vertex.set_data3(x[0], x[1], n[2]);
+      vertex.set_data3(x[0], x[1], x[2]);
+    }
 
     PT(GeomLinestrips) strip = new GeomLinestrips(Geom::UH_stream);
 

commit 9bfc425b75c013a656e158b2ee032c51efb30451

Fix assertion when rendering bounding volumes (broken since 6f8b379)

panda/src/pgraph/cullResult.cxx
@@ -105,6 +105,8 @@ add_object(CullableObject *object, const CullTraverser *traverser) {
   static const LColor flash_multisample_color(0.78f, 0.05f, 0.81f, 1.0f);
   static const LColor flash_dual_color(0.92, 0.01f, 0.01f, 1.0f);
 
+  nassertv(object->_draw_callback != nullptr || object->_geom != nullptr);
+
   bool force = !traverser->get_effective_incomplete_render();
   Thread *current_thread = traverser->get_current_thread();
   CullBinManager *bin_manager = CullBinManager::get_global_ptr();

panda/src/pgraph/cullTraverser.cxx
@@ -232,7 +232,7 @@ draw_bounding_volume(const BoundingVolume *vol,
   if (bounds_viz != (Geom *)NULL) {
     _geoms_pcollector.add_level(2);
     CullableObject *outer_viz =
-      new CullableObject(move(bounds_viz), get_bounds_outer_viz_state(),
+      new CullableObject(bounds_viz, get_bounds_outer_viz_state(),
                          internal_transform);
     _cull_handler->record_object(outer_viz, this);
 