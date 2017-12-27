8a94a0d4390c4d769eca417f0ff2dcc8756bd80e
rdb <git@rdb.name>
2017-12-27 22:56:21 +0100

    loader: fix crash getting result from async load

--- panda/src/event/asyncFuture.h
+++ panda/src/event/asyncFuture.h
@@ -86,6 +86,7 @@ PUBLISHED:
   INLINE void set_result(nullptr_t);
   INLINE void set_result(TypedObject *result);
   INLINE void set_result(TypedReferenceCount *result);
+  INLINE void set_result(TypedWritableReferenceCount *result);
   INLINE void set_result(const EventParameter &result);
 
 public:
