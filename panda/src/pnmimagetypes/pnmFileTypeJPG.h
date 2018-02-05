3636339500174901cef3856a0a9d9c9b71f918e1
Sam Edwards <CFSworks@gmail.com>
2018-02-04 17:43:07 -0700

    pnmimagetypes: Get rid of my INLINE macro push/pop now that master has it

--- panda/src/pnmimagetypes/pnmFileTypeJPG.h
+++ panda/src/pnmimagetypes/pnmFileTypeJPG.h
@@ -41,17 +41,11 @@
 #pragma push_macro("INLINE")
 #endif
 
-// jpeglib sometimes redefines the INLINE macro and breaks inlining
-// (on e.g. Ubuntu 12.04) so we need to preserve it somehow - this uses the
-// MSVC push_macro/pop_macro pragmas, which are also supported on GCC and
-// Clang, but may not be cross-compiler enough to merge up to master. Hrm. :/
-#pragma push_macro("INLINE")
 extern "C" {
 #include <stdio.h>  // jpeglib requires this to be included first.
 #include <jpeglib.h>
 #include <setjmp.h>
 }
-#pragma pop_macro("INLINE")
 
 // Restore our own INLINE definition.
 #ifdef __GNUC__
