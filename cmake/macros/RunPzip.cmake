1d666a9841f98f79ade19e9bca77af67a898a42d
Sam Edwards <CFSworks@gmail.com>
2018-02-07 18:19:56 -0700

    CMake: Build the models, makepanda-style

---
+++ cmake/macros/RunPzip.cmake
@@ -0,0 +1,23 @@
+function(run_pzip target_name source destination glob)
+  file(GLOB_RECURSE files RELATIVE "${source}" "${source}/${glob}")
+
+  set(dstfiles "")
+  foreach(srcfile ${files})
+    file(RELATIVE_PATH srcfile_rel "${destination}" "${source}/${srcfile}")
+    file(RELATIVE_PATH dstfile_rel "${destination}" "${destination}/${srcfile}.pz")
+
+    list(APPEND dstfiles "${dstfile_rel}")
+    add_custom_command(OUTPUT "${dstfile_rel}"
+      COMMAND pzip -c > "${dstfile_rel}" < "${srcfile_rel}"
+      WORKING_DIRECTORY "${destination}"
+      DEPENDS pzip
+      COMMENT "")
+
+    get_filename_component(dstdir "${destination}/${dstfile_rel}" DIRECTORY)
+    file(MAKE_DIRECTORY "${dstdir}")
+  endforeach(srcfile)
+
+  add_custom_target(${target_name} ALL
+    DEPENDS ${dstfiles}
+    WORKING_DIRECTORY "${destination}")
+endfunction(run_pzip)
