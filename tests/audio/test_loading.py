7aedc2151035174632a7f3e55be7563f71e65117
Sam Edwards <CFSworks@gmail.com>
2018-02-20 00:55:13 -0700

    tests: Update audio test to recognize missing sounds as NullAudioSound

--- tests/audio/test_loading.py
+++ tests/audio/test_loading.py
@@ -1,6 +1,5 @@
 import pytest
 
-@pytest.mark.xfail
 def test_missing_file(audiomgr):
     sound = audiomgr.get_sound('/not/a/valid/file.ogg')
-    assert sound is None
+    assert str(sound).startswith('NullAudioSound')
