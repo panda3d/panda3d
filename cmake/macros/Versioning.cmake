1401a1cc480cffbee204f9f10a1bb74dd329426f
Sam Edwards <CFSworks@gmail.com>
2018-04-05 16:10:19 -0600

    CMake: Don't use (SO)VERSION for modules

--- cmake/macros/Versioning.cmake
+++ cmake/macros/Versioning.cmake
@@ -1,6 +1,9 @@
 function(add_library target_name)
   _add_library("${target_name}" ${ARGN})
-  set_target_properties("${target_name}" PROPERTIES
-    VERSION "${PROJECT_VERSION}"
-    SOVERSION "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}")
+  get_target_property(type "${target_name}" TYPE)
+  if(type STREQUAL "SHARED_LIBRARY")
+    set_target_properties("${target_name}" PROPERTIES
+      VERSION "${PROJECT_VERSION}"
+      SOVERSION "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}")
+  endif()
 endfunction(add_library)
