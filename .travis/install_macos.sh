09009ca1a4131b2621fff0aff09eedeb0ca1c03d
Donny Lawrence <trea@treamous.com>
2018-05-30 10:05:37 -0500

    Be more specific in which python interpreter we want to run

--- .travis/install_macos.sh
+++ .travis/install_macos.sh
@@ -5,5 +5,5 @@ wxmac opencv assimp libvorbis openssl@1.0 || true
 brew install ode --with-shared
 
 # We can't trust brew to make the right symlinks, so execute commands as modules
-${PYTHON_INTERP:-python3} -m pip install virtualenv
-${PYTHON_INTERP:-python3} -m virtualenv --python=${PYTHON_INTERP:-python3} venv
+/usr/local/bin/${PYTHON_INTERP:-python3} -m pip install virtualenv
+/usr/local/bin/${PYTHON_INTERP:-python3} -m virtualenv --python=${PYTHON_INTERP:-python3} venv
