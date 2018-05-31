a70ccf5d1306cd5721039121de0e2bb10084a282
Donny Lawrence <trea@treamous.com>
2018-05-30 19:16:25 -0500

    CMake won't find the right Python 2.7, so tell it where it is.

--- .travis/install_macos.sh
+++ .travis/install_macos.sh
@@ -5,5 +5,5 @@ wxmac opencv assimp libvorbis openssl@1.0 || true
 brew install ode --with-shared
 
 # We can't trust brew to make the right symlinks, so execute commands as modules
-/usr/local/bin/${PYTHON_INTERP:-python3} -m pip install virtualenv
-/usr/local/bin/${PYTHON_INTERP:-python3} -m virtualenv venv
+${PYTHON_INTERP:-python3} -m pip install virtualenv
+${PYTHON_INTERP:-python3} -m virtualenv venv
