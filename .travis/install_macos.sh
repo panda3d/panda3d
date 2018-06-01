d8f3a669c2f7e3a7aca960f15b8e308d26a4ce20
Donny Lawrence <trea@treamous.com>
2018-06-01 12:47:59 -0500

    Move all the little scripts back into .travis.yml, use less specific paths when searching for openssl and python2.7

--- .travis/install_macos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-brew install python@3 eigen libtar fftw libsquish zlib ffmpeg freetype bullet \
-wxmac opencv assimp libvorbis openssl@1.0 || true
-brew install ode --with-shared
-
-# We can't trust brew to make the right symlinks, so execute commands as modules
-${PYTHON_INTERP:-python3} -m pip install virtualenv
-${PYTHON_INTERP:-python3} -m virtualenv venv
--- .travis/script_macos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ "$PYTHON_INTERP" == "python2.7" ]]; then
-  export PY2_CMAKE_ARGS="-DPYTHON_EXECUTABLE=/usr/local/bin/python -DPYTHON_LIBRARY=/usr/local/opt/python@2/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib"
-fi
-
-cmake -DHAVE_CARBON=NO -DOPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2o_1 \
--DBUILD_METALIBS=$BUILD_METALIBS -DCOMPOSITE_SOURCE_LIMIT=$COMPOSITE_SOURCE_LIMIT \
-$PY2_CMAKE_ARGS ..
