panda3d/panda3d

commit 23bf9ea5c73006a7b561fa2d83af4f568ac54ae6

The direct tree is now valid Python 2 *and* 3

direct/src/showbase/Loader.py
@@ -6,7 +6,6 @@ from panda3d.core import *
 from panda3d.core import Loader as PandaLoader
 from direct.directnotify.DirectNotifyGlobal import *
 from direct.showbase.DirectObject import DirectObject
-import types
 
 # You can specify a phaseChecker callback to check
 # a modelPath to see if it is being loaded in the correct
@@ -139,8 +138,7 @@ class Loader(DirectObject):
         if allowInstance:
             loaderOptions.setFlags(loaderOptions.getFlags() | LoaderOptions.LFAllowInstance)
 
-        if isinstance(modelPath, types.StringTypes) or \
-           isinstance(modelPath, Filename):
+        if not isinstance(modelPath, (tuple, list, set)):
             # We were given a single model pathname.
             modelList = [modelPath]
             if phaseChecker:
@@ -270,8 +268,7 @@ class Loader(DirectObject):
             # Maybe we were given a node
             modelNode = model
 
-        elif isinstance(model, types.StringTypes) or \
-             isinstance(model, Filename):
+        elif isinstance(model, (str, Filename)):
             # If we were given a filename, we have to ask the loader
             # to resolve it for us.
             options = LoaderOptions(LoaderOptions.LFSearch | LoaderOptions.LFNoDiskCache | LoaderOptions.LFCacheOnly)
@@ -284,7 +281,7 @@ class Loader(DirectObject):
             assert Loader.notify.debug("%s resolves to %s" % (model, modelNode.getFullpath()))
 
         else:
-            raise 'Invalid parameter to unloadModel: %s' % (model)
+            raise TypeError('Invalid parameter to unloadModel: %s' % (model))
 
         assert Loader.notify.debug("Unloading model: %s" % (modelNode.getFullpath()))
         ModelPool.releaseModel(modelNode)
@@ -301,8 +298,7 @@ class Loader(DirectObject):
         else:
             loaderOptions = LoaderOptions(loaderOptions)
 
-        if isinstance(modelPath, types.StringTypes) or \
-           isinstance(modelPath, Filename):
+        if not isinstance(modelPath, (tuple, list, set)):
             # We were given a single model pathname.
             modelList = [modelPath]
             nodeList = [node]
@@ -795,12 +791,8 @@ class Loader(DirectObject):
         just as in loadModel(); otherwise, the loading happens before
         loadSound() returns."""
 
-        if isinstance(soundPath, types.StringTypes) or \
-           isinstance(soundPath, Filename):
+        if not isinstance(soundPath, (MovieAudio, tuple, list, set)):
             # We were given a single sound pathname.
-            soundList = [soundPath]
-            gotList = False
-        elif isinstance(soundPath, MovieAudio):
             soundList = [soundPath]
             gotList = False
         else:

commit c55b47338e8b9559921d14636f03fe6847af59d0

Make Loader.loadSound() accept a MovieAudio instance as soundPath
This functionality seems to have inadvertently been removed by
refactoring commit 23bf9ea5.

Fixes: 23bf9ea5c73006a7b561fa2d83af4f568ac54ae6

direct/src/showbase/Loader.py
@@ -934,8 +934,8 @@ class Loader(DirectObject):
         just as in loadModel(); otherwise, the loading happens before
         loadSound() returns."""
 
-        if not isinstance(soundPath, (MovieAudio, tuple, list, set)):
-            # We were given a single sound pathname.
+        if not isinstance(soundPath, (tuple, list, set)):
+            # We were given a single sound pathname or a MovieAudio instance.
             soundList = [soundPath]
             gotList = False
         else: