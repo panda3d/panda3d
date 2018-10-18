cefc000235455c5daca72d74b3c3db6445312d32
Sam Edwards <CFSworks@gmail.com>
2018-10-18 15:56:25 -0600

    CMake: Remove PATHS from FindEigen3
    Why do we need these? CMake checks the "obvious" paths for us.

--- cmake/modules/FindEigen3.cmake
+++ cmake/modules/FindEigen3.cmake
@@ -13,10 +13,7 @@
 if(NOT EIGEN3_INCLUDE_DIR)
   find_path(EIGEN3_INCLUDE_DIR
     NAMES signature_of_eigen3_matrix_library
-    PATHS ${CMAKE_INSTALL_PREFIX}/include
-          ${KDE4_INCLUDE_DIR}
-    PATH_SUFFIXES eigen3 eigen
-  )
+    PATH_SUFFIXES eigen3 eigen)
 
   mark_as_advanced(EIGEN3_INCLUDE_DIR)
 endif()
