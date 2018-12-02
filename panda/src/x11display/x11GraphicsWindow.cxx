ad8a6b4a885f5cf1106c2eb2a1ea2d18a8f0e9f3
Younguk Kim <bluekyu.dev@gmail.com>
2018-12-02 14:44:40 +0900

    display: fix pointer casting in get_pointer()

--- panda/src/x11display/x11GraphicsWindow.cxx
+++ panda/src/x11display/x11GraphicsWindow.cxx
@@ -164,7 +164,7 @@ get_pointer(int device) const {
         double time = ClockObject::get_global_clock()->get_real_time();
         result._xpos = event.xbutton.x;
         result._ypos = event.xbutton.y;
-        ((GraphicsWindowInputDevice &)_input_devices[0]).set_pointer_in_window(result._xpos, result._ypos, time);
+        ((GraphicsWindowInputDevice *)_input_devices[0].p())->set_pointer_in_window(result._xpos, result._ypos, time);
       }
       x11GraphicsPipe::_x_mutex.release();
     }
