8a38337e6bf390ed2ddab7afa8b964be547232af
rdb <git@rdb.name>
2018-12-24 00:17:02 +0100

    makepanda: fix model cache written in current dir on macOS
    This was a regression in 2b537d2263d312672fe4a380fb6f49bda641cd48 that caused the model-cache to be written to ./Library/Caches rather than $HOME/Library/Caches

--- makepanda/makepanda.py
+++ makepanda/makepanda.py
@@ -2934,7 +2934,7 @@ else:
     configprc = configprc.replace("aux-display pandadx9", "")
 
 if (GetTarget() == 'darwin'):
-    configprc = configprc.replace("$XDG_CACHE_HOME/panda3d", "Library/Caches/Panda3D-%s" % MAJOR_VERSION)
+    configprc = configprc.replace("$XDG_CACHE_HOME/panda3d", "$HOME/Library/Caches/Panda3D-%s" % MAJOR_VERSION)
 
     # OpenAL is not yet working well on OSX for us, so let's do this for now.
     configprc = configprc.replace("p3openal_audio", "p3fmod_audio")
