25d3c739b783924890962d0e0a95369a16f24d27
Q Misell <q@misell.cymru>
2019-02-28 12:03:16 +0000

    gitignore

--- dtool/src/parser-inc/opencv2/core/core.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef _CV_H_
-#define _CV_H_
-
-typedef struct CvCapture CvCapture;
-
-typedef struct CvMemStorage CvMemStorage;
-typedef struct CvHaarClassifierCascade CvHaarClassifierCascade;
-#endif
