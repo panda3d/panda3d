945d8bf4ab93b293bf6befd96a4561e3619e10c4
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2019-03-14 14:03:57 +0100

    Added all CollisionSolids except FloorMesh Refactored a bit

--- samples/collision/collision_tester.py
+++ samples/collision/collision_tester.py
@@ -7,6 +7,8 @@ from panda3d.core import CollisionNode
 from panda3d.core import CollisionTraverser
 from panda3d.core import CollisionHandlerQueue
 
+from panda3d.core import Plane
+#from panda3d.core import Parabolaf
 from panda3d.core import CollisionBox
 from panda3d.core import CollisionCapsule
 from panda3d.core import CollisionFloorMesh
@@ -23,64 +25,87 @@ from panda3d.core import CollisionTube
 from direct.showbase.ShowBase import ShowBase
 
 
+FROM = 1
+INTO = 2
+
 class CollisionTester(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
         self.disable_mouse()
         self.accept("escape", sys.exit)
+        self.cam.set_pos(0, -20, 10)
+        self.cam.look_at(0, 0, 0)
 
-        self.from_np = self.render.attach_new_node("from")
-        self.from_np.set_pos(5, 0, 0)
-        self.from_model = self.loader.load_model("models/zup-axis")
-        self.from_model.reparent_to(self.from_np)
-        self.from_model.set_scale(0.1)
-        self.from_coll = CollisionNode("from_coll")
-        self.from_coll.add_solid(CollisionSphere((0, 0, 0), 1))
-        self.from_coll_np = self.from_np.attach_new_node(self.from_coll)
-        self.from_coll_np.show()
-        self.task_mgr.add(self.wobble_from, "wobble_from")
-        self.solid_from = [("", )]
-
-        self.into_np = self.render.attach_new_node("from")
-        self.into_np.set_pos(0, 0, 0)
-        self.into_model = self.loader.load_model("models/zup-axis")
-        self.into_model.reparent_to(self.into_np)
-        self.into_model.set_scale(0.1)
-        self.into_coll = CollisionNode("into_coll")
-        self.into_coll.add_solid(CollisionSphere((0, 0, 0), 1))
-        self.into_coll_np = self.into_np.attach_new_node(self.into_coll)
-        self.into_coll_np.show()
-        self.task_mgr.add(self.wobble_into, "wobble_into")
-        self.solid_into = [
-            ("Sphere", CollisionSphere((0, 0, 0), 1)),
-            ("Box", CollisionBox((-0.5, -0.5, -0.5), (0.5, 0.5, 0.5))),
-            ]
-        #self.
+        solids_from = [
+            ("Sphere", CollisionSphere((0, 0, 0), 1.5)),
+            ("Capsule", CollisionCapsule((-1, 0, 0), (1, 0, 0), 1)),
+            ("Box", CollisionBox((-1, -1, -1), (1, 1, 1))),
+            ("Segment", CollisionSegment((-1, 0, 0), (1, 0, 0))),
+            ("Ray", CollisionRay((0, 0, 0), (-1, 0, 0))),
+            ("Line", CollisionSegment((-1, 0, 0), (1, 0, 0))),
+            #("Parabola", CollisionParabola(
+            #    Parabolaf((0, 0, -1), (1, 0, 5), (0, 0, 0)),
+            #    0, 5,
+            #)),
+        ]
+        solids_into = [
+            ("Sphere", CollisionSphere((0, 0, 0), 1.5)),
+            ("Capsule", CollisionCapsule((-1, 0, 0), (1, 0, 0), 1)),
+            ("Box", CollisionBox((-1, -1, -1), (1, 1, 1))),
+            ("Plane", CollisionPlane(Plane((1, 0, 0), (0, 0, 0)))),
+            ("InvSphere", CollisionInvSphere((0, 0, 0), 2)),
+        ]
+        self.from_object = Collidable(solids_from, 0, FROM)
+        self.into_object = Collidable(solids_into, 0, INTO)
+        self.accept("1", self.from_object.switch_solid)
+        self.accept("2", self.into_object.switch_solid)
 
         self.traverser = CollisionTraverser("traverser")
         self.traverser.show_collisions(self.render)
         self.queue = CollisionHandlerQueue()
-        self.traverser.add_collider(self.from_coll_np, self.queue)
+        self.traverser.add_collider(self.from_object.get_collider(), self.queue)
         self.task_mgr.add(self.collide, "collide")
-
-        self.cam.set_pos(0, -20, 10)
-        self.cam.look_at(0, 0, 0)
-
-    def wobble_from(self, task):
-        self.from_np.set_x(sin((task.time*2*pi)/5) * 5)
-        return task.cont
-
-    def wobble_into(self, task):
-        self.into_np.set_hpr(
-            task.time*360/10,
-            task.time*360/20,
-            task.time*360/30,
-        )
-        return task.cont
 
     def collide(self, task):
         self.traverser.traverse(self.render)
         return task.cont
+
+
+class Collidable:
+    def __init__(self, solids, solid_id, wobble_type):
+        self.solids = solids
+        self.solid_id = solid_id
+        self.wobble_type = wobble_type
+
+        self.np = base.render.attach_new_node("from")
+        self.model = base.loader.load_model("models/zup-axis")
+        self.model.reparent_to(self.np)
+        self.model.set_scale(0.1)
+        self.coll = CollisionNode("coll")
+        self.coll.add_solid(self.solids[self.solid_id][1])
+        self.coll_np = self.np.attach_new_node(self.coll)
+        self.coll_np.show()
+        base.task_mgr.add(self.wobble, "wobble")
+
+    def get_collider(self):
+        return self.coll_np
+
+    def switch_solid(self):
+        self.coll.remove_solid(0)
+        self.solid_id = (self.solid_id + 1) % len(self.solids)
+        self.coll.add_solid(self.solids[self.solid_id][1])
+
+    def wobble(self, task):
+        if self.wobble_type == FROM:
+            self.np.set_x(sin((task.time*2*pi)/10) * 5)
+        elif self.wobble_type == INTO:
+            self.np.set_hpr(
+                task.time*360/10,
+                task.time*360/20,
+                task.time*360/30,
+            )
+        return task.cont
+
 
 colltester = CollisionTester()
 colltester.run()
