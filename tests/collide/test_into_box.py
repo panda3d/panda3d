7b6a3c2b68fb8062a063f3c47d58059397f64efe
hecris <christopher.he33@myhunter.cuny.edu>
2019-03-21 16:47:30 -0400

    add tests for no collisions and collisions just on the edge between two solids

--- tests/collide/test_into_box.py
+++ tests/collide/test_into_box.py
@@ -2,10 +2,17 @@ from collisions import *
 
 
 def test_sphere_into_box():
-    sphere = CollisionSphere(0, 0, 0, 3)
+    sphere = CollisionSphere(0, 0, 4, 3)
     box = CollisionBox((0, 0, 0), 2, 3, 4)
-
-    entry, np_from, np_into = make_collision(sphere, box)
+    entry = make_collision(sphere, box)[0]
     assert entry is not None
     assert entry.get_from() == sphere
     assert entry.get_into() == box
+
+    # Colliding just on the edge
+    entry, np_from, np_into = make_collision(CollisionSphere(0, 0, 10, 6), box)
+    assert entry.get_surface_point(np_from) == Point3(0, 0, 4)
+
+    # No collision
+    entry = make_collision(CollisionSphere(100, 100, 100, 100), box)[0]
+    assert entry is None
--- tests/collide/test_into_poly.py
+++ tests/collide/test_into_poly.py
@@ -5,17 +5,33 @@ def test_box_into_poly():
     box = CollisionBox((0, 0, 0), 2, 3, 4)
     poly = CollisionPolygon(Point3(0, 0, 0), Point3(0, 0, 1), Point3(0, 1, 1), Point3(0, 1, 0))
 
-    entry, np_from, np_into =  make_collision(box, poly)
+    entry = make_collision(box, poly)[0]
     assert entry is not None
     assert entry.get_from() == box
     assert entry.get_into() == poly
+
+    # Colliding just on the edge
+    entry, np_from, np_into = make_collision(CollisionBox((0, 3, 0), 1, 2, 1), poly)
+    assert entry.get_surface_point(np_from) == Point3(0, 3, 0)
+
+    # No collision
+    entry = make_collision(CollisionBox((10, 10, 10), 8, 9, 10), poly)[0]
+    assert entry is None
 
 
 def test_sphere_into_poly():
     sphere = CollisionSphere(0, 0, 0, 1)
     poly = CollisionPolygon(Point3(0, 0, 0), Point3(0, 0, 1), Point3(0, 1, 1), Point3(0, 1, 0))
 
-    entry, np_from, np_into = make_collision(sphere, poly)
+    entry = make_collision(sphere, poly)[0]
     assert entry is not None
     assert entry.get_from() == sphere
     assert entry.get_into() == poly
+
+    # Colliding just on the edge
+    entry, np_from, np_into = make_collision(CollisionSphere(0, 0, 3, 2), poly)
+    assert entry.get_surface_point(np_from) == Point3(0, 0, 3)
+
+    # No collision
+    entry = make_collision(CollisionSphere(100, 100, 100, 100), poly)[0]
+    assert entry is None
