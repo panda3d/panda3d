88fc34b9d4449f28bae00f11908b897da88b34ec
Rishabh Tewari <rishabhtewari1@gmail.com>
2019-04-04 12:03:31 -0500

    git commit

--- tests/audio/test_loading.py
+++ tests/audio/test_loading.py
@@ -1,5 +1,11 @@
 import pytest
-
+import os
 def test_missing_file(audiomgr):
-    sound = audiomgr.get_sound('/not/a/valid/file.ogg')
+    filename = "/not/a/valid/file.ogg"
+    sound = audiomgr.get_sound(filename)
     assert str(sound).startswith('NullAudioSound')
+
+def test_check_file_extension(audiomgr):
+    filename, file_ext = os.path.splitext("/not/a/valid/file.ogg")
+    assert file_ext.startswith((".mp3",".wav","aiff",".midi",".mod",".wma",".ogg",".vorbis"))
+
