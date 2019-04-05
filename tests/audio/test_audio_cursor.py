bc3be89ad3153a52ae7bf980fa407ed7ca933436
Rishabh Tewari <rishabhtewari1@gmail.com>
2019-04-05 18:23:22 -0500

    modified:   ../audio/test_audio_cursor.py 	modified:   ../audio/test_loading.py 	modified:   test_video_cursor.py

--- tests/audio/test_audio_cursor.py
+++ tests/audio/test_audio_cursor.py
@@ -1,7 +1,7 @@
 from panda3d.core import MovieAudio
 
 def test_cursor_type():
-    movie_path = "impulse.flac"
+    movie_path = os.path.join(os.path.dirname(__file__), "impulse.flac"
     reference_file = MovieAudio.get(movie_path)
     cursor = MovieAudio.open(reference_file)
     file_name_return = MovieAudio.getFilename(reference_file)
