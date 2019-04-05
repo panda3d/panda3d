8faf61bf775fb30262465980d66b4fb6bb702a80
DrEuler <rishabhtewari1@gmail.com>
2019-04-04 23:58:42 -0500

    Updated suggestions

--- tests/video/test_loading_cursor.py
+++ tests/video/test_loading_cursor.py
@@ -6,4 +6,4 @@ def test_cursor_check():
     movie_path = "../../samples/media-player/PandaSneezes.ogv"
     get_File = MovieVideo.get(movie_path)
     cursor_type = MovieVideo.open(get_File)
-    assert cursor_type != None
+    assert cursor_type is not None
