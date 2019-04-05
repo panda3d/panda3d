12df01b167cf8f34d9c03097cf9720a056b3c42e
DrEuler <rishabhtewari1@gmail.com>
2019-04-05 13:04:18 -0500

    Update test_loading_cursor.py

--- tests/video/test_loading_cursor.py
+++ tests/video/test_loading_cursor.py
@@ -1,8 +1,7 @@
 from panda3d.core import MovieVideo
-import pytest
 
 def test_cursor_check(): 
     movie_path = "../../samples/media-player/PandaSneezes.ogv"
-    get_file = MovieVideo.get(movie_path)
-    cursor_type = MovieVideo.open(get_file)
-    assert cursor_type is not None
+    reference_file = MovieVideo.get(movie_path)
+    cursor = MovieVideo.open(reference_file)
+    assert cursor is not None
