457261a57dfa675217408abf0c9d5e2579d13fc0
DrEuler <rishabhtewari1@gmail.com>
2019-04-05 12:47:28 -0500

    Update test_cursor.py

--- tests/audio/test_cursor.py
+++ tests/audio/test_cursor.py
@@ -1,8 +1,7 @@
 from panda3d.core import MovieAudio
-import pytest
 
 def test_cursor_type():
     movie_path = "impulse.flac"
-    get_file = MovieAudio.get(movie_path)
-    cursor_type = MovieAudio.open(get_file)
-    assert cursor_type is not None
+    reference_file = MovieAudio.get(movie_path)
+    cursor = MovieAudio.open(reference_file)
+    assert cursor is not None
