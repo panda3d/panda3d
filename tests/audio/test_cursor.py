946cd6bbb1ae454299f4b96f3654b49d51404f71
Rishabh Tewari <rishabhtewari1@gmail.com>
2019-04-05 16:28:01 -0500

    tests whether the returned movie path is still the same

--- tests/audio/test_cursor.py
+++ tests/audio/test_cursor.py
@@ -1,8 +1,10 @@
 from panda3d.core import MovieAudio
-import pytest
 
 def test_cursor_type():
     movie_path = "impulse.flac"
-    get_file = MovieAudio.get(movie_path)
-    cursor_type = MovieAudio.open(get_file)
-    assert cursor_type is not None
+    reference_file = MovieAudio.get(movie_path)
+    cursor = MovieAudio.open(reference_file)
+    file_name_return = MovieAudio.getFilename(reference_file)
+    assert file_name_return == movie_path
+    assert cursor is not None
+    
