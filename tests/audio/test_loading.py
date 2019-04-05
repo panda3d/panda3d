418e599dc5bc14bbaf06e6218b12a6d296fd1ed3
Rishabh Tewari <rishabhtewari1@gmail.com>
2019-04-05 16:58:59 -0500

    modified:   tests/audio/test_loading.py 	modified:   tests/video/test_loading_cursor.py

--- tests/audio/test_loading.py
+++ tests/audio/test_loading.py
@@ -4,8 +4,3 @@ def test_missing_file(audiomgr):
     filename = "/not/a/valid/file.ogg"
     sound = audiomgr.get_sound(filename)
     assert str(sound).startswith('NullAudioSound')
-
-def test_check_file_extension(audiomgr):
-    filename, file_ext = os.path.splitext("/not/a/valid/file.ogg")
-    assert file_ext.startswith((".mp3",".wav","aiff",".midi",".mod",".wma",".ogg",".vorbis"))
-
--- tests/video/test_loading_cursor.py
+++ tests/video/test_loading_cursor.py
@@ -1,28 +1,7 @@
 from panda3d.core import MovieVideo
-from panda3d.core import MovieVideoCursor
-import pytest
 
-def get_movie():
-    movie_path = "../samples/media-player/PandaSneezes.ogv"
-    getFile = MovieVideo.get(movie_path)
-    return getFile #Obtains a MovieVideo that references a file
-
-@pytest.fixture
-def cursor_check():
-    movie_path = "../samples/media-player/PandaSneezes.ogv"
-    getFile = get_movie() #references the function above
-    valid_cursor_check = MovieVideo.open(getFile) #Open this video, returns a MovieVideoCursor 
-
-
-def test_loading():
-    getFile = get_movie()
-    FileReturn = MovieVideo.getFilename(getFile)
-    assert FileReturn == FileReturn
-
-def test_cursor_check(cursor_check):
-    valid_cursor_check = cursor_check
-    assert valid_cursor_check != None
-
-
-    
-
+ def test_cursor_check(): 
+    movie_path = "../../samples/media-player/PandaSneezes.ogv"
+    reference_file = MovieVideo.get(movie_path)
+    cursor = MovieVideo.open(reference_file)
+    assert cursor is not None
