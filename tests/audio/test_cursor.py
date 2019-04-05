e2c4ccfc4ca1f67182b007fb741a7e1bc35d30b2
Rishabh Tewari <rishabhtewari1@gmail.com>
2019-04-05 12:13:44 -0500

    Added MoviAudio cursor type test
    checks if the cursor value returned is valid

--- tests/audio/test_cursor.py
+++ tests/audio/test_cursor.py
@@ -1,7 +1,8 @@
 from panda3d.core import MovieAudio
+import pytest
 
 def test_cursor_type():
     movie_path = "impulse.flac"
-    reference_file = MovieAudio.get(movie_path)
-    cursor = MovieAudio.open(reference_file)
-    assert cursor is not None
+    get_file = MovieAudio.get(movie_path)
+    cursor_type = MovieAudio.open(get_file)
+    assert cursor_type is not None
