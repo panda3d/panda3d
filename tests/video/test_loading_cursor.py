e4b5028b79db2e9c35cfff861bcf713c537f4b87
Rishabh Tewari <rishabhtewari1@gmail.com>
2019-04-05 17:08:54 -0500

    modified:   test_loading_cursor.py

--- tests/video/test_loading_cursor.py
+++ tests/video/test_loading_cursor.py
@@ -1,6 +1,6 @@
 from panda3d.core import MovieVideo
 
- def test_cursor_check(): 
+def test_cursor_check(): 
     movie_path = "../../samples/media-player/PandaSneezes.ogv"
     reference_file = MovieVideo.get(movie_path)
     cursor = MovieVideo.open(reference_file)
