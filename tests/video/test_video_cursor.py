ef97593af376bc407c97610253e0b2adbbbc3226
Rishabh Tewari <rishabhtewari1@gmail.com>
2019-04-05 19:34:37 -0500

    modified:   test_video_cursor.py

--- tests/video/test_video_cursor.py
+++ tests/video/test_video_cursor.py
@@ -1,8 +1,8 @@
 from panda3d.core import MovieVideo
 
 def test_cursor_check(): 
-    movie_path = os.path.join(os.path.dirname(__file__), "../../samples/media-player/PandaSneezes.ogv"
-    reference_file = MovieVideo.get(movie_path)
+    movie_path = "../../samples/media-player/PandaSneezes.ogv"
+    reference_file = MovieVideo.get(movie_path)	    
     cursor = MovieVideo.open(reference_file)
     file_name_return = MovieVideo.getFilename(reference_file)
     assert file_name_return == movie_path
