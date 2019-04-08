3500ed79bd4d51fe33f26f0c12bd0357f0520208
Rishabh Tewari <rishabhtewari1@gmail.com>
2019-04-07 23:34:34 -0500

    Updated video_is_playing to check the stop func 	modified:   test_video_is_playing.py

--- tests/video/test_video_is_playing.py
+++ tests/video/test_video_is_playing.py
@@ -1,10 +1,12 @@
 from panda3d.core import MovieVideo
 from panda3d.core import MovieTexture
 import os
-
-def test_texture_loop_count(): 
+#This test checks isPlaying() and stop() functionality
+def test_video_is_playing(): 
     movie_path = os.path.join(os.path.dirname(__file__), "small.webm")
     reference_file = MovieVideo.get(movie_path)	    
     reference_texture = MovieTexture(reference_file)
     MovieTexture.play(reference_texture)
     assert MovieTexture.isPlaying(reference_texture) is True #checks whether the video is playing or not
+    MovieTexture.stop(reference_texture)
+    assert MovieTexture.isPlaying(reference_texture) is False
