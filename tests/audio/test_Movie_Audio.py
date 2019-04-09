31ed521011fac960adafae8f92ed896604a389d5
Rishabh Tewari <rishabhtewari1@gmail.com>
2019-04-08 19:01:44 -0500

    modified:   ../audio/test_Movie_Audio.py

--- tests/audio/test_Movie_Audio.py
+++ tests/audio/test_Movie_Audio.py
@@ -9,11 +9,6 @@ def test_audio_rate(): #tests for audio rate
     movie_file = MovieAudio.open(reference_file)
     assert movie_file.audioRate() == 48000
 
-def test_missing_file(audiomgr): #test whether file is loaded in properly
-    filename = "/not/a/valid/file.ogg"
-    sound = audiomgr.get_sound(filename)
-    assert str(sound).startswith('NullAudioSound')
-
 def test_audio_length(): #test for testing audio length
     movie_path = os.path.join(os.path.dirname(__file__), "impulse.flac")
     reference_file = MovieAudio.get(movie_path)
@@ -35,7 +30,7 @@ def test_can_seek_fast(): #test for seeking fast
 def test_audio_channel(): #tests for number of audio channels
     movie_path = os.path.join(os.path.dirname(__file__), "impulse.flac") #to make sure relative paths aren't a problem
     reference_file = MovieAudio.get(movie_path)
-    movie_file = MovieAudio.open(reference_file)
+    movie_file = reference_file.open() 
     assert movie_file.audio_channels() == 1
 
 def test_cursor(): #opening the file returns a cursor
