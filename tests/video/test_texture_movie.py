86b66a82dff8d47d25f1dd7a3899d1dbaeef0f11
Rishabh Tewari <rishabhtewari1@gmail.com>
2019-04-10 01:15:21 -0500

    Updated tests to be platform independent         modified:   audio/test_audio_movie.py 	modified:   video/test_texture_movie.py 	modified:   video/test_video_movie.py

--- tests/video/test_texture_movie.py
+++ tests/video/test_texture_movie.py
@@ -1,19 +1,22 @@
 from panda3d.core import MovieVideo
 from panda3d.core import MovieTexture
+from panda3d.core import Filename
 import os
 
 def test_texture_loop_count(): 
     movie_path = os.path.join(os.path.dirname(__file__), "small.webm")
+    movie_path = Filename.from_os_specific(movie_path) # enables Platform independent testing
     reference_file = MovieVideo.get(movie_path)	    
     reference_texture = MovieTexture(reference_file)
     assert reference_texture.get_loop_count() == 1
 
-#This test checks isPlaying() and stop() functionality
-def test_video_is_playing(): 
+
+def test_video_is_playing(): #This test checks isPlaying() and stop() functionality
     movie_path = os.path.join(os.path.dirname(__file__), "small.webm")
+    movie_path = Filename.from_os_specific(movie_path) # enables Platform independent testing
     reference_file = MovieVideo.get(movie_path)	    
     reference_texture = MovieTexture(reference_file)
     reference_texture.play()
-    assert reference_texture.is_playing() is True #checks whether the video is playing or not
+    assert reference_texture.is_playing() is True 
     reference_texture.stop()
     assert reference_texture.is_playing() is False
--- tests/video/test_video_movie.py
+++ tests/video/test_video_movie.py
@@ -5,18 +5,21 @@ import os
 
 def test_cursor_check(): 
     movie_path = os.path.join(os.path.dirname(__file__), "small.webm")
+    movie_path = Filename.from_os_specific(movie_path) # enables Platform independent testing
     reference_file = MovieVideo.get(movie_path)	    
-    assert reference_file.get_filename() == Filename.from_os_specific(movie_path)
+    assert reference_file.get_filename() == movie_path
     assert reference_file.open() is not None
 
 def test_video_length(): 
     movie_path = os.path.join(os.path.dirname(__file__), "../../samples/media-player/PandaSneezes.ogv")
+    movie_path = Filename.from_os_specific(movie_path) # enables Platform independent testing
     reference_file = MovieVideo.get(movie_path)	    
     cursor = reference_file.open()
     assert cursor.length() == 14
 
 def test_video_size(): 
     movie_path = os.path.join(os.path.dirname(__file__), "small.webm")
+    movie_path = Filename.from_os_specific(movie_path) # enables Platform independent testing
     reference_file = MovieVideo.get(movie_path)	    
     cursor = reference_file.open()
     assert cursor.size_x() == 560 #found the height and width using mkvinfo
