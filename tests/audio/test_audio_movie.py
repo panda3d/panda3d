61aac6135ed3f6af3de2ce5185f808778e7665f9
Rishabh Tewari <rishabhtewari1@gmail.com>
2019-04-11 22:15:48 -0500

    Added the Skip functionality. 	modified:   audio/test_audio_movie.py 	deleted:    ffmpeg/Example.ogg 	deleted:    ffmpeg/test_check_ffmpeg.py 	modified:   video/test_texture_movie.py 	modified:   video/test_video_movie.py

--- tests/audio/test_audio_movie.py
+++ tests/audio/test_audio_movie.py
@@ -1,47 +1,66 @@
 from panda3d.core import MovieAudioCursor
 from panda3d.core import MovieAudio
 from panda3d.core import Filename
+import pytest
 import os
+from panda3d.core import PandaSystem
+from panda3d.core import MovieTexture
+from panda3d.core import MovieVideo
 
-def test_audio_rate(): #tests for audio rate
+def check_ffmpeg():
     movie_path = os.path.join(os.path.dirname(__file__), "impulse.flac")
-    movie_path = Filename.from_os_specific(movie_path) # enables Platform independent testing
-    reference_file = MovieAudio.get(movie_path)
-    movie_file = reference_file.open()
-    assert movie_file.audio_rate() == 48000
+    movie_path = Filename.from_os_specific(movie_path) #platform independent path
+    reference_file = MovieVideo.get(movie_path)	    
+    reference_texture = MovieTexture(reference_file)
+    reference_texture.play() # plays the reference textture
+    system = PandaSystem.get_global_ptr()
+    has_ffmpeg = 'FFmpeg' in system.systems #checks whether ffmpeg is loaded
+    assert has_ffmpeg is True
+    reference_texture.stop()
+    if has_ffmpeg is True:
+        return True
 
-def test_audio_length(): #test for testing audio length
-    movie_path = os.path.join(os.path.dirname(__file__), "impulse.flac")
-    movie_path = Filename.from_os_specific(movie_path)
-    reference_file = MovieAudio.get(movie_path)
-    movie_file = reference_file.open()
-    assert movie_file.length() == 2
+@pytest.mark.skipif(check_ffmpeg() is False, reason="skip when ffmpeg is not available")
+class Test_Audio_Movie():
+    def test_audio_rate(self): #tests for audio rate
+        movie_path = os.path.join(os.path.dirname(__file__), "impulse.flac")
+        movie_path = Filename.from_os_specific(movie_path) # enables Platform independent testing
+        reference_file = MovieAudio.get(movie_path)
+        movie_file = reference_file.open()
+        assert movie_file.audio_rate() == 48000
 
-def test_can_seek(): #test for seeking
-    movie_path = os.path.join(os.path.dirname(__file__), "impulse.flac")
-    movie_path = Filename.from_os_specific(movie_path)
-    reference_file = MovieAudio.get(movie_path)
-    movie_file = reference_file.open()
-    assert movie_file.can_seek() is True
+    def test_audio_length(self): #test for testing audio length
+        movie_path = os.path.join(os.path.dirname(__file__), "impulse.flac")
+        movie_path = Filename.from_os_specific(movie_path)
+        reference_file = MovieAudio.get(movie_path)
+        movie_file = reference_file.open()
+        assert movie_file.length() == 2
 
-def test_can_seek_fast(): #test for seeking fast
-    movie_path = os.path.join(os.path.dirname(__file__), "impulse.flac")
-    movie_path = Filename.from_os_specific(movie_path)
-    reference_file = MovieAudio.get(movie_path)
-    movie_file = reference_file.open()
-    assert movie_file.can_seek_fast() is True
+    def test_can_seek(self): #test for seeking
+        movie_path = os.path.join(os.path.dirname(__file__), "impulse.flac")
+        movie_path = Filename.from_os_specific(movie_path)
+        reference_file = MovieAudio.get(movie_path)
+        movie_file = reference_file.open()
+        assert movie_file.can_seek() is True
 
-def test_audio_channel(): #tests for number of audio channels
-    movie_path = os.path.join(os.path.dirname(__file__), "impulse.flac")
-    movie_path = Filename.from_os_specific(movie_path)
-    reference_file = MovieAudio.get(movie_path)
-    movie_file = reference_file.open() 
-    assert movie_file.audio_channels() == 1
+    def test_can_seek_fast(self): #test for seeking fast
+        movie_path = os.path.join(os.path.dirname(__file__), "impulse.flac")
+        movie_path = Filename.from_os_specific(movie_path)
+        reference_file = MovieAudio.get(movie_path)
+        movie_file = reference_file.open()
+        assert movie_file.can_seek_fast() is True
 
-def test_cursor(): #opening the file returns a cursor
-    movie_path = os.path.join(os.path.dirname(__file__), "impulse.flac")
-    movie_path = Filename.from_os_specific(movie_path)
-    reference_file = MovieAudio.get(movie_path)
-    file_name_return = reference_file.get_filename()
-    assert reference_file.open() is not None #checks the cursor
-    assert file_name_return == movie_path #checks the return name of the file
+    def test_audio_channel(self): #tests for number of audio channels
+        movie_path = os.path.join(os.path.dirname(__file__), "impulse.flac")
+        movie_path = Filename.from_os_specific(movie_path)
+        reference_file = MovieAudio.get(movie_path)
+        movie_file = reference_file.open() 
+        assert movie_file.audio_channels() == 1
+
+    def test_cursor(self): #opening the file returns a cursor
+        movie_path = os.path.join(os.path.dirname(__file__), "impulse.flac")
+        movie_path = Filename.from_os_specific(movie_path)
+        reference_file = MovieAudio.get(movie_path)
+        file_name_return = reference_file.get_filename()
+        assert reference_file.open() is not None #checks the cursor
+        assert file_name_return == movie_path #checks the return name of the file
--- tests/video/test_texture_movie.py
+++ tests/video/test_texture_movie.py
@@ -2,37 +2,55 @@ from panda3d.core import MovieVideo
 from panda3d.core import MovieTexture
 from panda3d.core import Filename
 import os
+import pytest
+from panda3d.core import PandaSystem
+from panda3d.core import MovieTexture
+from panda3d.core import MovieVideo
 
-def test_texture_loop_count(): 
+def check_ffmpeg():
     movie_path = os.path.join(os.path.dirname(__file__), "small.webm")
-    movie_path = Filename.from_os_specific(movie_path) # enables Platform independent testing
+    movie_path = Filename.from_os_specific(movie_path) #platform independent path
     reference_file = MovieVideo.get(movie_path)	    
     reference_texture = MovieTexture(reference_file)
-    assert reference_texture.get_loop_count() == 1
-
-
-def test_video_is_playing(): #This test checks isPlaying() and stop() functionality
-    movie_path = os.path.join(os.path.dirname(__file__), "small.webm")
-    movie_path = Filename.from_os_specific(movie_path) # enables Platform independent testing
-    reference_file = MovieVideo.get(movie_path)	    
-    reference_texture = MovieTexture(reference_file)
-    reference_texture.play()
-    assert reference_texture.is_playing() is True 
+    reference_texture.play() # plays the reference textture
+    system = PandaSystem.get_global_ptr()
+    has_ffmpeg = 'FFmpeg' in system.systems #checks whether ffmpeg is loaded
+    assert has_ffmpeg is True
     reference_texture.stop()
-    assert reference_texture.is_playing() is False
+    if has_ffmpeg is True:
+        return True
 
-def test_play_rate():
-    movie_path = os.path.join(os.path.dirname(__file__), "small.webm")
-    movie_path = Filename.from_os_specific(movie_path) # enables Platform independent testing 
-    reference_file = MovieVideo.get(movie_path)	    
-    reference_texture = MovieTexture(reference_file)
-    assert reference_texture.get_play_rate() == 1
-    reference_texture.set_play_rate(2)
-    assert reference_texture.get_play_rate() == 2
+@pytest.mark.skipif(check_ffmpeg() is False, reason="skip when ffmpeg is not available")
+class Test_Texture_Movie():
+    def test_texture_loop_count(self): 
+        movie_path = os.path.join(os.path.dirname(__file__), "small.webm")
+        movie_path = Filename.from_os_specific(movie_path) # enables Platform independent testing
+        reference_file = MovieVideo.get(movie_path)	    
+        reference_texture = MovieTexture(reference_file)
+        assert reference_texture.get_loop_count() == 1
 
-def test_video_texture_length():
-    movie_path = os.path.join(os.path.dirname(__file__), "small.webm")
-    movie_path = Filename.from_os_specific(movie_path) # enables Platform independent testing 
-    reference_file = MovieVideo.get(movie_path)	    
-    reference_texture = MovieTexture(reference_file)
-    assert reference_texture.get_video_length() == 05.56800 #got info from mkvinfo
+    def test_video_is_playing(self): #This test checks isPlaying() and stop() functionality
+        movie_path = os.path.join(os.path.dirname(__file__), "small.webm")
+        movie_path = Filename.from_os_specific(movie_path) # enables Platform independent testing
+        reference_file = MovieVideo.get(movie_path)	    
+        reference_texture = MovieTexture(reference_file)
+        reference_texture.play()
+        assert reference_texture.is_playing() is True 
+        reference_texture.stop()
+        assert reference_texture.is_playing() is False
+
+    def test_play_rate(self):
+        movie_path = os.path.join(os.path.dirname(__file__), "small.webm")
+        movie_path = Filename.from_os_specific(movie_path) # enables Platform independent testing 
+        reference_file = MovieVideo.get(movie_path)	    
+        reference_texture = MovieTexture(reference_file)
+        assert reference_texture.get_play_rate() == 1
+        reference_texture.set_play_rate(2)
+        assert reference_texture.get_play_rate() == 2
+
+    def test_video_texture_length(self):
+        movie_path = os.path.join(os.path.dirname(__file__), "small.webm")
+        movie_path = Filename.from_os_specific(movie_path) # enables Platform independent testing 
+        reference_file = MovieVideo.get(movie_path)	    
+        reference_texture = MovieTexture(reference_file)
+        assert reference_texture.get_video_length() == 05.56800 #got info from mkvinfo
