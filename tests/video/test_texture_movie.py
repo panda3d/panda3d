c896e52909eaa8174138c529733fb57a4f75c558
Rishabh Tewari <rishabhtewari1@gmail.com>
2019-04-11 20:13:13 -0500

    Added test for video_length(texture) 	modified:   test_texture_movie.py

--- tests/video/test_texture_movie.py
+++ tests/video/test_texture_movie.py
@@ -29,3 +29,10 @@ def test_play_rate():
     assert reference_texture.get_play_rate() == 1
     reference_texture.set_play_rate(2)
     assert reference_texture.get_play_rate() == 2
+
+def test_video_texture_length():
+    movie_path = os.path.join(os.path.dirname(__file__), "small.webm")
+    movie_path = Filename.from_os_specific(movie_path) # enables Platform independent testing 
+    reference_file = MovieVideo.get(movie_path)	    
+    reference_texture = MovieTexture(reference_file)
+    assert reference_texture.get_video_length() == 05.56800 #got info from mkvinfo
