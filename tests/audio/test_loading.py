3b8e4a38a0281d8758f38832b3ab2c70d1fc14f5
Rishabh Tewari <rishabhtewari1@gmail.com>
2019-04-14 17:15:33 -0500

    Removed files from here 	deleted:    tests/audio/impulse.flac 	deleted:    tests/audio/test_audio_movie.py 	deleted:    tests/audio/test_loading.py 	deleted:    tests/video/test_texture_movie.py 	deleted:    tests/video/test_video_movie.py

--- tests/audio/test_loading.py
+++
@@ -1,4 +0,0 @@
-def test_missing_file(audiomgr): #test whether file is loaded in properly
-    filename = "/not/a/valid/file.ogg"
-    sound = audiomgr.get_sound(filename)
-    assert str(sound).startswith('NullAudioSound')
