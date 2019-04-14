41d2b3b7e86e566924a5fa1f74bec4889d267850
Rishabh Tewari <rishabhtewari1@gmail.com>
2019-04-14 18:27:40 -0500

    Same 	modified:   test_loading.py

--- tests/audio/test_loading.py
+++ tests/audio/test_loading.py
@@ -1,5 +1,5 @@
 import pytest
 
 def test_missing_file(audiomgr):
-    sound = audiomgr.get_sound("/not/a/valid/file.ogg")
+    sound = audiomgr.get_sound('/not/a/valid/file.ogg')
     assert str(sound).startswith('NullAudioSound')
