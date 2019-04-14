e1d711cd4c8bc0bb97bfc881dd7d864d7a9528fa
Rishabh Tewari <rishabhtewari1@gmail.com>
2019-04-13 23:11:36 -0500

    Added a test for clock object modes 	modified:   test_clockobject.py

--- tests/putil/test_clockobject.py
+++ tests/putil/test_clockobject.py
@@ -21,3 +21,21 @@ def test_get_dt(clockobj):
     clockobj.tick()
     second_tick = clockobj.get_frame_time()
     assert clockobj.get_dt() == second_tick - first_tick
+
+def test_get_mode(clockobj):
+    clockobj.tick()
+    assert clockobj.get_mode() == 0 #M_normal
+    clockobj.set_mode(1)
+    assert clockobj.get_mode() == 1 #M_non_real_time
+    clockobj.set_mode(2)
+    assert clockobj.get_mode() == 2 #M_forced
+    clockobj.set_mode(3)
+    assert clockobj.get_mode() == 3 #M_degrade
+    clockobj.set_mode(4)
+    assert clockobj.get_mode() == 4 #M_Slave
+    clockobj.set_mode(5)
+    assert clockobj.get_mode() == 5 #M_Limited
+    clockobj.set_mode(6)
+    assert clockobj.get_mode() == 6 #M_Integer
+    clockobj.set_mode(7)
+    assert clockobj.get_mode() == 7  #M_Integer_Limited
