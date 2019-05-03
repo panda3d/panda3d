fce282ea33df949d58dd5d8b354bb4820056370e
rdb <git@rdb.name>
2019-05-03 22:04:24 +0200

    Emit warning when importing panda3d using Python 2.7
    Fixes #602

--- makepanda/makepanda.py
+++ makepanda/makepanda.py
@@ -2880,6 +2880,14 @@ for basename in del_files:
 p3d_init = """"Python bindings for the Panda3D libraries"
 
 __version__ = '%s'
+
+if __debug__:
+    import sys
+    if sys.version_info < (3, 0):
+        sys.stderr.write("WARNING: Python 2.7 will reach EOL after December 31, 2019.\\n")
+        sys.stderr.write("To suppress this warning, upgrade to Python 3.\\n")
+        sys.stderr.flush()
+    del sys
 """ % (WHLVERSION)
 
 if GetTarget() == 'windows':
