e2f6cde0f01f3adf9764b1193a9da4c4962c4167
hecris <christopher.he33@myhunter.cuny.edu>
2019-05-29 13:35:03 -0400

    add signature to collisionBox.h

--- panda/src/collide/collisionBox.h
+++ panda/src/collide/collisionBox.h
@@ -82,6 +82,8 @@ protected:
   virtual PT(CollisionEntry)
     test_intersection_from_segment(const CollisionEntry &entry) const;
   virtual PT(CollisionEntry)
+    test_intersection_from_parabola(const CollisionEntry &entry) const;
+  virtual PT(CollisionEntry)
     test_intersection_from_capsule(const CollisionEntry &entry) const;
   virtual PT(CollisionEntry)
     test_intersection_from_box(const CollisionEntry &entry) const;
