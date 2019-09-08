ebd150eef6c6e99c34284803a60614fda6ab7a52
Mitchell Stokes <mogurijin@gmail.com>
2019-09-08 12:24:08 -0700

    direct: Bring back AppRunnerGlobal and print a deprecation message instead

---
+++ direct/src/showbase/AppRunnerGlobal.py
@@ -0,0 +1,14 @@
+""" This module serves as a placeholder for the global AppRunner
+object, which only exists when we are running a .p3d file via
+runp3d.py or via the Panda3D plugin or standalone executable.
+
+This is needed for apps that start themselves by importing
+DirectStart; it provides a place for these apps to look for
+the AppRunner at startup. """
+
+if __debug__:
+    print('AppRunner has been removed and AppRunnerGlobal has been deprecated')
+
+#: Contains the global AppRunner instance, or None if this application
+#: was not run from the runtime environment.
+appRunner = None
