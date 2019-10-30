5ba527045ef1aea301ed32688ca913cbd2055c39
Max Voss <bruno.m.voss@gmail.com>
2019-10-30 17:10:02 +0100

    nvm autopep8, + requested pep8 changes

--- tests/gui/test_DirectWaitBar.py
+++ tests/gui/test_DirectWaitBar.py
@@ -2,8 +2,8 @@
 from direct.gui.DirectWaitBar import DirectWaitBar
 
 def test_create_waitbar():
-    bar=DirectWaitBar(pos=(0,0,0),scale=(0.1,0.1,0.1))
-    bar["range"]=1.0
-    bar["value"]=0.0
+    bar = DirectWaitBar(pos=(0, 0, 0), scale=(0.1, 0.1, 0.1))
+    bar["range"] = 1.0
+    bar["value"] = 0.0
     bar.setRange()
     bar.setValue()
