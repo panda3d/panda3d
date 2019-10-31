61022d38bd7a3ca8265487c92ffe7da21ba8a35e
Max Voss <bruno.m.voss@gmail.com>
2019-10-31 11:43:26 +0100

    setter test

--- tests/gui/test_DirectButton.py
+++ tests/gui/test_DirectButton.py
@@ -4,3 +4,15 @@ from direct.gui.DirectButton import DirectButton
 def test_button_destroy():
     btn = DirectButton(text="Test")
     btn.destroy()
+
+def test_button_setPos():
+    btn = DirectButton(text="Test", pos=(0.5, 0.5, 0.5))
+    
+    btn["pos"] = (0.1, 0.1, 0.1)
+    assert btn["pos"] == (0.1, 0.1, 0.1)
+    
+
+if __name__=="__main__":
+    test_button_destroy()
+    test_button_setPos()
+    
