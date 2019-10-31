d5d9d3e75ed745a2b3e729952f011417a81866aa
Max Voss <bruno.m.voss@gmail.com>
2019-10-31 12:29:45 +0100

    dict like access or assignment don't actually work, c++ functions do

--- tests/gui/test_DirectButton.py
+++ tests/gui/test_DirectButton.py
@@ -5,14 +5,29 @@ def test_button_destroy():
     btn = DirectButton(text="Test")
     btn.destroy()
 
+def test_dict_get_broken():
+    btn = DirectButton(text="Test", pos=(0.5, 0.5, 0.5))
+    t = (0.1, 0.1, 0.1)
+    btn["pos"] = t
+    assert btn["pos"]!=t
+    
 def test_button_setPos():
     btn = DirectButton(text="Test", pos=(0.5, 0.5, 0.5))
+    t = (0.1, 0.1, 0.1)
+    btn["pos"] = t
+    btn.setPos(*t)
+    assert btn.getPos()==t
     
-    btn["pos"] = (0.1, 0.1, 0.1)
-    assert btn["pos"] == (0.1, 0.1, 0.1)
+def test_button_setScale():
+    btn = DirectButton(text="Test", pos=(0.5, 0.5, 0.5),scale=(0.5, 0.5, 0.5))
+    t = (0.1, 0.1, 0.1)
+    btn.setScale(*t)
+    assert btn.getScale() == t
     
 
 if __name__=="__main__":
+    test_dict_get_broken()
     test_button_destroy()
     test_button_setPos()
+    test_button_setScale()
     
