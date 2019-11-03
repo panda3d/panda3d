33975b82435bb6b624f06df64a1330c2609aab17
Max Voss <bruno.m.voss@gmail.com>
2019-11-03 10:41:40 +0100

    that one fails, not good to introduce a failing test

--- tests/gui/test_DirectButton.py
+++ tests/gui/test_DirectButton.py
@@ -10,12 +10,6 @@ def test_dict_get_broken():
     t = (0.1, 0.1, 0.1)
     btn["pos"] = t
     assert btn["pos"] != t
-
-def test_dict_get_broken2():
-    btn = DirectButton(text="Test", pos=(0.5, 0.5, 0.5))
-    t = "Hello"
-    btn["text"] = t
-    assert btn["text"] != t
     
 def test_button_setPos():
     btn = DirectButton(text="Test", pos=(0.5, 0.5, 0.5))
