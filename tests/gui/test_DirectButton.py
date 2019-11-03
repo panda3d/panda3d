44d09aaeec1a36a1e23376df286978faf07c80fe
Max Voss <bruno.m.voss@gmail.com>
2019-11-03 10:11:42 +0100

    suggested changes...

--- tests/gui/test_DirectButton.py
+++ tests/gui/test_DirectButton.py
@@ -9,14 +9,20 @@ def test_dict_get_broken():
     btn = DirectButton(text="Test", pos=(0.5, 0.5, 0.5))
     t = (0.1, 0.1, 0.1)
     btn["pos"] = t
-    assert btn["pos"]!=t
+    assert btn["pos"] != t
+
+def test_dict_get_broken2():
+    btn = DirectButton(text="Test", pos=(0.5, 0.5, 0.5))
+    t = "Hello"
+    btn["text"] = t
+    assert btn["text"] != t
     
 def test_button_setPos():
     btn = DirectButton(text="Test", pos=(0.5, 0.5, 0.5))
     t = (0.1, 0.1, 0.1)
     btn["pos"] = t
     btn.setPos(*t)
-    assert btn.getPos()==t
+    assert btn.getPos() == t
     
 def test_button_setScale():
     btn = DirectButton(text="Test", pos=(0.5, 0.5, 0.5),scale=(0.5, 0.5, 0.5))
@@ -24,10 +30,4 @@ def test_button_setScale():
     btn.setScale(*t)
     assert btn.getScale() == t
     
-
-if __name__=="__main__":
-    test_dict_get_broken()
-    test_button_destroy()
-    test_button_setPos()
-    test_button_setScale()
     
--- tests/gui/test_DirectWaitBar.py
+++ tests/gui/test_DirectWaitBar.py
@@ -2,7 +2,7 @@
 from direct.gui.DirectWaitBar import DirectWaitBar
 
 def test_create_waitbar():
-    bar = DirectWaitBar(pos = (0, 0, 0), scale = (0.1, 0.1, 0.1))
+    bar = DirectWaitBar(pos=(0, 0, 0), scale=(0.1, 0.1, 0.1))
     bar["range"] = 1.0
     bar["value"] = 0.0
     bar.setRange()
