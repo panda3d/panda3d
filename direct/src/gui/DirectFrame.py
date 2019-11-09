6721a83a1cce92ae05b1383dc0235f907f087023
max <bruno.m.voss@gmail.com>
2019-11-09 16:15:17 +0100

    passes all local tests now :)

--- direct/src/gui/DirectFrame.py
+++ direct/src/gui/DirectFrame.py
@@ -34,7 +34,8 @@ else:
 
 class DirectFrame(DirectGuiWidget):
     DefDynGroups = ('text', 'geom', 'image')
-    def __init__(self, parent = None, **kw):
+
+    def __init__(self, parent=None, **kw):
         # Inherits from DirectGuiWidget
         optiondefs = (
             # Define type of DirectGuiWidget
@@ -51,10 +52,10 @@ class DirectFrame(DirectGuiWidget):
             # Change default value of text mayChange flag from 0
             # (OnscreenText.py) to 1
             ('textMayChange',  1,          None),
-            )
+        )
         # Merge keyword options with default options
         self.defineoptions(kw, optiondefs,
-                           dynamicGroups = DirectFrame.DefDynGroups)
+                           dynamicGroups=DirectFrame.DefDynGroups)
 
         # Initialize superclasses
         DirectGuiWidget.__init__(self, parent)
@@ -67,124 +68,97 @@ class DirectFrame(DirectGuiWidget):
         self.setText()
 
     def setText(self, text=None):
-        self.new_set("text",text)
-        
+        self.new_set("text", text)
+
     def clearGeom(self):
         self['geom'] = None
         self.setGeom()
-    
-    def unset(self,name):
-        """what set[Geom,Text,Image] would have done, if no image, text
-        or geom are provided takes 
-        name must be in ["geom","text","image"]"""
-        
-        if name not in ["geom","text","image"]:
-            raise TypeError("name must be in ['geom','text','image']")
-            
-        #max number of states is 4 I think.
-        c=0
-        m=4
-        while c < m:
-            comp_name=name+str(c)
-            if self.hascomponent(comp_name):
-                self.destroycomponent()
-            c+=1
-    
-    def long_type_check(self,name):
-        """basically an elaborate type check."""
-        object_ = self[name]
-        if name=="image":
-            if (isinstance(object_, NodePath) or 
-                isinstance(object_, Texture) or 
-                isinstance(object_, stringType)):
+
+    def long_type_check(self, name, object_):
+        if isinstance(object_, stringType):
+            return True
+        if name == "image":
+            if ((isinstance(object_, NodePath) or
+                isinstance(object_, Texture)) 
+                or
+                ((len(object_) == 2) and# Its a model/node pair of strings
+                isinstance(object_[0], stringType) and
+                isinstance(object_[1], stringType))):
                 return True
-        elif name=="text":
-            if isinstance(object_, stringType):
+        elif name == "geom":
+            if (isinstance(object_, NodePath)):
                 return True
-        elif name=="geom":
-            if (isinstance(object_, NodePath) or 
-                isinstance(object_, stringType)):
-                return True
-        else:
-            raise TypeError("how did you get here?")
-            
-        return False
-    
-    def new_set(self,name,object_=None):
+
+    def new_set(self, name, object_=None):
         """my new code golf par is 1"""
-        assert name in ["geom","image","text"]
+        assert name in ["geom", "image", "text"]
         if object_ is not None:
-            self[name]=object_
+            self[name] = object_
+
+        object_ = self[name]
+
+        if object_ == None:
+            object_list = (None,) * self['numStates']
         else:
-            self.unset(name)
-            return
-            
-        object_=self[name]
-        
-        mul_bool = self.long_type_check(name)
-        
-        if mul_bool  == True:
-            object_list = (object_,) * self["numStates"]
-        elif mul_bool == False:
-            object_list = object_
-        else:
-            raise ValueError
-                
-        name_based_classes={
-        "geom":OnscreenGeom,
-        "image":OnscreenImage,
-        "text":OnscreenText,}
-        
-        name_based_constants={
-            "geom":{
-                    "geom":object_,
-                    "scale":1,
-                    "sort":DGG.GEOM_SORT_INDEX,
-                    },
-            "imgage":{
-                    "image":object_,
-                    "scale":1,
-                    "sort":DGG.IMAGE_SORT_INDEX,
-                    },
-            "text":{
-                    "text":object_,
-                    "scale":1,
-                    "mayChange":self['textMayChange'],
-                    "sort":DGG.TEXT_SORT_INDEX,
-                    },
-                }
-        #Index is probably constant and only used at creation.
-        #should be a local variable to the constructor.
-        #
-        
-        c=0
-        m=self["numStates"]
-        while c < m:
+            if self.long_type_check(name, object_):
+                object_list = (object_,) * self['numStates']
+            else:
+                object_list = object_
+
+        name_based_classes = {
+            "geom": OnscreenGeom,
+            "image": OnscreenImage,
+            "text": OnscreenText, }
+
+        name_based_constants = {
+            "geom": {
+                "scale": 1,
+                "sort": DGG.GEOM_SORT_INDEX, # this is "20"
+            },
+            "image": {
+                "scale": 1,
+                "sort": DGG.IMAGE_SORT_INDEX, # this is "10"
+            },
+            "text": {
+                "scale": 1,
+                "mayChange": self['textMayChange'],
+                "sort": DGG.TEXT_SORT_INDEX, # this is "30"
+            },
+        }
+        # constants should be local to or default arguments of constructors
+
+        c = 0
+        m = self["numStates"]
+        for c in range(self['numStates']):
             component_name = name+str(c)
             comp_input = object_list[c]
-            name_based_constants[name]=comp_input
-            
+            name_based_constants[name][name] = comp_input
+
             if self.hascomponent(component_name):
-                self[component_name]=comp_input
+                if comp_input == None:
+                    self.destroycomponent(component_name)
+                else:
+                    self[component_name+"_"+name] = comp_input
             else:
+                if comp_input == None:
+                    return
                 self.createcomponent(
-                        component_name, 
-                        (), 
-                        name,
-                        name_based_classes[name],#class type, positional
-                        (),
-                        parent = self.stateNodePath[c],
-                        **name_based_constants[name]
-                        ) #keywords, order irrelevant
-                        
-            c+=1
-    
+                    component_name,
+                    (),
+                    name,
+                    name_based_classes[name],  # class type, positional
+                    (),
+                    parent=self.stateNodePath[c],
+                    **name_based_constants[name]
+                )  # keywords, order irrelevant
+            c += 1
+
     def setGeom(self, geom=None):
-        self.new_set("geom",geom)
-        
+        self.new_set("geom", geom)
+
     def clearImage(self):
         self['image'] = None
         self.setImage()
 
     def setImage(self, image=None):
-        self.new_set("image",image)
+        self.new_set("image", image)
