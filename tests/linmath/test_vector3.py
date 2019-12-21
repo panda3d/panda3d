af6e6a1529622fb454a79bec862e6338fdd4fe82
Leandro (Cerberus1746) Benedet Garcia <cerberus1746@gmail.com>
2019-12-21 15:06:24 -0300

    Added check in the rounding tests to not execute in python 2.7

--- tests/linmath/test_vector3.py
+++ tests/linmath/test_vector3.py
@@ -1,3 +1,4 @@
+import sys
 from math import floor, ceil
 
 from panda3d.core import Vec3
@@ -6,21 +7,26 @@ from panda3d.core import Vec3
 original_vector = Vec3(2.3, 2.6, 3.5)
 
 def test_round():
-    rounded_vector = round(original_vector)
-    assert rounded_vector.x == 2
-    assert rounded_vector.y == 3
-    assert rounded_vector.z == 4
+    # Rounding in python 2.7 expects to return a float value, since it returns a Vector it does not
+    # work. When python 2.7 gets desprected, remove this check.
+    if sys.version_info >= (3, 5):
+        rounded_vector = round(original_vector)
+        assert rounded_vector.x == 2
+        assert rounded_vector.y == 3
+        assert rounded_vector.z == 4
 
 
 def test_floor():
-    rounded_vector = floor(original_vector)
-    assert rounded_vector.x == 2
-    assert rounded_vector.y == 2
-    assert rounded_vector.z == 3
+    if sys.version_info >= (3, 5):
+        rounded_vector = floor(original_vector)
+        assert rounded_vector.x == 2
+        assert rounded_vector.y == 2
+        assert rounded_vector.z == 3
 
 
 def test_ceil():
-    rounded_vector = ceil(original_vector)
-    assert rounded_vector.x == 3
-    assert rounded_vector.y == 3
-    assert rounded_vector.z == 4
+    if sys.version_info >= (3, 5):
+        rounded_vector = ceil(original_vector)
+        assert rounded_vector.x == 3
+        assert rounded_vector.y == 3
+        assert rounded_vector.z == 4
--- tests/linmath/test_vector4.py
+++ tests/linmath/test_vector4.py
@@ -1,3 +1,4 @@
+import sys
 from math import floor, ceil
 
 from panda3d.core import Vec4
@@ -6,24 +7,29 @@ from panda3d.core import Vec4
 original_vector = Vec4(2.3, 2.6, 3.5, 1)
 
 def test_round():
-    rounded_vector = round(original_vector)
-    assert rounded_vector.x == 2
-    assert rounded_vector.y == 3
-    assert rounded_vector.z == 4
-    assert rounded_vector.w == 1
+    # Rounding in python 2.7 expects to return a float value, since it returns a Vector it does not
+    # work. When python 2.7 gets desprected, remove this check.
+    if sys.version_info >= (3, 5):
+        rounded_vector = round(original_vector)
+        assert rounded_vector.x == 2
+        assert rounded_vector.y == 3
+        assert rounded_vector.z == 4
+        assert rounded_vector.w == 1
 
 
 def test_floor():
-    rounded_vector = floor(original_vector)
-    assert rounded_vector.x == 2
-    assert rounded_vector.y == 2
-    assert rounded_vector.z == 3
-    assert rounded_vector.w == 1
+    if sys.version_info >= (3, 5):
+        rounded_vector = floor(original_vector)
+        assert rounded_vector.x == 2
+        assert rounded_vector.y == 2
+        assert rounded_vector.z == 3
+        assert rounded_vector.w == 1
 
 
 def test_ceil():
-    rounded_vector = ceil(original_vector)
-    assert rounded_vector.x == 3
-    assert rounded_vector.y == 3
-    assert rounded_vector.z == 4
-    assert rounded_vector.w == 1
+    if sys.version_info >= (3, 5):
+        rounded_vector = ceil(original_vector)
+        assert rounded_vector.x == 3
+        assert rounded_vector.y == 3
+        assert rounded_vector.z == 4
+        assert rounded_vector.w == 1
