b68b169a9e6b59b6b1a8472614081bfbd0da8066
Leandro (Cerberus1746) Benedet Garcia <cerberus1746@gmail.com>
2019-12-22 12:14:09 -0300

    using skipif in the rounding tests

--- tests/linmath/test_vector3.py
+++ tests/linmath/test_vector3.py
@@ -1,32 +1,35 @@
-import sys
 from math import floor, ceil
+import sys
+
+import pytest
 
 from panda3d.core import Vec3
 
 
 original_vector = Vec3(2.3, 2.6, 3.5)
 
+reason = '''Rounding in python 2.7 expects to return a float value, since it returns a Vector it
+does not work. When python 2.7 gets desprected, remove this check.'''
+
+@pytest.mark.skipif(sys.version_info < (3, 5), reason=reason)
 def test_round():
-    # Rounding in python 2.7 expects to return a float value, since it returns a Vector it does not
-    # work. When python 2.7 gets desprected, remove this check.
-    if sys.version_info >= (3, 5):
-        rounded_vector = round(original_vector)
-        assert rounded_vector.x == 2
-        assert rounded_vector.y == 3
-        assert rounded_vector.z == 4
+    rounded_vector = round(original_vector)
+    assert rounded_vector.x == 2
+    assert rounded_vector.y == 3
+    assert rounded_vector.z == 4
 
 
+@pytest.mark.skipif(sys.version_info < (3, 5), reason=reason)
 def test_floor():
-    if sys.version_info >= (3, 5):
-        rounded_vector = floor(original_vector)
-        assert rounded_vector.x == 2
-        assert rounded_vector.y == 2
-        assert rounded_vector.z == 3
+    rounded_vector = floor(original_vector)
+    assert rounded_vector.x == 2
+    assert rounded_vector.y == 2
+    assert rounded_vector.z == 3
 
 
+@pytest.mark.skipif(sys.version_info < (3, 5), reason=reason)
 def test_ceil():
-    if sys.version_info >= (3, 5):
-        rounded_vector = ceil(original_vector)
-        assert rounded_vector.x == 3
-        assert rounded_vector.y == 3
-        assert rounded_vector.z == 4
+    rounded_vector = ceil(original_vector)
+    assert rounded_vector.x == 3
+    assert rounded_vector.y == 3
+    assert rounded_vector.z == 4
--- tests/linmath/test_vector4.py
+++ tests/linmath/test_vector4.py
@@ -1,35 +1,38 @@
-import sys
 from math import floor, ceil
+import sys
+
+import pytest
 
 from panda3d.core import Vec4
 
 
 original_vector = Vec4(2.3, 2.6, 3.5, 1)
 
+reason = '''Rounding in python 2.7 expects to return a float value, since it returns a Vector it
+does not work. When python 2.7 gets desprected, remove this check.'''
+
+@pytest.mark.skipif(sys.version_info < (3, 5), reason=reason)
 def test_round():
-    # Rounding in python 2.7 expects to return a float value, since it returns a Vector it does not
-    # work. When python 2.7 gets desprected, remove this check.
-    if sys.version_info >= (3, 5):
-        rounded_vector = round(original_vector)
-        assert rounded_vector.x == 2
-        assert rounded_vector.y == 3
-        assert rounded_vector.z == 4
-        assert rounded_vector.w == 1
+    rounded_vector = round(original_vector)
+    assert rounded_vector.x == 2
+    assert rounded_vector.y == 3
+    assert rounded_vector.z == 4
+    assert rounded_vector.w == 1
 
 
+@pytest.mark.skipif(sys.version_info < (3, 5), reason=reason)
 def test_floor():
-    if sys.version_info >= (3, 5):
-        rounded_vector = floor(original_vector)
-        assert rounded_vector.x == 2
-        assert rounded_vector.y == 2
-        assert rounded_vector.z == 3
-        assert rounded_vector.w == 1
+    rounded_vector = floor(original_vector)
+    assert rounded_vector.x == 2
+    assert rounded_vector.y == 2
+    assert rounded_vector.z == 3
+    assert rounded_vector.w == 1
 
 
+@pytest.mark.skipif(sys.version_info < (3, 5), reason=reason)
 def test_ceil():
-    if sys.version_info >= (3, 5):
-        rounded_vector = ceil(original_vector)
-        assert rounded_vector.x == 3
-        assert rounded_vector.y == 3
-        assert rounded_vector.z == 4
-        assert rounded_vector.w == 1
+    rounded_vector = ceil(original_vector)
+    assert rounded_vector.x == 3
+    assert rounded_vector.y == 3
+    assert rounded_vector.z == 4
+    assert rounded_vector.w == 1
