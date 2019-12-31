bff568798f5126b3df7e7d50ac4bd0f688b3ca42
Sam Edwards <CFSworks@gmail.com>
2019-12-31 06:59:39 -0700

    CMake: Make FindAssimp pull in libIrrXML as well, if found

--- cmake/modules/FindAssimp.cmake
+++ cmake/modules/FindAssimp.cmake
@@ -7,16 +7,27 @@
 # Once done this will define:
 #   ASSIMP_FOUND        - system has Assimp
 #   ASSIMP_INCLUDE_DIR  - the path to the location of the assimp/ directory
-#   ASSIMP_LIBRARY      - the library to link against for Assimp
+#   ASSIMP_LIBRARIES    - the libraries to link against for Assimp
 #
 
 find_path(ASSIMP_INCLUDE_DIR
   NAMES "assimp/Importer.hpp")
 
-find_library(ASSIMP_LIBRARY
+find_library(ASSIMP_ASSIMP_LIBRARY
   NAMES "assimp")
 
-mark_as_advanced(ASSIMP_INCLUDE_DIR ASSIMP_LIBRARY)
+find_library(ASSIMP_IRRXML_LIBRARY
+  NAMES "IrrXML")
+
+if(ASSIMP_ASSIMP_LIBRARY)
+  set(ASSIMP_LIBRARIES "${ASSIMP_ASSIMP_LIBRARY}")
+
+  if(ASSIMP_IRRXML_LIBRARY)
+    list(APPEND ASSIMP_LIBRARIES "${ASSIMP_IRRXML_LIBRARY}")
+  endif()
+endif()
+
+mark_as_advanced(ASSIMP_INCLUDE_DIR ASSIMP_LIBRARIES)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Assimp DEFAULT_MSG ASSIMP_INCLUDE_DIR ASSIMP_LIBRARY)
+find_package_handle_standard_args(Assimp DEFAULT_MSG ASSIMP_INCLUDE_DIR ASSIMP_LIBRARIES)
