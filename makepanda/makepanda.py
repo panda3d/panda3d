aad8b45df4a202e8034fe1d3d96d89e5c135d26d
rdb <git@rdb.name>
2020-01-06 03:34:07 +0100

    makepanda: update Python 2.7 EOL warning

--- makepanda/makepanda.py
+++ makepanda/makepanda.py
@@ -2588,7 +2588,7 @@ __version__ = '%s'
 if __debug__:
     import sys
     if sys.version_info < (3, 0):
-        sys.stderr.write("WARNING: Python 2.7 will reach EOL after December 31, 2019.\\n")
+        sys.stderr.write("WARNING: Python 2.7 has reached EOL as of January 1, 2020.\\n")
         sys.stderr.write("To suppress this warning, upgrade to Python 3.\\n")
         sys.stderr.flush()
     del sys
