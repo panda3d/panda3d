8a86ca97ebfcf186362b13ccfa2e9c027398fbe4
rdb <git@rdb.name>
2020-03-02 11:00:00 +0100

    makewheel: don't include libpythonX.Y.a in wheel
    Fixes #839

--- makepanda/makewheel.py
+++ makepanda/makewheel.py
@@ -732,7 +732,10 @@ __version__ = '{0}'
             pylib_path = os.path.join(libdir, pylib_arch, pylib_name)
         else:
             pylib_path = os.path.join(libdir, pylib_name)
-    whl.write_file('deploy_libs/' + pylib_name, pylib_path)
+
+    # If Python was linked statically, we don't need to include this.
+    if not pylib_name.endswith('.a'):
+        whl.write_file('deploy_libs/' + pylib_name, pylib_path)
 
     whl.close()
 
