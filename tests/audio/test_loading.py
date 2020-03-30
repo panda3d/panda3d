b20d7a6333156a85362423e5d13e8d2184e0be1d
atulsaswat <32965617+atulsaswat@users.noreply.github.com>
2020-03-30 22:26:44 +0530

    Added testing for presence of valid .ogg files
    In reference to a valid ogg file referenced from the samples.

--- tests/audio/test_loading.py
+++ tests/audio/test_loading.py
@@ -3,3 +3,7 @@ import pytest
 def test_missing_file(audiomgr):
     sound = audiomgr.get_sound('/not/a/valid/file.ogg')
     assert str(sound).startswith('NullAudioSound')
+
+def test_found_file(audiomgr):
+    sound = audiomgr.get_sound('../../samples/music-box/music/musicbox.ogg')
+    assert not str(sound).startswith('NullAudioSound')
