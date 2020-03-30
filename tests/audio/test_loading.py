45a5f9bf0976e540588e83f6e84c93c192009ce5
atulsaswat <32965617+atulsaswat@users.noreply.github.com>
2020-03-30 23:12:46 +0530

    Update #2 (Modification)

--- tests/audio/test_loading.py
+++ tests/audio/test_loading.py
@@ -1,9 +1,11 @@
 import pytest
+import panda3d.core
+import os
 
 def test_missing_file(audiomgr):
     sound = audiomgr.get_sound('/not/a/valid/file.ogg')
     assert str(sound).startswith('NullAudioSound')
 
-def test_found_file(audiomgr):
-    sound = audiomgr.get_sound('../../samples/music-box/music/musicbox.ogg')
-    assert not str(sound).startswith('NullAudioSound')
+def test_load_ogg_vorbis(audiomgr):
+    path = os.path.dirname(__file__) + '../../samples/music-box/music/musicbox.ogg'
+    assert not str(path).startswith('NullAudioSound')
