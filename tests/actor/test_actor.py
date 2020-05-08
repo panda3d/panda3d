40f718a4ff34bd44c3835c7f1b43d90aff057186
max <bruno.m.voss@gmail.com>
2020-05-08 12:56:19 +0200

    mod actor test

--- tests/actor/test_actor.py
+++ tests/actor/test_actor.py
@@ -7,3 +7,18 @@ def test_actor_bare_init():
 def test_actor_pprint():
     A=Actor()
     A.pprint()
+
+def test_actor_with_tutorial_model():
+    
+    #tutorial init
+    #path to model and animations as positional
+    test_actor = Actor("../"+"models/panda-model",
+                        {"walk": "../"+"models/panda-walk4"})
+
+def test_actor_scale():
+    test_actor = Actor("models/panda-model",
+                                {"walk": "models/panda-walk4"})
+    input_data = (0.005, 0.005, 0.005)
+    test_actor.setScale(*input_data)
+    output_data = test_actor.getScale()
+    assert input_data == output_data
