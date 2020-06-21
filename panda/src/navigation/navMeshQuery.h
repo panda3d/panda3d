d03694149f62dc5b73eff11cef22529031118289
Ashwini <movies1215.ashwini@gmail.com>
2020-06-21 22:29:09 +0530

    worked on suggestions

--- panda/src/navigation/navMeshQuery.h
+++ panda/src/navigation/navMeshQuery.h
@@ -1,3 +1,17 @@
+/**
+ * PANDA 3D SOFTWARE
+ * Copyright (c) Carnegie Mellon University.  All rights reserved.
+ *
+ * All use of this software is subject to the terms of the revised BSD
+ * license.  You should have received a copy of this license along
+ * with this source code in a file named "LICENSE."
+ *
+ * @file movieAudio.h
+ * @author ashwini
+ * @date 2020-060-21
+ */
+
+
 #ifndef NAVMESHQUERY_H
 #define NAVMESHQUERY_H
 
@@ -6,12 +20,14 @@
 
 class NavMeshQuery
 {
+PUBLISHED:
+  void set_nav_query(NavMeshBuilder *nav_mesh) { _nav_query = nav_mesh->get_nav_query(); }
+  bool nearest_point(LPoint3 &p);
 private:
   class dtNavMeshQuery *_nav_query;
   
 public:
-  void set_nav_query(NavMeshBuilder *nav_mesh) { _nav_query = nav_mesh->get_nav_query(); }
-  bool nearest_point(LPoint3 &p);
+  
   
 };
 
