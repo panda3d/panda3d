dde0e5255d3136c38ea13c7d9159009fa06e34f0
rdb <git@rdb.name>
2020-08-10 10:49:29 +0200

    nativenet: remove unused and invalid HP_SOCK implementation
    Closes #984

--- panda/src/nativenet/socket_portable.h
+++ panda/src/nativenet/socket_portable.h
@@ -17,38 +17,6 @@ typedef unsigned long SOCKET;
 #include <netinet/in.h>
 
 /************************************************************************
-* HP SOCKET LIBRARY STUFF
-************************************************************************/
-#elif defined(HP_SOCK)
-
-#ifndef _INCLUDE_HPUX_SOURCE
-#define _INCLUDE_HPUX_SOURCE
-#define _INCLUDE_POSIX_SOURCE
-#define _INCLUDE_XOPEN_SOURCE
-#endif
-#include <sys/socket.h>
-#include <sys/time.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <errno.h>
-
-#define socket_read read
-#define socket_write write
-#define socket_close close
-
-#define DO_CONNECT(a,b,c)   connect(a,b,c)
-#define DO_SOCKET_READ(a,b,c,d)     socket_read(a,b,c)
-#define DO_SOCKET_WRITE(a,b,c,d)    socket_write(a,b,c)
-
-#define GETERROR()                  errno
-
-
-#define  SOCKIOCTL       ioctl
-typedef unsigned long SOCKET;
-#define BAD_SOCKET 0xffffffff
-
-
-/************************************************************************
 * WINSOCK 32 bit STUFF
 ************************************************************************/
 #elif defined(_WIN32)
