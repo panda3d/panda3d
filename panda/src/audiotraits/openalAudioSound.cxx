7d9a3c659017b3a383c24e5bd4ba61b5377e9398
Brian Lach <brianlach72@gmail.com>
2020-10-07 00:43:19 -0400

    openal: Fix paused sounds not resuming from where they were paused

--- panda/src/audiotraits/openalAudioSound.cxx
+++ panda/src/audiotraits/openalAudioSound.cxx
@@ -831,6 +831,8 @@ set_active(bool active) {
         if (_loop_count==0) {
           // ...we're pausing a looping sound.
           _paused=true;
+          // Store off the current time so we can resume from where we paused.
+          _start_time = get_time();
         }
         stop();
       }
