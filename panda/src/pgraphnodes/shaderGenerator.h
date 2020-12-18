617a759fd2574c03d8c027c54c8d827819851b56
rdb <git@rdb.name>
2020-12-19 00:24:26 +0100

    pgraphnodes: Add exposed methods to ShaderGenerator stub
    Since HAVE_CG is now dependent on __aarch64__, it otherwise relies on interrogate having been run with the same architecture flags.

--- panda/src/pgraphnodes/shaderGenerator.h
+++ panda/src/pgraphnodes/shaderGenerator.h
@@ -196,6 +196,18 @@ private:
 
 // If we don't have Cg, let's replace this with a stub.
 class EXPCL_PANDA_PGRAPHNODES ShaderGenerator : public TypedReferenceCount {
+PUBLISHED:
+  ShaderGenerator(const GraphicsStateGuardianBase *gsg);
+  virtual ~ShaderGenerator();
+
+  virtual CPT(ShaderAttrib) synthesize_shader(const RenderState *rs,
+                                              const GeomVertexAnimationSpec &anim) {
+    return nullptr;
+  }
+
+  void rehash_generated_shaders() {}
+  void clear_generated_shaders() {}
+
 public:
   static TypeHandle get_class_type() {
     return _type_handle;
