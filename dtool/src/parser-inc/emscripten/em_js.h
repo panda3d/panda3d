2f8544a81f9ba40083c5bda9b5d951e03c26ff76
rdb <git@rdb.name>
2021-02-05 17:02:02 +0100

    parser-inc: Add emscripten headers

---
+++ dtool/src/parser-inc/emscripten/em_js.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define EM_JS(ret, name, params, ...)
---
+++ dtool/src/parser-inc/emscripten/fiber.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdint.h>
+#include <emscripten/emscripten.h>
+
+typedef struct asyncify_data_s asyncify_data_t;
+typedef struct emscripten_fiber_s emscripten_fiber_t;
