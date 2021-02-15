20bdd32765ffcfc43a718e514973232f0d06ca42
rdb <git@rdb.name>
2021-02-15 11:13:57 +0100

    downloader: Fix an uninitialized member

--- panda/src/downloader/httpChannel.cxx
+++ panda/src/downloader/httpChannel.cxx
@@ -117,6 +117,7 @@ HTTPChannel(HTTPClient *client) :
   _cipher_list = _client->get_cipher_list();
   _last_status_code = 0;
   _last_run_time = 0.0f;
+  _download_dest = DD_none;
   _download_to_ramfile = nullptr;
   _download_to_stream = nullptr;
 }
