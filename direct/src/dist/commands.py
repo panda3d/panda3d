b803ef2ac58cfbec9808d57ed607ee2e26c794da
rdb <git@rdb.name>
2022-01-14 10:50:48 +0100

    dist: Set default Linux target to manylinux2014
    According to
    https://github.com/mayeut/pep600_compliance/blob/master/README.rst
    manylinux2010 is EOL, and so are all Linux distributions that support manylinux2010 but not manylinux2014.
    
    Setting the default to manylinux2014 means that more thirdparty packages are supported and hopefully means we're not always behind and have to struggle to keep building for it.

--- direct/src/dist/commands.py
+++ direct/src/dist/commands.py
@@ -263,7 +263,7 @@ class build_apps(setuptools.Command):
         self.exclude_modules = {}
         self.icons = {}
         self.platforms = [
-            'manylinux2010_x86_64',
+            'manylinux2014_x86_64',
             'macosx_10_9_x86_64',
             'win_amd64',
         ]
@@ -588,7 +588,7 @@ class build_apps(setuptools.Command):
         if platform.startswith('linux_'):
             # Also accept manylinux.
             arch = platform[6:]
-            pip_args += ['--platform', 'manylinux2010_' + arch]
+            pip_args += ['--platform', 'manylinux2014_' + arch]
 
         if self.use_optimized_wheels:
             pip_args += [
