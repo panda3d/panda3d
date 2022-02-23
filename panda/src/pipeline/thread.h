70c49a6416b34c002711fb753e5ac61c4d5e3cd7
rdb <git@rdb.name>
2022-02-23 21:41:53 +0100

    pipeline: Add Thread::relax() for more efficient busy waiting
    Equivalent to cpu_relax() or the pause instruction on x86

--- panda/src/pipeline/thread.h
+++ panda/src/pipeline/thread.h
@@ -80,6 +80,7 @@ PUBLISHED:
 
   BLOCKING INLINE static void force_yield();
   BLOCKING INLINE static void consider_yield();
+  BLOCKING INLINE static void relax();
 
   INLINE static bool get_context_switches(size_t &total, size_t &involuntary);
 
