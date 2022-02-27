ec93c4c3c828f7414daa8bf916b600cf24f4c847
Maxwell Dreytser <admin@mdtech.us>
2022-02-27 14:17:43 -0500

    Fix skipping tests when navigation/navmeshgen are not built.

--- tests/navigation/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-@pytest.fixture
-def navigation():
-    navmeshgen = pytest.importorskip("panda3d.navmeshgen")
-    navigation = pytest.importorskip("panda3d.navigation")
-    return navmeshgen, navigation
