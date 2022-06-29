3fc579c7d4b6db1c2ea4f35dec66d9e9842a5bad
rdb <git@rdb.name>
2022-06-29 17:03:44 +0200

    tinydisplay: Implement resizeability of offscreen buffers
    Fixes #1322

--- panda/src/tinydisplay/tinyGraphicsBuffer.h
+++ panda/src/tinydisplay/tinyGraphicsBuffer.h
@@ -35,6 +35,8 @@ public:
   virtual bool begin_frame(FrameMode mode, Thread *current_thread);
   virtual void end_frame(FrameMode mode, Thread *current_thread);
 
+  virtual void set_size(int x, int y);
+
   INLINE ZBuffer *get_frame_buffer();
 
 protected:
