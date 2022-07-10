f39ad0ab99babd05760a8b255e32df666d3c5f5f
Maxwell Dreytser <admin@mdtech.us>
2022-07-09 20:19:37 -0400

    Implement Tiling and dynamically updating the navmesh during runtime.

--- panda/src/navigation/navMesh.h
+++ panda/src/navigation/navMesh.h
@@ -23,41 +23,76 @@
 #include "eventParameter.h"
 #include "pandabase.h"
 #include "geomNode.h"
+#include "nodePath.h"
 #include "navMeshPoly.h"
 #include <unordered_map>
+#include <set>
 
 /**
  * NavMeshParams class stores all the parameters of a navigation mesh.
  */
 class EXPCL_NAVIGATION NavMeshParams {
 PUBLISHED:
-  int vert_count;
-  int poly_count;
-  int nvp;
-  int detail_vert_count;
-  int detail_tri_count;
-  float walkable_height;
-  float walkable_radius;
-  float walkable_climb;
-  float cs;
-  float ch;
-  bool build_bv_tree;
-  float b_min[3];
-  float b_max[3];
-  unsigned short *verts;        // size: 3 * vert_count
-  unsigned short *polys;        // size: poly_count * 2 * nvp
-  unsigned short *poly_flags;   // size: poly_count
-  unsigned char *poly_areas;    // size: poly_count
-  unsigned int *detail_meshes;  // size: poly_count * 4
-  float *detail_verts;          // size: detail_vert_count * 3
-  unsigned char *detail_tris;   // size: detail_tri_count * 4
-  int border_index = 65535;
-  int input_model_coordinate_system = 0;
+  enum PartitionType {
+    SAMPLE_PARTITION_WATERSHED,
+    SAMPLE_PARTITION_MONOTONE,
+    SAMPLE_PARTITION_LAYERS,
+  };
 
+  float cell_size;
+  float cell_height;
+  float tile_size;
+  int max_tiles;
+  int max_polys_per_tile;
+  float agent_height;
+  float agent_radius;
+  float agent_max_climb;
+  float agent_max_slope;
+  float region_min_size;
+  float region_merge_size;
+  float edge_max_len;
+  float edge_max_error;
+  float verts_per_poly;
+  float detail_sample_dist;
+  float detail_sample_max_error;
+  float orig_bound_min[3];
+  float tile_cell_size;
+  PartitionType partition_type;
+  bool filter_low_hanging_obstacles;
+  bool filter_ledge_spans;
+  bool filter_walkable_low_height_spans;
+};
+
+
+struct TriVertGroup {
+  LVector3 a;
+  LVector3 b;
+  LVector3 c;
+};
+
+inline bool operator==(const TriVertGroup& lhs, const TriVertGroup& rhs)
+{
+  return std::tie(lhs.a, lhs.b, lhs.c) ==
+         std::tie(rhs.a, rhs.b, rhs.c);
+}
+
+inline bool operator<(const TriVertGroup& lhs, const TriVertGroup& rhs)
+{
+  return std::tie(lhs.a, lhs.b, lhs.c) <
+         std::tie(rhs.a, rhs.b, rhs.c);
+}
+
+struct TrackedCollInfo {
+  NodePath node;
+  BitMask32 mask;
 };
 
 typedef pvector<NavMeshPoly> NavMeshPolys;
+typedef pvector<NodePath> NodePaths;
+typedef pvector<TriVertGroup> TriVertGroups;
+typedef pvector<TrackedCollInfo> TrackedCollInfos;
 
+class NavMeshBuilder;
 
 /**
  * NavMesh class stores the navigation mesh. The navigation mesh 
@@ -67,16 +102,17 @@ typedef pvector<NavMeshPoly> NavMeshPolys;
 class EXPCL_NAVIGATION NavMesh : public TypedWritableReferenceCount
 {
 public:
-  explicit NavMesh(dtNavMesh *nav_mesh);
+  explicit NavMesh(dtNavMesh *nav_mesh,
+                   NavMeshParams params,
+                   TriVertGroups &untracked_tris,
+                   NodePaths &tracked_nodes,
+                   TrackedCollInfos &tracked_coll_nodes,
+                   pvector<TriVertGroup> &last_tris);
 
 PUBLISHED:
   explicit NavMesh(NavMeshParams mesh_params);
   NavMesh();
   PT(GeomNode) draw_nav_mesh_geom();
-  INLINE int get_vert_count() const;
-  INLINE int get_poly_count() const;
-  INLINE int get_detail_vert_count() const;
-  INLINE int get_detail_tri_count() const;
 
   NavMeshPolys get_polys();
 
@@ -86,9 +122,18 @@ PUBLISHED:
 
   INLINE void reset_debug_colors();
 
+  INLINE NavMeshParams get_params();
+
+  INLINE NodePaths get_tracked_nodes();
+
+  INLINE TrackedCollInfos get_tracked_coll_nodes();
+
+  INLINE TriVertGroups get_untracked_tris();
+
+  void update();
+
 private:
   dtNavMesh *_nav_mesh;
-  dtNavMeshCreateParams _params {};
   int border_index = 0;
 
   std::unordered_map<dtPolyRef, LColor> _debug_colors;
@@ -131,6 +176,14 @@ public:
 protected:
   static TypedWritable *make_from_bam(const FactoryParams &params);
   void fillin(DatagramIterator &scan, BamReader *manager);
+
+  unsigned char* buildTileMesh(int tx, int ty, const float* bmin, const float* bmax, int& dataSize, pvector<float> &verts, pvector<int> &tris) const;
+
+  TriVertGroups _untracked_tris;
+  NodePaths _tracked_nodes;
+  TrackedCollInfos _tracked_coll_nodes;
+  NavMeshParams _params;
+  std::set<TriVertGroup> _last_tris;
 };
 
 #include "navMesh.I"
