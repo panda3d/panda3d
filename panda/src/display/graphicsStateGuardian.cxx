899cbb9fff3f7e77bb69f6d62d1247884d6891c0
rdb <git@rdb.name>
2022-08-31 11:37:49 +0200

    shadow: Add shadow-cube-map-filter setting, disabled by default
    Enabling this will enable the `FT_shadow` filter for cube maps, which doesn't work with Cg shaders (incl. shader generator) but does with custom GLSL shaders
    
    This will be enabled by default once the shaderpipeline branch is merged
    
    Fixes #1332

--- panda/src/display/graphicsStateGuardian.cxx
+++ panda/src/display/graphicsStateGuardian.cxx
@@ -3580,8 +3580,15 @@ get_dummy_shadow_map(Texture::TextureType texture_type) const {
       dummy_cube->setup_cube_map(1, Texture::T_unsigned_byte, Texture::F_depth_component);
       dummy_cube->set_clear_color(1);
       // Note: cube map shadow filtering doesn't seem to work in Cg.
-      dummy_cube->set_minfilter(SamplerState::FT_linear);
-      dummy_cube->set_magfilter(SamplerState::FT_linear);
+      // That is why it is currently disabled by default, but it can be
+      // overridden in Config.prc for apps that have custom GLSL shaders.
+      if (shadow_cube_map_filter && get_supports_shadow_filter()) {
+        dummy_cube->set_minfilter(SamplerState::FT_shadow);
+        dummy_cube->set_magfilter(SamplerState::FT_shadow);
+      } else {
+        dummy_cube->set_minfilter(SamplerState::FT_linear);
+        dummy_cube->set_magfilter(SamplerState::FT_linear);
+      }
     }
     return dummy_cube;
   }
