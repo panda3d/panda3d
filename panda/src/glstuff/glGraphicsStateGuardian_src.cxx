panda3d/panda3d

commit 2c9deaaaf06fba7b4d5f71e9e75cd4919aa5a18e

glgsg: Fix 1 texture stage limit when setting `gl-version 3 2` or higher
Fixes #1404

panda/src/glstuff/glGraphicsStateGuardian_src.cxx
@@ -11623,7 +11623,17 @@ set_state_and_transform(const RenderState *target,
   if (_target_rs->get_attrib(texture_slot) != _state_rs->get_attrib(texture_slot) ||
       !_state_mask.get_bit(texture_slot)) {
     PStatGPUTimer timer(this, _draw_set_state_texture_pcollector);
-    determine_target_texture();
+    if (has_fixed_function_pipeline() ||
+        _current_shader == nullptr ||
+        _current_shader == _default_shader) {
+      determine_target_texture();
+    } else {
+      // If we have a custom shader, don't filter down the list of textures.
+      _target_texture = (const TextureAttrib *)
+        _target_rs->get_attrib_def(TextureAttrib::get_class_slot());
+      _target_tex_gen = (const TexGenAttrib *)
+        _target_rs->get_attrib_def(TexGenAttrib::get_class_slot());
+    }
     do_issue_texture();
 
     // Since the TexGen and TexMatrix states depend partly on the particular

commit 45ac5029c00209aacbbbed88d311d0033c21034b

glgsg: Fix compile error with OpenGL ES 1
Introduced by 2c9deaaaf06fba7b4d5f71e9e75cd4919aa5a18e

panda/src/glstuff/glGraphicsStateGuardian_src.cxx
@@ -11623,6 +11623,9 @@ set_state_and_transform(const RenderState *target,
   if (_target_rs->get_attrib(texture_slot) != _state_rs->get_attrib(texture_slot) ||
       !_state_mask.get_bit(texture_slot)) {
     PStatGPUTimer timer(this, _draw_set_state_texture_pcollector);
+#ifdef OPENGLES_1
+    determine_target_texture();
+#else
     if (has_fixed_function_pipeline() ||
         _current_shader == nullptr ||
         _current_shader == _default_shader) {
@@ -11634,6 +11637,7 @@ set_state_and_transform(const RenderState *target,
       _target_tex_gen = (const TexGenAttrib *)
         _target_rs->get_attrib_def(TexGenAttrib::get_class_slot());
     }
+#endif
     do_issue_texture();
 
     // Since the TexGen and TexMatrix states depend partly on the particular