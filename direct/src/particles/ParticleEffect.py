panda3d/panda3d

commit c7c70bc32a298557684c58065bae5a02c1cb72ba

direct: Cut down on usage of `import *` (#1454)
* Remove most `import *`s in `direct`

* Address feedback

direct/src/particles/ParticleEffect.py
@@ -1,5 +1,5 @@
 
-from panda3d.core import *
+from panda3d.core import Filename, NodePath, VirtualFileSystem, getModelPath
 
 # Leave these imports in, they may be used by ptf files.
 from panda3d.physics import * # pylint: disable=unused-import

commit 10dd0f79bcac1d4fb8a81d143f4090bcf6773f75

particles: Fix regression loading .ptf files
Regression introduced in #1454

direct/src/particles/ParticleEffect.py
@@ -2,6 +2,7 @@
 from panda3d.core import Filename, NodePath, VirtualFileSystem, getModelPath
 
 # Leave these imports in, they may be used by ptf files.
+from panda3d.core import * # pylint: disable=unused-import
 from panda3d.physics import * # pylint: disable=unused-import
 from . import Particles # pylint: disable=unused-import
 from . import ForceGroup # pylint: disable=unused-import