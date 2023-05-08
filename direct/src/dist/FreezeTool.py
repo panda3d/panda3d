3df594a6dd351bbfa08ca4d84cc8b61f1d72e8a7
rdb <git@rdb.name>
2023-05-08 19:50:14 +0200

    dist: Add hidden import for shapely
    [skip ci]

--- direct/src/dist/FreezeTool.py
+++ direct/src/dist/FreezeTool.py
@@ -96,6 +96,7 @@ hiddenImports = {
     'scipy.special._ufuncs': ['scipy.special._ufuncs_cxx'],
     'scipy.stats._stats': ['scipy.special.cython_special'],
     'setuptools.monkey': ['setuptools.msvc'],
+    'shapely._geometry_helpers': ['shapely._geos'],
 }
 
 if sys.version_info >= (3,):
