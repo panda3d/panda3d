c08353dea7ed9cf9bff8c0b78854da14d3e91407
rdb <git@rdb.name>
2023-08-04 13:26:52 +0200

    showbase: Don't disable Windows mesasge loop with multi-threaded pipeline
    Fixes half of #1494

--- direct/src/showbase/ShowBase.py
+++ direct/src/showbase/ShowBase.py
@@ -3219,8 +3219,9 @@ class ShowBase(DirectObject.DirectObject):
         init_app_for_gui()
 
         # Disable the Windows message loop, since Tcl wants to handle this all
-        # on its own.
-        ConfigVariableBool('disable-message-loop', False).value = True
+        # on its own, except if the Panda window is on a separate thread.
+        if self.graphicsEngine.getThreadingModel().getDrawStage() == 0:
+            ConfigVariableBool('disable-message-loop', False).value = True
 
         if ConfigVariableBool('tk-main-loop', True):
             # Put Tkinter in charge of the main loop.  It really
