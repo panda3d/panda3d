b9a98619c9e72473b17284540b5d85a3588513ee
jb <jochen.bandhauer@gmx.net>
2023-12-02 22:01:37 +0100

    fix uppercase

--- panda/src/device/inputDeviceNode.h
+++ panda/src/device/inputDeviceNode.h
@@ -6,7 +6,7 @@
  * license.  You should have received a copy of this license along
  * with this source code in a file named "LICENSE."
  *
- * @file InputDeviceNode.h
+ * @file inputDeviceNode.h
  * @author fireclaw
  * @date 2016-07-14
  */
--- panda/src/event/axisEvent.cxx
+++ panda/src/event/axisEvent.cxx
@@ -6,7 +6,7 @@
  * license.  You should have received a copy of this license along
  * with this source code in a file named "LICENSE."
  *
- * @file AxisEvent.cxx
+ * @file axisEvent.cxx
  * @author jb
  * @date 2023-11-24
  */
