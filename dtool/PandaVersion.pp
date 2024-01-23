26ce3eab297e018516361647f269af14936c3079
rdb <git@rdb.name>
2024-01-23 14:24:23 +0100

    Update version number to 1.10.15

--- dtool/PandaVersion.pp
+++ dtool/PandaVersion.pp
@@ -7,7 +7,7 @@
 // place to put this.
 
 // Use spaces to separate the major, minor, and sequence numbers here.
-#define PANDA_VERSION 1 10 14
+#define PANDA_VERSION 1 10 15
 
 // This variable will be defined to false in the CVS repository, but
 // scripts that generate source tarballs and/or binary releases for
