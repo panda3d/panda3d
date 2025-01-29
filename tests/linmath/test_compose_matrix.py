1281875cbc871fd56abf1b3efe01c828f37bf5af
rdb <git@rdb.name>
2025-01-29 16:04:53 +0100

    linmath: Fix degenerate case in decompose_matrix
    Regression in 583f7366db786ff750d77fb8ff09fd3c5718db1c

--- tests/linmath/test_compose_matrix.py
+++ tests/linmath/test_compose_matrix.py
@@ -24,3 +24,20 @@ def test_compose_matrix(coordsys):
     new_quat = core.LQuaternion()
     new_quat.set_hpr(new_hpr, coordsys)
     assert quat.is_same_direction(new_quat)
+
+
+@pytest.mark.parametrize("coordsys", (core.CS_zup_right, core.CS_yup_right, core.CS_zup_left, core.CS_yup_left))
+def test_compose_matrix2(coordsys):
+    mat = core.LMatrix3(1, 0, 0, 0, 0, -1, 0, 1, 0)
+
+    new_scale = core.LVecBase3()
+    new_hpr = core.LVecBase3()
+    new_shear = core.LVecBase3()
+    core.decompose_matrix(mat, new_scale, new_shear, new_hpr, coordsys)
+
+    assert new_scale.almost_equal(core.LVecBase3(1, 1, 1))
+    if coordsys in (core.CS_zup_left, core.CS_yup_left):
+        assert new_hpr.almost_equal(core.LVecBase3(0, 90, 0))
+    else:
+        assert new_hpr.almost_equal(core.LVecBase3(0, -90, 0))
+    assert new_shear.almost_equal(core.LVecBase3(0, 0, 0))
