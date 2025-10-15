97b3c1896017e1033ac56bc13cd8ab32b39049a6
rdb <git@rdb.name>
2025-10-15 11:59:07 +0200

    express: Fix build with older OpenSSL versions

--- panda/src/express/zipArchive.cxx
+++ panda/src/express/zipArchive.cxx
@@ -28,6 +28,11 @@
 
 #include "openSSLWrapper.h"
 
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#define EVP_MD_CTX_new() EVP_MD_CTX_create()
+#define EVP_MD_CTX_free(ctx) EVP_MD_CTX_destroy(ctx)
+#endif
+
 using std::streamoff;
 using std::streampos;
 using std::streamsize;
