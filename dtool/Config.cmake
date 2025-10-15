5006f92110d6286855620ce30e7d8fd1829408f4
rdb <git@rdb.name>
2025-10-15 21:21:04 +0200

    build: Update to interrogate v0.7.1

--- dtool/Config.cmake
+++ dtool/Config.cmake
@@ -265,7 +265,7 @@ if(BUILD_INTERROGATE)
     panda3d-interrogate
 
     GIT_REPOSITORY https://github.com/panda3d/interrogate.git
-    GIT_TAG c19b93c00bd31ecdde861f6511e7830d9f69ac91
+    GIT_TAG ccbd5f9d79037a9d53d6dfb1a5e9fdf30f2510c1
 
     PREFIX ${_interrogate_dir}
     CMAKE_ARGS
