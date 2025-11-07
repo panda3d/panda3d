d1aeef5c25b8af15cbeed0d8f03d5a8c7807492d
rdb <git@rdb.name>
2025-11-07 09:30:39 +0100

    makepanda: Remove -m32 and -m64 flags for Android

--- makepanda/makepanda.py
+++ makepanda/makepanda.py
@@ -1382,10 +1382,10 @@ def CompileCxx(obj,src,opts):
             elif arch == 'mips64':
                 cmd += ' -fintegrated-as'
             elif arch == 'x86':
-                cmd += ' -march=i686 -mssse3 -mfpmath=sse -m32'
+                cmd += ' -march=i686 -mssse3 -mfpmath=sse'
                 cmd += ' -mstackrealign'
             elif arch == 'x86_64':
-                cmd += ' -march=x86-64 -msse4.2 -mpopcnt -m64'
+                cmd += ' -march=x86-64 -msse4.2 -mpopcnt'
 
             cmd += " -Wa,--noexecstack"
 
