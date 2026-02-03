24b7376598484b40008a68774abedd99324dde7d
rdb <git@rdb.name>
2026-02-03 13:19:05 +0100

    tests: Gracefully skip test_Placer if window creation failed

--- tests/tkpanels/test_Placer.py
+++ tests/tkpanels/test_Placer.py
@@ -6,7 +6,12 @@ from direct.tkpanels.Placer import Placer
 
 
 def test_Placer(tk_toplevel):
-    base = ShowBase()
+    try:
+        base = ShowBase()
+    except Exception as ex:
+        if str(ex) == 'Could not open window.':
+            pytest.skip(str(ex))
+
     base.start_direct()
     root = Pmw.initialise()
     widget = Placer()
