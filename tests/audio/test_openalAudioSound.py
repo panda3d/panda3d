b544c4a82a7bbd7e12d67a1664416fcc7f820dd3
gossfunkel <katherineegoss@gmail.com>
2026-04-08 02:35:16 +0100

    test passed with the proper file loading pattern

--- tests/audio/test_openalAudioSound.py
+++ tests/audio/test_openalAudioSound.py
@@ -1,12 +1,22 @@
-from panda3d.core import AudioSound
+import os
+
 import pytest
+from panda3d.core import Filename, MovieAudio
 
 def test_make_copy(audiomgr):
     if "openal" not in str(audiomgr).lower():
         # NULL audio manager (as well as fmod) don't support copying yet
         pytest.skip("Copying is currently only supported on OpenAL")
 
-    test_sound = audiomgr.get_sound("openclose_with_comments.mp3")
+    sound_path = os.path.join(os.path.dirname(__file__), "wav_test.wav")
+    sound_path = Filename.from_os_specific(sound_path)
+    test_sound = MovieAudio.get(sound_path)
+    if str(test_sound) == 'Load-Failure Stub':
+        pytest.skip("Could not load audio file")
+    test_sound = audiomgr.get_sound(test_sound)
+    if str(test_sound).startswith("NullAudioSound"):
+        pytest.skip("Sound loading failed")
+
     test_sound.setActive(1)
     test_sound.set3dMaxDistance(20.0)
     test_copy = test_sound.make_copy()
